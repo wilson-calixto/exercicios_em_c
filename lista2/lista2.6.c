2a9e16d6bdd8168e3f2f4f3df4d11d6876063b21
rai-santos <raisoledade@outlook.com>
2015-08-24 17:58:41 -0400

    questao 6, lista 2.6.c

---
+++ lista2/lista2.6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int n1, n2, n3;
+
+	printf("Insira o primeiro numero: \n");
+	scanf("%d", &n1);
+	printf("Insira o segundo numero: \n");
+	scanf("%d", &n2);
+	printf("Insira o terceiro numero: \n");
+	scanf("%d", &n3);
+
+	if(n1 > n2 && n1 > n3)
+	{
+		printf("O maior numero é: %d \n", n1);
+	}
+	else
+	{
+		if(n2 > n1 && n2 > n3)
+		{
+			printf("O maior numero é: %d \n", n2);
+		}else
+		{
+		printf("O maior numero é: %d \n", n3);
+		}
+	}
+	return 0;
+}
