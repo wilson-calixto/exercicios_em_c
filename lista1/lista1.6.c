af53a816936ef2cbb1f972e43b27ef32a8e7de4e
borgesbsb <borges.bnjamin@gmail.com>
2015-08-24 17:05:49 -0400

    Grupo 1 - Corrigindo comentarios

--- lista1/lista1.6.c
+++ lista1/lista1.6.c
@@ -1,6 +1,15 @@
-/* Grupo - 1
- *
- * */
+/* 
+ Programação de Computadores e Algoritmos
+ Trabalho 1
+ Questão 1.1
+ Equipe: Benjamin Borges
+	 Davi Tavares
+	 Manoel Souza 
+	 Lucas Botinelly
+	 Jackson Gomes
+	 Robson Borges
+Grupo 1 - 
+ */
 
 #include<stdio.h>
 
