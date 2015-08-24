653d0d6e7dcf769d2ae01bb7901354bfdf3e26a6
richardson-souza <richardson.allan.souza@gmail.com>
2015-08-24 16:05:38 -0400

    ex lista1 questao 10

---
+++ lista1/lista1.10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("       X\n      XXX\n     XXXXX\n    XXXXXXX\n   XXXXXXXXX\n  XXXXXXXXXXX\n XXXXXXXXXXXXX\nXXXXXXXXXXXXXXX\n      XX\n      XX\n     XXXX\n");
+	
+	return 0;
+}
