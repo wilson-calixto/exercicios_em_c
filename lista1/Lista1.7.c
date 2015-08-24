b6da71ba37dc6984d44ba9a56d47fbaf0340d58e
ewertonpetillo <ewerton.petillo.gmail.com>
2015-08-24 16:20:52 -0400

    Lista 1 Questao 7

---
+++ lista1/Lista1.7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+	printf("ALUNO(A)       NOTA\n");
+        printf("========       =====\n");
+        printf("ALINE           9.0\n");
+        printf("MÁRIO           DEZ\n");
+        printf("SÉRGIO          4.5\n");
+        printf("SHIRLEY         7.0\n");
+	return 0;
+}
