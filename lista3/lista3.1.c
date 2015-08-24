41b34b974377537b09e8d62a2640596da97aacba
eivandru <forum-konoha@hotmail.com>
2015-08-24 14:06:14 -0400

    Questão 1 da Lista 3

---
+++ lista3/lista3.1.c
@@ -0,0 +1,32 @@
+/*
+Programação de Computadores e Algoritmos
+Trabalho 1
+Questão 2.2
+
+Equipe:
+	Evandro Fernandes
+	Wilson Neto
+	John Lucas
+	Jessica Kelly
+	Fernando Antonio
+	Jailson Pereira
+	Juliany Raiol
+	Raí Santos*/
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int n;
+    printf("Digite o numero: ");
+    scanf("%d", &n);
+    while(n<0 || n>10)
+    {
+        printf("Valor Invalido, digite novamente: ");
+        scanf("%d", &n);
+    }
+
+    printf("Valor digitado: %d \n", n);
+
+    return 0;
+}
