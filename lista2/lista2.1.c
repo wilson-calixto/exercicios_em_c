c077a1866368630deb4aab3d3158371ee1f2dad1
Juliany <julianyraiol@gmail.com>
2015-08-25 14:30:57 -0400

    Questão 1 da Lista 2

--- lista2/lista2.1.c
+++ lista2/lista2.1.c
@@ -1,3 +1,21 @@
+/*
+
+Programacao de Computadores e Algoritmos 
+
+Trabalho1
+
+Questão 2.1
+
+Equipe: Evandro Fernandes
+	Fernando Antonio
+	Jailson Pereira
+	Jessica Kelly
+	Jhon Lucas
+	Juliany Raiol
+	Wilson Neto
+	Raí Santos
+*/
+
 #include <stdio.h>
 
 int main(int arc, const char *arcv)
