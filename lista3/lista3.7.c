876c62ce79f8111d7fb191d51fcfe34cdfaf4a57
FernandoCalderaro <fernandosilvacalderaro@gmail.com>
2015-08-25 15:47:30 -0400

    Grupo 5 Upando

--- lista3/lista3.7.c
+++ lista3/lista3.7.c
@@ -1,31 +1,41 @@
-/* 
- *   Programação de Computadores e Algoritmos
- *   Trabalho 1
- *   Questão 3.7
- *   Equipe: Benjamin Borges
- *   Davi Tavares
- *   Manoel Souza 
- *   Lucas Botinelly
- *   Jackson Gomes
- *   Robson Borges
- *   Grupo 1 - 
- */
+/*
+Programação de computadores e Algoritmos
+Trabalho 1
+Questão 3.07
+
+Equipe:
+Evandro Fernandes
+Wilson Neto
+John Lucas
+Jessica Kelly
+Fernando Antonio
+Jailson Pereira
+Juliany Raiol
+Raí Santos*/
 
 #include<stdio.h>
 
-int main(int argc, const char *argv[])
+int main ( int argc, const char* agrv[])
 {
-	int i,num,maior;
-	printf("Digite um número: \n");
-	scanf("%d",&num);
-	maior=num;
-	for (i==0;i<4;i++){
-		printf("Digite um número: \n");
-		scanf("%d",&num);
-		if(maior<num){
-			maior=num;
-			}
-		}	
-	printf("O maior número é: %d\n",maior);
-	return 0;
+        int i,numeros[5],maior;
+
+        for(i=1;i<6;i++)
+        {
+           printf("Digite o %d° número:",i);
+           scanf("%d",&numeros[i]);
+        }
+
+        maior=numeros[1];
+
+        for(i=1;i<6;i++)
+        {
+              if(numeros[i]>maior)
+                {
+                 maior=numeros[i];
+                }
+        }
+
+        printf("O maior numero: %d \n",maior);
+        return 0;
 }
+
