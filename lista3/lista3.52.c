f494595bfe5dc8aabbb4dafada7d68145b9f5ed7
Delrick-Oliveira <delricknunes@gmail.com>
2015-08-25 11:42:42 -0400

    questão 52 da lista 3

---
+++ lista3/lista3.52.c
@@ -0,0 +1,32 @@
+/*
+ * Programa de Computadores e Algoritmos
+ * Trabalho 1
+ * Questao 3.52
+ *
+ * Equipe: 
+ *         Delrik
+ *         Elisabeth
+ *         Hermamm
+ *         Luis
+ *         Luana
+ *         Richardson
+ *
+ */
+
+#include<stdio.h>
+
+int main( int argc, const char *argv[])
+{
+	int tam;
+	float termo, h,i;
+	printf("Digite a quantidade de termos\n");
+	scanf("%d", &tam);
+	h=0;
+	for(i=0;i<tam;i++)
+	{
+		termo=1/(i+1);
+		h= h+termo;		
+	}
+	printf("O valor de h e: %f\n", h);
+	return 0;
+}
