9e14791e0843191805b503a4be2f57f46b60b402
SergioPinheiro <sergio.a.a.p@hotmail.com>
2015-08-25 10:54:05 -0400

    lista3.24

---
+++ lista3/lista3.24.c
@@ -0,0 +1,39 @@
+/*
+Programacao de computadores e Algoritmos
+Trabalho 1
+Questao 2.19
+
+Equipe:
+	Paulo Marinho
+	Sergio Pinheiro	
+	Wilson Calixto
+	Andressa Moreira
+*/
+
+#include <stdio.h>
+
+int main (int argc, const char argv[])
+{
+	int cont = 0, i;
+	float n[50], media = 0.0;
+
+	do
+	{
+		printf("\nDigite a nota (-1 para terminar): ");
+		scanf("%f", &n[cont]);
+		cont++;
+
+	} while (n[cont-1] > 0);
+
+	cont--;
+
+	for (i = 0; i < cont; i++)
+	{
+		media = media + n[i];
+	}
+	media = media / cont;
+
+	printf("\n\nMedia: %f\n\n", media);
+
+	return 0;
+}
