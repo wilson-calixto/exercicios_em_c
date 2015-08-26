a04472b75ebceb11ffa088d21cade7603c5c0d2b
AndressaMoreira <dreka.moreira@gmail.com>
2015-08-26 15:21:30 -0400

    lista3.26.c eleicao

--- lista3/lista3.26.c
+++ lista3/lista3.26.c
@@ -1,3 +1,12 @@
+/*Programacao de Computadores e Algoritmos
+Trabalho 1
+Questao 3.26
+Equipe: Andressa Moreira
+	Paulo Marinho
+	Sergio Pinheiro
+	Wilson Calixto
+	Rodrigo Oliveira*/
+
 #include <stdio.h>
 
 int main(int argc, const char *argv[])
