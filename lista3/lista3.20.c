81f7442524bf20b69ccc71690ee17cf01ddc25ef
lucasfrota <frota_lv@hotmail.com>
2015-08-26 15:12:10 -0400

    upando questoes 17 e 20 da lista3

---
+++ lista3/lista3.20.c
@@ -0,0 +1,36 @@
+/*
+Questao 20 da Lista 3
+
+O programa le o numero e testa se ele e primo.
+
+Professor Jucimar Junior
+Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
+
+Equipe: Jackson Kelvin
+	Ewerton Petillo
+	Gabriel Faraco
+	Lucas Frota
+
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]){
+	int i,num, cont=0;
+
+	printf("Digite o numero inteiro positivo: ");	
+	scanf("\n %d", &num); // Le o inteiro positivo
+	for(i=1;i<=num;i++){ // Laco para contar quantos divisores possui o numero
+		if(num%i==0){
+			cont++; // Contador da quantidade de divisores
+		}
+	}
+	if(cont > 2){ // Se possuir mais de 2 divisores nao eh primo.
+		printf("Numero nao Primo. \n" );
+		
+	}else{ // Senao, se possuir so 2 divisores e primo.
+		printf("Numero  Primo. \n");
+	}
+} 
