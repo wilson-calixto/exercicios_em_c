bf5c708b5651eb58083e6cc6bcab80629b4935e6
pflmarinho <pflmarinho@gmail.com>
2015-08-28 23:02:40 -0400

    adicionando a exceção da regra do ano 2000

--- lista2/lista2.17.c
+++ lista2/lista2.17.c
@@ -22,10 +22,14 @@
 	printf("Digite um ano:");
 	scanf("%d",&ano);
 
-	if((ano%4 == 0 && ano%100 != 0) || ano%400 == 0)
+	if(ano%4 == 0 && ano%100 != 0)
 	{
 		printf("O ano é bissexto.\n");
 
+	}
+	else if (ano == 2000)
+	{
+		printf("o ano é bissexto.\n");
 	}
 	else 
 	{
