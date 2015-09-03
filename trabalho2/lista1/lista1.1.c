6817a119bd719df13add2fb072ecd837586acd18
jailsonpj <jailsoncolares@gmail.com>
2015-09-03 18:31:15 -0400

    Adcionando questão 1 da lista 1

---
+++ trabalho2/lista1/lista1.1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int imprimirNumeros(){
+   int qtdeNumeros, i, j;
+   printf("Numero: ");
+   scanf("%i",&qtdeNumeros);
+
+  
+  for(i = 1; i <= qtdeNumeros; i++){
+      for(j = 1; j <= i; j++){
+        printf("%i ",i);
+     }
+     printf("\n");
+   }
+}
+
+
+
+
+
+int main(){
+   imprimirNumeros(); 
+   return 0;
+}
