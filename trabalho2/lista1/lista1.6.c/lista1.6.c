591a1c072ea9143bcba86a4e9c38f43370a0daac
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-03 10:58:41 -0400

    Lista 1 Q-6 Comentada.Usando MakeFile

--- trabalho2/lista1/lista1.6.c/lista1.6.c
+++ trabalho2/lista1/lista1.6.c/lista1.6.c
@@ -2,8 +2,7 @@
  *
  * Trabalho 2
  * Lista 1
- * Qustao 6
- *
+ * Questao 6
  *
  *\Equipe:  Richardson 
  *          Jailson Pereira
@@ -15,37 +14,29 @@
  *          Autor: Ewerton Petillo
  *
  * / */
-
-
-
-
-
-
-
-
-#include <stdio.h>
-#include "lib_function.h"
+#include <stdio.h> 
+#include "lib_function.h" // Biblioteca onde estao as funcoes usados no codigo
 
 int main(int argc, const char *argv[]){
-    int hr, min;
-    char op;
-    do{
+    int hr, min; // Variaveis de horas e minutos
+    char op; // Opcao informada pelo usuario para continuar no loop de escolha
+    do{                             // Loop de escolha do usuario
         printf("Digite as horas: ");
-        scanf("%d", &hr);
-        if (is_hour(hr)==0){
-            printf("Valor invalido. \n");
-        }else{
-            printf("Digite os minutos: ");
-            scanf("%d", &min);
-            if(is_minute(min)==0){
-                printf("Valor invalido. \n");
+        scanf("%d", &hr); // Le a variavel hora
+        if (is_hour(hr)==0){ // Chama a funcao para validar a hora
+            printf("Valor invalido. \n"); // Retorna valor invalido para usuario
+        }else{ 
+            printf("Digite os minutos: "); 
+            scanf("%d", &min); // Le a variavel minutos
+            if(is_minute(min)==0){ // Chama a funcao para validar os minutos
+                printf("Valor invalido. \n"); // Retorna valor invalido para usuario
             }else{
-                printf("%d:%d %c.M \n ",convert_hour(hr), min, am_pm(hr)) ; 
+                printf("%d:%d %c.M \n ",convert_hour(hr), min, am_pm(hr)) ; // Imprime as horas no novo format
             }
         }
-        printf("Deseja tentar novamente[s/n]: ");
-        scanf(" %c", &op);
-        fflush(stdin);
+        printf("Deseja tentar novamente[s/n]: "); 
+        scanf(" %c", &op); // Le a escolha do usario para continuar
+        fflush(stdin); // Limpa o buffer do teclado
     }while(op == 's' || op== 'S');
 
 }
