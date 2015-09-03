f81832918d85a64d8c9bfad3778a1c657ee66c24
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-03 09:56:51 -0400

    Implementando a Lista 1 Questao 6 - UsandoMakefile

---
+++ trabalho2/lista1/lista1.6.c/lib_function.h
@@ -0,0 +1,10 @@
+#ifdef FUNCAO_H
+#define FUNCAO_H
+
+int is_hour(int);
+int is_minute(int);
+int convert_hour(int);
+int convert_minute(int);
+
+#endif
+
---
+++ trabalho2/lista1/lista1.6.c/lista1.6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lib_function.h"
+
+int main(int argc, const char *argv[]){
+    int hr, min;
+    char op;
+    do{
+        printf("Digite as horas: ");
+        scanf("%d", &hr);
+        if (is_hour(hr)==0){
+            printf("Valor invalido. \n");
+            return 0;
+        }else{
+            printf("Digite os minutos: ");
+            scanf("%d", &min);
+            if(is_minute(min)==0){
+                printf("Valor invalido. \n");
+                return 0;
+            }else{
+                printf("%d:%d",convert_hour(hr), convert_minute(min)) ; 
+            }
+        }
+
+    }while(op == 's' || op== 'S');
+
+}
