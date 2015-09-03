9e93f059ac9a3adab7333ef8ce7bc670abbe6332
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-03 10:41:08 -0400

    Lista 1 Questao 6 - Finalizada, usando MAKEFILE

--- trabalho2/lista1/lista1.6.c/lista1.6.c
+++ trabalho2/lista1/lista1.6.c/lista1.6.c
@@ -9,18 +9,18 @@ int main(int argc, const char *argv[]){
         scanf("%d", &hr);
         if (is_hour(hr)==0){
             printf("Valor invalido. \n");
-            return 0;
         }else{
             printf("Digite os minutos: ");
             scanf("%d", &min);
             if(is_minute(min)==0){
                 printf("Valor invalido. \n");
-                return 0;
             }else{
                 printf("%d:%d %c.M \n ",convert_hour(hr), min, am_pm(hr)) ; 
             }
         }
-
+        printf("Deseja tentar novamente[s/n]: ");
+        scanf(" %c", &op);
+        fflush(stdin);
     }while(op == 's' || op== 'S');
 
 }
