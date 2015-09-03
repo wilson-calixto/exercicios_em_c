fb0a2befbaa7d7083cbdebb4173245f4efa9612e
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-03 12:57:42 -0400

    trabalhando na questão 20 da lista 2

--- trabalho2/lista2/lista2.20.c
+++ trabalho2/lista2/lista2.20.c
@@ -13,11 +13,35 @@
  *          Wilson 
  */
 
+/* Funcao para realizar o somatorio de n numeros */
 
-#include <stdio.h> 
+#include <stdio.h>
 
-int main ( int argc, char *argv[] )
+int calc_somatorio(int n)
 {
+    int i,sum,num;
+
+    sum=0;
+    
+    for ( i = 0; i < n; i++ )
+    {
+        scanf ( "%d", &num );
+        sum+=num;
+    }
+
+    return sum;
+}
+
+int main ( int argc, const char *argv[] )
+{
+    int result,n;
+
+    printf ( "\nInforme um valor: " );
+    scanf ( "%d", &n );
+    
+    result=calc_somatorio(n);
+
+    printf ( "\nSomatorio: %d", result );
     
     return 0;
 }
