95457a72dfae27b285204d92ab0a015ba52dfba4
borgesbsb <borges.bnjamin@gmail.com>
2015-09-04 16:43:13 -0400

    Criação do diretorio lista2.23 e comentarios iniciais

---
+++ trabalho2/lista2/lista2.23/lista2.23.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lista2.23.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04-09-2015 16:31:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Benjamin Borges (bsb), borges.bnjamin@gmail.com
+ *        Company:  CINDACTA4
+ *
+ * =====================================================================================
+ */
+
+
