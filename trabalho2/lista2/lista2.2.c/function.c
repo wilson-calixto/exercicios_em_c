bca7b0f6845751ed9994d688d01d48e8646ea90d
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-04 12:31:39 -0400

    Lista 2 Q-2 Finalizada-Usando Makefile

--- trabalho2/lista2/lista2.2.c/function.c
+++ trabalho2/lista2/lista2.2.c/function.c
@@ -1,16 +1,18 @@
-double media(double not1, double not2, double not3, char med){
-    double mfinal;
-    if(med == 'a' || med == 'A'){
-        mfinal = ((not1+not2+not3) / 3);
-            return mfinal;
+float media(float not1, float not2, float not3, char med)
+{
+    float mfinal;
+    if(med == 'a' || med == 'A')
+    {
+        mfinal = (not1+not2+not3)/3;
     }
-    if (med == 'p'||med == 'P'){
+    if (med == 'p'||med == 'P')
+    {
         mfinal = ((not1*5)+(not2*3)+(not3*2))/(5+3+2);
-        return mfinal;
     }
-    if(med == 'h' || med == 'H'){
+    if(med == 'h' || med == 'H')
+    {
         mfinal = 3/((1/not1)+(1/not2)+(1/not3));
-        return mfinal;
     }
+    return (mfinal);
 }
 
--- trabalho2/lista2/lista2.2.c/lista2.2.c
+++ trabalho2/lista2/lista2.2.c/lista2.2.c
@@ -14,22 +14,25 @@
  */
 #include <stdio.h>
 #include "function.h"
+#include <string.h>
 
-int main(int argc, const char *argv[]) {
-    double n1,n2,n3, res;
+int main(int argc, const char *argv[])
+{
+    float n1,n2,n3, res;
     char tip;
 
     printf("Digite a nota 1: ");
-    scanf("%lf", &n1);
+    scanf("%f", &n1);
     printf("Digite a nota 2: ");
-    scanf("%lf", &n2);
+    scanf("%f", &n2);
     printf("Digite a nota 3: ");
-    scanf("%lf", &n3);
-    fflush(stdin); // Limpa o buffer do teclado
+    scanf("%f", &n3);
+    getchar();   
     printf("Digite o tipo de media: \n A- Aritmetica\t P-Ponderada\t H-Harmonica\n");
-    scanf(" %c", &tip);
-    res = media(n1, n2, n3, tip);
-    printf("Media: %.2lf \n ", res);
+    scanf("%c", &tip);
+    res = media(n1,n2,n3,tip); // Variavel para receber o retorno da funcao
+    printf("Media: %.2f \n ", res);
+    return 0;
 }
 
 
