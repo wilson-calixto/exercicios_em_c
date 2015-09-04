1d8bccb21875caa52d276f4a45cd8f6f9399c82f
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-03 23:40:40 -0400

    atualizando questão 1 da lista 1

--- trabalho2/lista1/lista1.1.c
+++ trabalho2/lista1/lista1.1.c
@@ -1,30 +1,38 @@
 /*
-*
-*
-*/
-
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 2
+ * lista2.25.c
+ *
+ * Equipe:
+ *        Ewerton
+ *        Hermann
+ *        Jailson
+ *        Lucas
+ *        Richardson
+ *        Wilson 
+ */
 
 #include<stdio.h>
 
-int imprimirNumeros(){
-   int qtdeNumeros, i, j;
+void print_numbers()
+{
+   int n_num, i, j;
    printf("Numero: ");
-   scanf("%i",&qtdeNumeros);
-
+   scanf("%i",&n_num);
   
-  for(i = 1; i <= qtdeNumeros; i++){
-      for(j = 1; j <= i; j++){
+  for(i = 1; i <= n_num; i++)
+  {
+      for(j = 1; j <= i; j++)
+      {
         printf("%i ",i);
-     }
+      }
      printf("\n");
    }
 }
 
 
-
-
-
-int main(){
-   imprimirNumeros(); 
+int main(int argc, const char *argv[])
+{
+   print_numbers(); 
    return 0;
 }
