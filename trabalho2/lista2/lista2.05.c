945e89e02c1f356d8c63399bcd25a9416d7d4417
raisantos <raisoledade@outlook.com>
2015-09-04 22:29:53 -0400

    questao 5, lista2

---
+++ trabalho2/lista2/lista2.05.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/*function to subtraction*/
+int subtraction(int num1, int num2)
+{
+    int aux;
+    aux = num1 - num2;
+    return aux;
+}
+
+/*function seconds to hours*/
+
+int sec_hours(int sec)
+{
+	int hour;
+	hour = sec/3600;
+	return hour;
+}
+/*function seconds to minutes*/
+
+int sec_minutes(int sec)
+{
+	int min;
+	min = sec/60;
+	return min;
+}
+
+int main(int argc, const char *argv[])
+{
+    int seconds, hours, minutes, aux1, aux2, sec;
+
+    printf("Insira os segundos: \n");
+    scanf("%d", &seconds);
+
+    hours = sec_hours(seconds);
+    aux1 = seconds - (hours*3600);
+    minutes = sec_minutes(aux1);
+    aux2 = ((hours*3600)+(minutes*60));
+    sec = subtraction(seconds, aux2);
+
+    printf("%d segundos equivalem a %d horas, %d minutos e %d segundos \n", seconds, hours, minutes, sec);
+
+    return 0;
+}
