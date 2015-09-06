b4789ec1e2358da42263546c3b8259cabb01fc72
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-06 02:34:28 -0400

    atulizando questão 15 da lista 2 e criando um makefile

--- trabalho2/lista2/lista2.15/lista2.15.c
+++ trabalho2/lista2/lista2.15/lista2.15.c
@@ -14,41 +14,13 @@
  */
 
 #include <stdio.h> 
+#include "function.h"
 
 /* 
  * Calcula media aritimetica de varios numeros
  * ate que seja digitado 0
  * retorna um float
  */
-float calc_mean()
-{
-    float i,num,sum,mean;
-
-    sum=0;
-    i=0;
-    num=1;
-
-    while(num!=0)
-    {
-        printf ( "\nDigite um numero positivo: " );
-        scanf ( "%f", &num );
-
-        
-        while ( num<0 )
-        {
-            printf ( "\nNumero invalido." );
-            printf ( "\nDigite um numero positivo: " );
-            scanf ( "%f", &num );
-        }
-        sum+=num;
-        i++;
-    }
-
-    mean=sum/(i-1);
-
-    return mean;
-}
-
 int main ( int argc,const char *argv[] )
 {
     float result;
