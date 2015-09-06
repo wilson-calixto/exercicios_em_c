bc7cac9ca5ade1e4fc2a744ecdd62a5826b7185d
Juliany <julianyraiol@gmail.com>
2015-09-06 11:48:45 -0400

    Criação do escorpo da função

---
+++ trabalho2/lista1/lista1.5/lista1.5.c
@@ -0,0 +1,31 @@
+/*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
+ *
+ * Trabalho 2
+ * Lista 1
+ * Questao 5
+ *
+ *\Equipe:  Elizabeth Castro
+ *          Andreza Moreira
+ *          Juliany Raiol
+ *          Luana Andrade
+ *          Wilson Neto
+ *
+ *     Autor: Juliany Raiol
+ *
+ * / */
+ 
+ int somaImposto(float taxaImposto, float custo)
+ {
+ 	
+ }
+ 
+ int main(int argc, char const *argv[])
+ {
+ 	
+ 	float taxa, custo;
+ 	printf("Digite a Taxa do Imposto de acordo como formato: ");
+ 	scanf("%f",&taxa);
+ 	printf("Digite o custo do item: ");  	
+ 	scanf("%f",&custo);
+ 	return 0;
+ }
