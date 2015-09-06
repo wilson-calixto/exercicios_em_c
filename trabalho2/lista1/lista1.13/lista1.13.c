c662011b0fbb5a9516bcd7794eb8693bc7d71398
Juliany <julianyraiol@gmail.com>
2015-09-06 12:31:59 -0400

    Escorpo da questão 13

---
+++ trabalho2/lista1/lista1.13/lista1.13.c
@@ -0,0 +1,22 @@
+/*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
+ *
+ * Trabalho 2
+ * Lista 1
+ * Questao 5
+ *
+ *\Equipe:  Elizabeth Castro
+ *          Andreza Moreira
+ *          Juliany Raiol
+ *          Luana Andrade
+ *          Wilson Neto
+ *
+ *     Autor: Juliany Raiol
+ *
+ * / */
+ 
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
