6ff57d5870160dea5e996cae82d4c66ce8a43681
Juliany <julianyraiol@gmail.com>
2015-09-06 12:14:26 -0400

    Questão 9 sem uso de função

--- trabalho2/lista1/lista1.9/lista1.9.c
+++ trabalho2/lista1/lista1.9/lista1.9.c
@@ -15,8 +15,18 @@
  * / */
  
 #include <stdio.h>
- 
+#include <string.h>
+
 int main()
 {
+	int i, tam;
+	char numero[1000];
+	scanf("%s",numero);
+	tam = strlen(numero);
+	for(i=tam;i--;)
+	{
+		printf("%c",numero[i]);
+	}
+	printf("\n");
 	return 0;
 } 
