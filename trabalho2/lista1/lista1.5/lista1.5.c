67e0c2ce20d81d4b344b2415fddd617793d2e6d1
Juliany <julianyraiol@gmail.com>
2015-09-06 23:43:39 -0400

    Alterando comentários

--- trabalho2/lista1/lista1.5/lista1.5.c
+++ trabalho2/lista1/lista1.5/lista1.5.c
@@ -17,12 +17,12 @@
  #include <stdio.h>
  
  
- /* Função que altera o custo, retornando um valor float */
- float somaImposto(float taxaImposto, float custo)
+ //Função que altera o custo, retornando um valor float
+ float soma_imposto(float taxaImposto, float custo)
  {
  	float novoValor;
  	
- 	/*Aplicando fórmula matemática para adicionar uma taxa sob o custo passado por parâmetro*/
+ 	//Aplicando fórmula matemática para adicionar uma taxa sob o custo passado por parâmetro
  	novoValor = custo + (custo*(taxaImposto)/100);	
  	return novoValor;	
  }
@@ -31,19 +31,19 @@
  int main(int argc, char const *argv[])
  {
  	
- 	/* Declaração das Variáveis*/
+ 	//Declaração das Variáveis
  	float taxa, custo, novoCusto;
  	
- 	/*Recebendo os valores de entrada do usuário*/
+ 	//Recebendo os valores de entrada do usuário
  	printf("Digite a Taxa do Imposto: ");
  	scanf("%f",&taxa);
  	printf("Digite o custo do item: ");  	
  	scanf("%f",&custo);
  	
- 	/* Chamando a função para alterar o custo atual */
+ 	// Chamando a função para alterar o custo atual
  	novoCusto = somaImposto(taxa, custo);
  	
- 	/* Imprimindo o valor antigo e o novo valor */
+ 	//Imprimindo o valor antigo e o novo valor
  	printf("O novo valor de %.2f eh %.2f: ",custo,novoCusto);
  	printf("\n");
  	return 0;
