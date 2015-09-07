ae6a568e6c58617524c6bf695d5fc8e8a618daea
raisantos <raisoledade@outlook.com>
2015-09-07 17:39:35 -0400

    lista4, questao2

--- trabalho2/lista4/lista4.02.c
+++ trabalho2/lista4/lista4.02.c
@@ -13,6 +13,9 @@
 
 #include <stdio.h>
 
+/*função que verifica se um numero b "encaixa" em um numero a
+ *ou seja, se os dígitos de b, correspondem aos ultimos dígitos de a*/
+
 int fits(int a, int b) 
 {
     while (b != 0 && a%10 == b%10) 
@@ -25,6 +28,8 @@ int fits(int a, int b)
     else
         return 0;
 }
+
+/*função principal*/
 
 int main(int argc, const char *argv[]) 
 {
