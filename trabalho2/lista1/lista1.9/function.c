39893d53fa59d6ac1ac602536d5a2b94b04b147a
Juliany <julianyraiol@gmail.com>
2015-09-06 22:38:45 -0400

    Alterações na questão

---
+++ trabalho2/lista1/lista1.9/function.c
@@ -0,0 +1,12 @@
+//Procedimento para a impressão de um número invertido
+void reverse_number(int num)
+{
+	while(num!=0) //Verifica se o quociente de um número dividido por 10 é maior que zero
+	{
+		// Imprime sempre o resto do número por 10
+		printf("%d",num%10); 
+		
+		//Decrementa o número dividindo o seu valor por 10
+		num/=10;
+	}
+}
--- trabalho2/lista1/lista1.9/lista1.9.c
+++ trabalho2/lista1/lista1.9/lista1.9.c
@@ -10,35 +10,20 @@
  *          Luana Andrade
  *          Wilson Neto
  *
- *     Autor: Juliany Raiol
- *
- * / */
+ * * / */
  
 #include <stdio.h>
 
-/*Procedimento para a impressão de um número invertido*/
-void inversoNumero(int num)
-{
-	while(num!=0) /*Verifica se o quociente de um número dividido por 10 é maior que zero*/
-	{
-		/* Imprime sempre o resto do número por 10 */
-		printf("%d",num%10); 
-		
-		/*Decrementa o número dividindo o seu valor por 10*/
-		num/=10;
-	}
-}
-
 int main()
 {
-	/*Declaração de variáveis*/
+	//Declaração de variáveis
 	int i, tam,numero;
 	
-	/*Recebendo os valores de entrada*/
+	//Recebendo os valores de entrada
 	printf("Digite um Numero: ");
 	scanf("%d",&numero);
 	
-	/*Imprimindo o reverso do numero digitado*/
+	//Imprimindo o reverso do numero digitado
 	printf("Inverso do numero %d eh: ",numero);
 	inversoNumero(numero);
 	
