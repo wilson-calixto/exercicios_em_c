a57b0e456b2d8061344b81f0a08e2a70b6e8e3c0
SergioPinheiro <sergio.a.a.p@hotmail.com>
2015-09-07 14:20:27 -0400

    Add Pasta desafio 2 e arquivo lista3deafio2.c

---
+++ trabalho2/lista3/desafio2/lista3desafio2.c
@@ -0,0 +1,35 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 3
+ * lista3desafio2.c
+ *
+ * Equipe:
+ *	Sergio Pinheiro
+ *  Delrick Oliveira
+ *  Jackson Kevin
+ *  Manoel Victor
+ *  Gabriel Faraco
+ *  Raí Santos
+ *      
+ */
+
+
+#include <stdio.h>
+#include "mdc.c"
+
+
+int main (int argc, const char *charv[])
+{
+    int x, y, z;
+
+    printf("\nDigite o primeiro numero: ");
+    scanf("%d", &x);
+
+    printf("\nDigite o segundo numero: ");
+    scanf("%d", &y);
+
+    z = mdc(x,y);
+    printf("\nO mdc e: %d\n", z);
+
+    return 0;
+}
