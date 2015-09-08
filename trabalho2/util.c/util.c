f1dee9883ef455c5c37ebc30fe7963d5f87b6234
raisantos <raisoledade@outlook.com>
2015-09-07 23:23:10 -0400

    util

--- trabalho2/util.c/util.c
+++ trabalho2/util.c/util.c
@@ -184,3 +184,20 @@ int is_pair (int x)
 
     else return 0;
 }
+
+/*função que verifica se um numero b "encaixa" em um numero a
+ *ou seja, se os dígitos de b, correspondem aos ultimos dígitos de a*/
+
+int fits(int a, int b) 
+{
+    while (b != 0 && a%10 == b%10) 
+    {
+        a = a/10;
+        b = b/10;
+    }
+    if (b == 0)
+        return 1;
+    else
+        return 0;
+}
+
