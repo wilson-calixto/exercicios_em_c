acbe6efa6fadc4791055c4fb4ad18ec5f3330d43
raisantos <raisoledade@outlook.com>
2015-09-08 13:00:01 -0400

    lista 4, questao2

---
+++ trabalho2/lista4/lista4.02/lista4.02.c
@@ -0,0 +1,61 @@
+/*Progrmação de Computadores e Algoritmos
+
+ *Trabalho 2
+ *Lista 4, questão 2
+ *
+ *Equipe:
+ *	Jackson Kelvin
+ *	Delrick
+ *	Manoel
+ *	Gabriel Faraco
+ *	Sérgio
+ *	Raí                       */
+
+#include <stdio.h>
+#include "funcao.h"
+
+int main(int argc, const char *argv[]) 
+{
+    int num1, num2, maior, menor, val;
+
+    printf("Insira um numero: \n");
+    scanf("%d", &num1);
+    printf("Insira um numero: \n");
+    scanf("%d", &num2);
+            
+    maior = big(num1, num2);
+    menor = small(num1, num2);
+
+    val = 0;
+    while (maior >= menor) 
+    {
+        if (fits(maior,menor) == 1)
+            val = 1;
+        maior/=10;
+    }
+    if (val == 1)
+    {
+        if(num1 > num2)
+        {
+            printf("%d é segmento de %d \n",num2, num1);
+        }
+        else
+        {
+            printf("%d é segmento de %d \n",num1, num2);
+        }
+    }
+
+    else
+    {
+        if(num1 > num2)
+        {
+            printf("%d nao é segmento de %d \n",num2, num1);
+        }
+        else
+        {
+            printf("%d nao é segmento de %d \n",num1, num2);
+        }
+    }
+
+    return 0;
+}
