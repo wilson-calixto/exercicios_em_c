178092d9a45d059187847696b2f33acabcd3f258
netoolii <netow@myself.com>
2015-09-08 22:19:17 -0400

    questao 2

---
+++ trabalho2/lista3/lista3.02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../util.c/util.h"
+
+int equacao_segundo_grau(int a,b,c)
+{
+    int delta=0;
+    
+    potencia=pow(b,2);
+    delta=potencia-(4*a*c);
+
+
+    return delta;
+}
