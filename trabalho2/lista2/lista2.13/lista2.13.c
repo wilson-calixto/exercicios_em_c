4fb24136045f50b2c92e6cde83d7994e01d3b59d
borgesbsb <borges.bnjamin@gmail.com>
2015-09-08 20:24:03 -0400

    Inserindo questao 13 da lista 2

---
+++ trabalho2/lista2/lista2.13/lista2.13.c
@@ -0,0 +1,48 @@
+﻿/* 
+ *  Programação de Computadores e Algoritmos
+ *  Trabalho 2
+ *  Questão 2.13
+ *  Equipe: Benjamin Borges
+ *  Davi Tavares
+ *  Paulo Henrique 
+ *  Fernando Calderaro
+ *  Lucas Frota
+ *  Luiz Fernando
+ *  
+ *  */
+ 
+/* #include	"../../util.c/util.h" */
+#include "triangle.h"
+#include <stdio.h>
+
+int main( int argc, const char *argv[] ) 
+{
+	
+	float a, b, c;
+	
+	printf( "Informe o lado A do triangulo: " );
+	scanf( "%f", &a );
+	printf( "Informe o lado B do triangulo: " );
+	scanf( "%f", &b );
+	printf( "Informe o lado C do triangulo: " );
+	scanf( "%f", &c );
+	
+	if( triangle( a, b, c ) == 2 ) 
+	{
+		printf( "Nao eh triangulo\n" );
+	}
+	else if( triangle( a, b, c ) == 3 ) 
+	{
+		printf( "Triangulo equilatero\n" );
+	}
+	else if( triangle( a, b, c ) == 4 ) 
+	{
+		printf( "Triangulo isosceles\n" );
+	}
+	else if( triangle( a, b, c ) == 5 ) 
+	{
+		printf( "Triangulo escaleno\n" );
+	}
+	
+	return 0;
+}
