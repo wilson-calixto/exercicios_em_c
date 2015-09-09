f63dfed747cdde2cad61898d9e7ec98dbb0ebab4
Juliany <julianyraiol@gmail.com>
2015-09-09 00:41:33 -0400

    Validações

--- trabalho2/lista1/lista1.13/lista1.13.c
+++ trabalho2/lista1/lista1.13/lista1.13.c
@@ -20,14 +20,26 @@
 int main()
 {
 	/*Declaração das variáveis*/
-	int linhas, colunas;
+	int linhas, colunas,novalinha, novacoluna,x,y;
 	
 	printf("Digite as linhas: ");
 	scanf("%d",&linhas);
 	printf("Digite as colunas: ");
 	scanf("%d",&colunas);
-	
-	retangulo(linhas,colunas);
+	if( (linhas >= 1 ) && (colunas <= 20) ) retangulo(linhas,colunas);
+	else
+	{
+		y = abs(linhas - 1); //Operação para achar quantos valores são necessários para alcançar o mínimo 1
+		novalinha = linhas + y; //Operação para chegar ao número 1
 		
+		//A mesma operação para a linha se repete para a coluna
+		x = colunas - 20; 
+		novacoluna = colunas - x;
+		
+		printf("Seus valores de entrada foram alterados para %d %d\n",novalinha, novacoluna);
+		
+		retangulo(novalinha,novacoluna);
+	
+	}
 	return 0;
 }
