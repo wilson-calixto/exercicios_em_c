4b382134229a558906c77a86ff30c8fca410620b
Paulo Fernado Lima Marinho <lucas_botinelly@hotmail.com>
2015-09-10 11:01:45 -0400

    levantando lista2.14

---
+++ trabalho2/lista2/lista2.14/lista2.14.c
@@ -0,0 +1,23 @@
+/*Programação
+*
+*Trabalho 2
+*lista 2
+*Questao 14
+*
+*Equipe: Paulo Marinho
+*	 Evandro Fernandes		
+*	 Robson
+*	 Jessica
+*	 Jackson Gomes
+*	 
+*
+*/
+#include<stdio.h>
+#include"util.h"
+
+
+int main (int argc, const char *argv[])
+{
+    city();
+    return 0;
+}
