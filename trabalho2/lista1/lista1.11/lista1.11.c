79147af0668fde0fc7f239efbb9eb6f47fda8f5b
lucasfrota <frota_lv@hotmail.com>
2015-09-10 00:22:49 -0400

    questao 11 da lista 1

---
+++ trabalho2/lista1/lista1.11/lista1.11.c
@@ -0,0 +1,20 @@
+/* 
+ *  Programação de Computadores e Algoritmos
+ *  Trabalho 2
+ *  Questão 1.8
+ *  Equipe: Benjamin Borges
+ *  Davi Tavares
+ *  Paulo Henrique 
+ *  Fernando Calderaro
+ *  Lucas Frota
+ *  Luiz Fernando
+ *  
+ *  */
+
+int main(int argc, const char *argv[])
+{
+	char num[12];
+	printf(">");
+	scanf("%s", num);
+	date(num);
+}
