d2bd37d70930ef4120b6a7aa09351202c3980603
raisantos <raisoledade@outlook.com>
2015-09-10 14:57:33 -0400

    deletando funcao

--- trabalho2/util.c/util.c
+++ trabalho2/util.c/util.c
@@ -243,21 +243,3 @@ int subtraction(int num1, int num2)
     aux = num1 - num2;
     return aux;
 }
-
-/* função para converter segundos em horas */
-
-int sec_hours(int sec)
-{
-	int hour;
-	hour = sec/3600;
-	return hour;
-}
-
-/* função que converte segundos em minutos */
-
-int sec_minutes(int sec)
-{
-	int min;
-	min = sec/60;
-	return min;
-}
