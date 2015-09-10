a2eeaf3838a9631d9daeeb5a3dca3897a66e88d0
Botinelly <lucas_botinelly@hotmail.com>
2015-09-10 14:57:53 -0400

    Chamando a função já presente no Util.c

--- trabalho2/lista2/lista2.07/lista2.07.c
+++ trabalho2/lista2/lista2.07/lista2.07.c
@@ -13,24 +13,13 @@
  *          Wilson Calisto 
  */
 #include<stdio.h>
-
-int perfect(int num)
+#include"util.c"
+int main(int argc, const char *argv[])
 {
-	int num, cont=1, comp;
-	while(num-1>cont);/*percorre até o próprio número -1, pois o próprio numero não conta para a verificação do número perfeito*/
-	{
-		if (num%cont==0)
-		{
-			comp+=cont;		
-		}
-	cont++
-	}		
-	if(comp==num)/*se o comp, que seria a soma dos divisores for igual ao numero, retorne TRUE*/
-	{
-		return 1;
-	}
-	else/*Se não, retorne Falso*/
-	{
-		return 0;
-	}
+	int num, perf;
+	printf("Digite o número que deseja saber se é perfeito : ");
+	scanf("%d", &num);
+	perf=is_perfect(num);
+	printf("%d", perf);
+	return 0;
 }
