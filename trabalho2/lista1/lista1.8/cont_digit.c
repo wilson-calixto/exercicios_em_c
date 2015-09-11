99c66a2abedd7010de123a71e64227c2ac30d36f
borgesbsb <borges.bnjamin@gmail.com>
2015-09-11 12:37:35 -0400

    Separação de função não utilizada por correção no retorno da função

--- trabalho2/lista1/lista1.8/cont_digit.c
+++ trabalho2/lista1/lista1.8/cont_digit.c
@@ -1,11 +1,24 @@
 /*
-Contagem de dígitos.
-
-
-
-
-
 
 
 */
+int qtd_num(int num)
+{
+    int contador;
+    if ( num != 0  )
+    {
+        if ( num < 0 )
+        {
+           num = num*(-1);
+        }
+        for ( contador = 0; num >  0 ;  contador++  )
+        {
+            num = num / 10;
+        }
+        return contador;
+     }else
+     {
+            return 1;
+     }
+}
 
