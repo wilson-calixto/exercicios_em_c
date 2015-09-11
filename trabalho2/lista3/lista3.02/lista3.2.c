2c9d750be1bc8172f8532a94e5e8bc919c36257e
Neto OLiveira <netow@myself.com>
2015-09-11 15:27:28 -0400

    Comentando

--- trabalho2/lista3/lista3.02/lista3.2.c
+++ trabalho2/lista3/lista3.02/lista3.2.c
@@ -5,11 +5,11 @@
         {
                 int a,b,c,resultado;
 
-                printf("Digite 3 valores para calcular o delta:");
-                scanf("%i %i %i", &a,&b,&c);
+                printf("Digite 3 valores para calcular o delta:");  
+                scanf("%i %i %i", &a,&b,&c);  // recebe os 3 valores de entrada.
 
-                resultado=delta(a,b,c);
+                resultado=delta(a,b,c);   // faz o processamento na funçao
 
-		printf("O resultado de delta é:%d\n", resultado);
+		printf("O resultado de delta é:%d\n", resultado);      //exibe o delta
 	}
 
