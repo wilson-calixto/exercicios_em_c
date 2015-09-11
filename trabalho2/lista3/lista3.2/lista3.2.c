5d966916f9700e0e6f20406bb20dec9dc4d629fb
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-11 01:27:35 -0400

    corrigindo variavel e adcionado Makefile

--- trabalho2/lista3/lista3.2/lista3.2.c
+++ trabalho2/lista3/lista3.2/lista3.2.c
@@ -3,13 +3,13 @@
 
 	int main( int argc,const char *argv[])
         {
-                int a,b,c;
+                int a,b,c,resultado;
 
                 printf("Digite 3 valores para calcular o delta:");
                 scanf("%i %i %i", &a,&b,&c);
 
                 resultado=delta(a,b,c);
 
-		printf("%d", resultado);
+		printf("%d\n", resultado);
 	}
 
