1f3ecdd9771383cea8713de413fe2e9a7ff3114d
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-09-11 10:13:00 -0400

    questao 22 comentada

--- trabalho2/lista2/lista2.22/Calcula_Fatorial_Inverso.c
+++ trabalho2/lista2/lista2.22/Calcula_Fatorial_Inverso.c
@@ -18,11 +18,11 @@ Equipe:
 
 int main(int argc, const char *argv[])
 {
-	int n;
+	int n; //numero
 
 	puts("Informe um numero:");
-	scanf("%d",&n);
+	scanf("%d",&n); //pede ao usuario o ultimo 'n' da sequencia s = 1 + 1/1! + 1/2! +...+ 1/n!
 
-	printf("O resultado é %1.2f\n",(float) calc_num(n));	
+	printf("O resultado é %1.2f\n",(float) calc_num(n));//Informa ao usuario o resultado do calculo mostrado acima.	
 	return 0;
 }
