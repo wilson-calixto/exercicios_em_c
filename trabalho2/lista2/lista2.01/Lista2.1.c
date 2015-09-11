f4a70ac2b9072c60f7d7ed24d33827e4ebdc0244
AndressaMoreira <dreka.moreira@gmail.com>
2015-09-11 15:26:14 -0400

    Lista2 Questao 1 usuario correto

--- trabalho2/lista2/lista2.01/Lista2.1.c
+++ trabalho2/lista2/lista2.01/Lista2.1.c
@@ -1,28 +1,24 @@
 /*Programacao de Computadores e Algoritmo / Laboratorio de Programacao de Computadores
-*
-*Trabalho 2
-*Lista 2
-*Questao 1 
-*Equipe:
-*	Andressa Moreira
-*	Elizabeth Castro
-*	Juliany Raiol
-*	Luana Andrade
-*	Wilson Neto
+
+Trabalho 2
+Lista 2
+Questao 1 
+Equipe: Andressa Moreira
+	Elizabeth Castro
+	Juliany Raiol
+	Luana Andrade
+	Wilson Neto
 */
 
-//Chamando bibliotecas
 #include <stdio.h> 
 #include "util.h"
 
-//Construcao usual da funcao main()
 int main(int argc, char const *argv[])
 {
 	float raio, volume1;
 	printf("Insira o valor do raio: \n");
 	scanf("%f",&raio);
 
-//Chamando a funcao volume(), referente ao arquivo volume_esfera
 	volume1 = volume(raio);
 
 	printf("O volume da esfera eh %.2f: \n", volume1);
--- trabalho2/lista2/lista2.01/volume_esfera.c
+++ trabalho2/lista2/lista2.01/volume_esfera.c
@@ -1,20 +1,17 @@
+
 /*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
-*
-*Trabalho 2
-*Lista 2
-*Questao 1
-*
-*Equipe:
-*	Andressa Moreira
-*	Elizabeth Castro
-*	Juliany Raiol
-*	Luana Andrade
-*	Wilson Neto
+
+Trabalho 2
+Lista 2
+Questao 1
+
+Equipe: Andressa Moreira
+	Elizabeth Castro
+	Juliany Raiol
+	Luana Andrade
+	Wilson Neto
 */
 
-//Definir uma funcao que encontre o volume de uma esfera
-
-//Chamando a funcao power do util.c
 float power(float x,float y)
 {
 	int i;
@@ -26,15 +23,12 @@ float power(float x,float y)
 	return k;
 }
 
-//Definindo funcao para encontrar o volume da esfera
+
 float volume (float raio)
 { 
-//Variavel principal
 	float V;
-//Variavel auxiliar para guardar o valor da potenciacao
 	float r;
 	r = power(raio,3);
-//Resolucao da formula do volume de uma esfera
 	V = (4*3.14*r)/3;
 	return V;
 }
