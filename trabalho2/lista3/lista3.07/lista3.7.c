8206bb5ac14cd03b1ed44cc36a542966199ba17a
Elizabeth Castro <elizabethsellycastro@hotmail.com>
2015-09-11 19:04:19 -0400

    adicionando a questão 7 da lista3

---
+++ trabalho2/lista3/lista3.07/lista3.7.c
@@ -0,0 +1,25 @@
+	#include <stdio.h>
+	#include "../../util.c/util.h"
+
+	int main (int argc,const char *argv[])
+	{
+
+		int n1,n2,n3,menor;
+		
+		printf("Digite três valores:\n");
+		scanf("%i %i %i", &n1,&n2,&n3);
+
+		menor=a;
+
+		if(lower_number(b,c)<menor)
+		{
+			printf("O menor: %i\n", menor);
+		}
+		else 		
+		{
+			printf("O menor é:%i\n", menor);
+		}
+	
+
+		return 0;		
+	}
