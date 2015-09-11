ae0068002d1577da399894635e045b432aa7737e
AndressaMoreira <dreka.moreira@gmail.com>
2015-09-11 15:29:44 -0400

    Update volume_esfera.c

--- trabalho2/lista2/lista2.01/volume_esfera.c
+++ trabalho2/lista2/lista2.01/volume_esfera.c
@@ -1,17 +1,18 @@
-
 /*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
-
-Trabalho 2
-Lista 2
-Questao 1
-
-Equipe: Andressa Moreira
-	Elizabeth Castro
-	Juliany Raiol
-	Luana Andrade
-	Wilson Neto
+*
+*Trabalho 2
+*Lista 2
+*Questao 1
+*
+*Equipe:
+*	Andressa Moreira
+*	Elizabeth Castro
+*	Juliany Raiol
+*	Luana Andrade
+*	Wilson Neto
 */
 
+//Chamando a funcao power do util.h
 float power(float x,float y)
 {
 	int i;
@@ -23,12 +24,16 @@ float power(float x,float y)
 	return k;
 }
 
-
+//Definindo a funcao para calcular o volume de uma esfera
 float volume (float raio)
 { 
+	//Variavel principal
 	float V;
+	//Variavel auxiliar para guardar o valor do raio elevado a 3
 	float r;
+	//Chamndo a funcao power() e definindo seus parametros
 	r = power(raio,3);
+	//Calculo do volume de uma esfera
 	V = (4*3.14*r)/3;
 	return V;
 }
