2215d31b0cd6236ef3f0415a9ac43b814a7146ab
borgesbsb <borges.bnjamin@gmail.com>
2015-09-11 17:21:20 -0400

    Criação da função main para soma de tres numeros.

---
+++ trabalho2/lista1/lista1.03/lista1.3.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lista1.3.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11-09-2015 16:56:22
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include         "soma.h"
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name: lista1.3.c 
+ *  Description: Somatorio de argumentos 
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+	float num1, num2, num3;
+	printf ( "Digite o primeiro numero");
+	scanf ( "%d", &num1 );
+	printf ( "Digite o segundo numero\n" );
+	scanf ( "%d", &num2 );	
+	printf ( "Digite o terceiro numero\n" );
+	scanf ( "%d", &num3 );
+	printf ( "A soma dos tres numeros  = %f\n", soma(num1,num2,num3) );
+	return 0;
+}				/* ----------  end of function main  ---------- */
