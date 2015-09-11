2986a2140c6e37f8f1a31d24063dbc4cf799c493
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-09-11 18:33:22 -0400

    renomenadopasta

---
+++ trabalho2/lista2/lista2.08/is_pos.c
@@ -0,0 +1,28 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 2
+ * lista2 questao 8.
+ * 
+ * Equipe:
+ * 
+ *      Ewerton
+ *      Hermann
+ *      Jailson
+ *      Lucas
+ *      Richardson
+ *      Wilson 	
+ */
+
+
+/* verifica se o numero e positivo */
+
+#include<stdio.h>
+int is_pos(int numero)
+{
+    int boolean=0;
+        if(numero>=0)
+        {
+           boolen=1;
+        }
+    return bolean;
+}		   
