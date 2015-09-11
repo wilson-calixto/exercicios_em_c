25fa4dd4a8c257d7c62674d1687f6b6fc881d3a9
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-09-11 10:01:57 -0400

    questao 17 comentada

--- trabalho2/lista2/lista2.17/Greater_value.c
+++ trabalho2/lista2/lista2.17/Greater_value.c
@@ -1,8 +1,8 @@
 int greater_value(int maior, int num)
 {
 
-	if(maior <= num)
-		maior = num;
+	if(maior <= num) //verifica se maior é menor que num
+		maior = num; //se for, 'maior' recebe 'num'
 
 	return maior;
 }
--- trabalho2/lista2/lista2.17/Values.c
+++ trabalho2/lista2/lista2.17/Values.c
@@ -25,25 +25,25 @@ Grupo:
 int main(int argc, const char *argv[])
 {
 
-	int num,cont,maior,menor;
+	int num,cont,maior,menor; //numero, contador
 
 	puts("Informe um numero:");
-	scanf("%d", &num);
+	scanf("%d", &num); //Pedindo que o usuario digite um numero
 
-	maior = num;
-	menor = num;
+	maior = num; //Para não declarar 'maior' e 'menor' com valores muito baixos ou altos, fiz com que eles tivessem o valor do
+	menor = num; //primeiro numero digitado pelo usuario.
 
-	for(cont = 1; cont<=49;cont++)
+	for(cont = 1; cont<=49;cont++) //cont até 49 pois o numero deve receber 50 valores e um ja foi recebido, então 50 - 1 = 49.
 	{
 		puts("Informe um numero:");
 		scanf("%d",&num);
 
-		maior = greater_value(maior,num);
-		menor = smaller_value(menor,num);
+		maior = greater_value(maior,num); //chamando a função greater_value para verificar se a variavel 'maior' é maior que 'num'
+		menor = smaller_value(menor,num); //chamando a função smaller_value para verificar se a variavel 'menor' é menor que 'num' 
 
 	}
 
-	printf("O maior valor é %d e o menor valor é %d",maior,menor);
+	printf("O maior valor é %d e o menor valor é %d",maior,menor); //mostra o maior e o menor valor digitados pelo usuario 
 
 	return 0;
 }
