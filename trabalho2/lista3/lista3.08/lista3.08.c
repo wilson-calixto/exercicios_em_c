d29174bff440ec665584729a617b4e1a1e8794c9
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-11 17:40:48 -0400

    refatorando codigo

--- trabalho2/lista3/lista3.08/lista3.08.c
+++ trabalho2/lista3/lista3.08/lista3.08.c
@@ -15,7 +15,7 @@
 #include<stdio.h>
 #include<stdlib.h> /*para utilizar a função rand() */
 #include<time.h> /*para usar o srand, enviando com "semente" o horário do sistema, para os valores não se repetirem*/
-int dice(void)
+int dice()
 {
 	srand(time(NULL));
 	return ("%d\n", rand() % 6);	
