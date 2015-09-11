7cba2a672b0550fe4f28100a32fb5895dcba2601
jailsonpj <jailsoncolares@gmail.com>
2015-09-11 18:58:59 -0400

    questao 1 ajustada

---
+++ trabalho2/lista1/lista1.01/lista1.1.c
@@ -0,0 +1,25 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 2
+ * lista1.1.c
+ *
+ * Equipe:
+ *
+ *          Ewerton
+ *          Hermann
+ *          Lucas
+ *          Jailson
+ *          Wilson
+ *          Richardson 
+ */
+
+#include<stdio.h>
+#include"function.h"
+int main (int argc,const char *argv[])
+{
+    int n;
+    printf("digite numero: "); /*numero que vai ser o tamanho da laço*/
+    scanf("%d",&n);
+    qnt_numbers(n); /*chamando a funçao*/
+  return 0;
+}
