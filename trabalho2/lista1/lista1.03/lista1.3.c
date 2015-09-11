835c7d2d24c405f31c0bce6be8b0c3990794ebb7
borgesbsb <borges.bnjamin@gmail.com>
2015-09-11 17:49:59 -0400

    Modificação no tipo de variável de saída para .2f

--- trabalho2/lista1/lista1.03/lista1.3.c
+++ trabalho2/lista1/lista1.03/lista1.3.c
@@ -17,7 +17,7 @@
  */
 
 #include         "soma.h"
-#include	<stdlib.h>
+#include	<stdio.h>
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -28,12 +28,12 @@
 int main ( int argc, char *argv[] )
 {
 	float num1, num2, num3;
-	printf ( "Digite o primeiro numero");
+	printf ( "Digite o primeiro numero\n");
 	scanf ( "%f", &num1 );
 	printf ( "Digite o segundo numero\n" );
 	scanf ( "%f", &num2 );	
 	printf ( "Digite o terceiro numero\n" );
 	scanf ( "%f", &num3 );
-	printf ( "A soma dos tres numeros  = %f\n", soma(num1,num2,num3) );
+	printf ( "A soma dos tres numeros  = %.2f\n", soma(num1,num2,num3) );
 	return 0;
 }				/* ----------  end of function main  ---------- */
