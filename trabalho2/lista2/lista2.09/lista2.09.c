0874ef8504ae3264075ebe7a0848f7f573729f8c
SergioPinheiro <sergio.a.a.p@hotmail.com>
2015-09-10 22:55:55 -0400

    Add lista2/lista2.09

---
+++ trabalho2/lista2/lista2.09/lista2.09.c
@@ -0,0 +1,19 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 
+ * lista2.09.c
+ *
+ * Equipe:
+ *          Sergio Pinheiro
+ *          Jacksonn Kevin (Head)
+ *          Manoel Victor
+ *          Delrick Oliveira
+ *          Gabriel Faraco
+ *          Rai Santos
+ *      
+ */
+#include <stdio.h>
+int main()
+{
+
+}
