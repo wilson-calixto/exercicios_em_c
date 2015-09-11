9455e716ea74402ebb5cfa2026d7294a19252ab8
AndressaMoreira <dreka.moreira@gmail.com>
2015-09-11 15:25:46 -0400

    Update Lista2.1.c

--- trabalho2/lista2/lista2.01/Lista2.1.c
+++ trabalho2/lista2/lista2.01/Lista2.1.c
@@ -1,24 +1,27 @@
 /*Programacao de Computadores e Algoritmo / Laboratorio de Programacao de Computadores
-
-Trabalho 2
-Lista 2
-Questao 1 
-Equipe: Andressa Moreira
-	Elizabeth Castro
-	Juliany Raiol
-	Luana Andrade
-	Wilson Neto
+*
+*Trabalho 2
+*Lista 2
+*Questao 1 
+*Equipe:
+*	Andressa Moreira
+*	Elizabeth Castro
+*	Juliany Raiol
+*	Luana Andrade
+*	Wilson Neto
 */
 
+//Chamando as bibliotecas
 #include <stdio.h> 
 #include "util.h"
 
+//Definindo a funcao main () usual
 int main(int argc, char const *argv[])
 {
 	float raio, volume1;
 	printf("Insira o valor do raio: \n");
 	scanf("%f",&raio);
-
+//Chamando a funcao volume() para calcular o volume da esfera
 	volume1 = volume(raio);
 
 	printf("O volume da esfera eh %.2f: \n", volume1);
