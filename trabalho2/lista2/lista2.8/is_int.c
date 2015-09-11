c020cedc0e910a594d93bb4483249be9b01cc002
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-09-11 18:20:36 -0400

    nova funcao e novo diretorio

---
+++ trabalho2/lista2/lista2.8/is_int.c
@@ -0,0 +1,22 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 2
+ * is_int.c
+ *
+ * Equipe:
+ *
+ */
+
+
+/* verifica se o numero e positivo */
+
+#include<stdio.h>
+int is_pos(int numero)
+{
+    int boolean=0;
+        if(numero>=0)
+        {
+           boolen=1;
+        }
+    return bolean;
+}		   
