50b32aebc01b816bd3f93c8949512b9d79028229
borgesbsb <borges.bnjamin@gmail.com>
2015-09-11 23:35:43 -0400

    Add função ao util geral

--- trabalho2/util.c/util.c
+++ trabalho2/util.c/util.c
@@ -284,3 +284,21 @@ float lowest(float a,float b, float c)
 		return c;
 	}
 }
+
+int conta_digitos (unsigned long int num1, int num2 )
+{
+	int i,contador;
+	while (num1 != 0)
+	{
+	      if ((num1 % 10) == num2)
+	     {
+                 contador += 1;
+             }
+             num1 = num1 / 10;
+        }
+	 return contador;
+}
+
+											}
+					}
+}
