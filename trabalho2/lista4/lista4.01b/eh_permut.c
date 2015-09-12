74aefc029efed2dd11255867fc1a1ffaae4fbd91
borgesbsb <borges.bnjamin@gmail.com>
2015-09-12 06:41:03 -0400

    Primeira versao da funcção eh_permutação, ela está utilizando a função da questão anterior

---
+++ trabalho2/lista4/lista4.01b/eh_permut.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  contagem.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12-09-2015 06:26:19
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int eh_permut(int num1, num2)
+{
+
+	while (num2 != 0 )
+	{
+		switch (conta_digitos(num1,num2 % 10))
+		{
+			case 1:
+				num2 = num2 / 10;
+				break;
+			default:
+				return 0;
+		}
+	}
+	return 1; 
+}
