8cd641966668a23e18c287ce818ede9aa0cacf32
borgesbsb <borges.bnjamin@gmail.com>
2015-09-12 06:14:20 -0400

    Inserção do arquivo main da questão b.

---
+++ trabalho2/lista4/lista4.01b/lista4.01.b.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lista4.01.b.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12-09-2015 06:11:19
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+	printf ( "Digite o primeiro numero\n" );
+	scanf ( "%d", &num1 );
+	printf ( "Digite o segundo numero\n" );
+	scanf ( "%d", &num2 );
+	
+
+	return 0;
+}				/* ----------  end of function main  ---------- */
