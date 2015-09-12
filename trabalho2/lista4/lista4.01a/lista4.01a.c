fb85f02a7ac30777e745269314ce39865c834866
borgesbsb <borges.bnjamin@gmail.com>
2015-09-12 19:20:30 -0400

    Ajuste no parametros no printf

--- trabalho2/lista4/lista4.01a/lista4.01a.c
+++ trabalho2/lista4/lista4.01a/lista4.01a.c
@@ -1,21 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  lista4.01a.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  11-09-2015 22:13:09
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
-
 #include <stdio.h>
 
 /* 
@@ -31,7 +13,7 @@ int main ( int argc, char *argv[] )
 	scanf ( "%lu", &num1 );
 	printf ( "Digite o segundo  numero\n" );
 	scanf ( "%lu", &num2 );
-	printf ( "O numero %d aparece no numero  %d vezes\n", num2 , conta_digitos(num1,num2) );
+	printf ( "O numero %lu aparece no numero  %d vezes\n", num2 , conta_digitos(num1,num2) );
 
 	return 0;
 }				/* ----------  end of function main  ---------- */
