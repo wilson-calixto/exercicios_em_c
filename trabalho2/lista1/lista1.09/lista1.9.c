76c2266c3045fd4b494436054bdcf71af980ee9b
Neto Oliveira <netow@myself.com>
2015-09-11 21:14:10 -0400

    alterando nome de biblioteca

--- trabalho2/lista1/lista1.09/lista1.9.c
+++ trabalho2/lista1/lista1.09/lista1.9.c
@@ -1,4 +1,4 @@
-/*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
+﻿/*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
  *
  * Trabalho 2
  * Lista 1
@@ -13,7 +13,7 @@
  * * / */
  
 #include <stdio.h>
-#include "util.h"
+#include "reverse_number.h"
 
 int main()
 {
