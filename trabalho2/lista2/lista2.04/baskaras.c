fe16c48c670841a10c75eadf9402577f933c8234
borgesbsb <borges.bnjamin@gmail.com>
2015-09-12 18:31:08 -0400

    Correção no bug de compilação

--- trabalho2/lista2/lista2.04/baskaras.c
+++ trabalho2/lista2/lista2.04/baskaras.c
@@ -1,7 +1,12 @@
-void bhaskara(float a, float b, float c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void baskaras(float a, float b, float c)
 {
-	float x1, x2, delta; //declara as variaveis
-	
+	float x1=0, x2=0, delta=0, sqrdelta; //declara as variaveis
+		
 	if(a == 0) //se a for igual a zero
 	{
 		printf("Nao e uma funcao do segundo grau. /n"); 
@@ -9,7 +14,7 @@ void bhaskara(float a, float b, float c);
 	else
 	{
 		delta = (b*b)-4*a*c; //formula de delta
-		
+		sqrdelta = sqrt(delta);
 		if(delta<0) //se delta for manor do que zero
 		{
 			printf("Nao ha raiz, pois delta menor do que 0 \n"); //neste caso a equacao nao tera raizes reais
@@ -17,16 +22,16 @@ void bhaskara(float a, float b, float c);
 		else if(delta == 0) //se delta for igual a zero x1 sera igual a x2, sendo assim
 		{
 			
-			//a partir daqui, sera usada a funcao <sqrt> (que calcula a raiz quadrada) inclusa na biblioteca <math.h>
+			//a partir daqui, sera usada a funcao (que calcula a raiz quadrada) inclusa na biblioteca <math.h>
 
-			x1 = (-b + sqrt(delta))/(2*a); //formula para x1 (x linha) 
+			x1 = (-b + sqrdelta)/(2*a); //formula para x1 (x linha) 
 
 			printf("x1 e x2 = %.2f \n", x1);
 		}
 		else
 		{
-			x1 = (-b + sqrt(delta))/(2*a); //formula para x1
-			x2 = (-b - sqrt(delta))/(2*a); //formula para x2
+			x1 = (-b + sqrdelta)/(2*a); //formula para x1
+			x2 = (-b - sqrdelta)/(2*a); //formula para x2
 			
 			printf("x1 = %.2f \n", x1); //impressao das raizes
 			printf("x2 = %.2f \n", x2);
--- trabalho2/lista2/lista2.04/lista2.04.c
+++ trabalho2/lista2/lista2.04/lista2.04.c
@@ -11,25 +11,23 @@
 *
 * */
 
-#include "bhaskara.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, const char *argv[])
 
 {
-	int a,b,c; // declaracao de 3 variaveis
+	float a,b,c; // declaracao de 3 variaveis
 
 	printf("Digite o valor de a: \n");	
-	scanf("%d",&a);	//recebe o valor da variavel a
+	scanf("%f",&a);	//recebe o valor da variavel a
 
 	printf("Digite o valor de b: \n");
-	scanf("%d",&b); //recebe o valor da variavel b
+	scanf("%f",&b); //recebe o valor da variavel b
 
 	printf("Digite o valor de c: \n");
-	scanf("%d",&c); //recebe o valor da variavel c
+	scanf("%f",&c); //recebe o valor da variavel c
 
-	printf("As raizes da equacao sao: %.2f \n", float bhaskara(float a, float b, float c)); //imprime as raizes da equacao, chamando a funcao bhaskara
+	baskaras(a, b, c); //imprime as raizes da equacao, chamando a funcao bhaskara
 	return 0;
 }
