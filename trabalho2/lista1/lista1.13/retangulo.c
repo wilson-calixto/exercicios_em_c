b17978372680033a6daec0ed0febf899e6c27098
Neto OLiveira <netow@myself.com>
2015-09-11 22:17:55 -0400

    Update retangulo.c

--- trabalho2/lista1/lista1.13/retangulo.c
+++ trabalho2/lista1/lista1.13/retangulo.c
@@ -1,3 +1,19 @@
+/*Programacao de Computadores e Algoritmos / Laboratorio de Programacao de Computadores
+ *
+ * Trabalho 2
+ * Lista 1
+ * Questao 13
+ *
+ *\Equipe:  Elizabeth Castro
+ *          Andreza Moreira
+ *          Juliany Raiol
+ *          Luana Andrade
+ *          Wilson Neto
+ *
+ *     Autor: Juliany Raiol
+ *
+ * / */
+
 #include <stdio.h>
 
 void retangulo(int linhas, int colunas)
