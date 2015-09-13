bfcbaea3c24281c767ea1ac19ebb0c1b3c778f50
pflmarinho <pflmarinho@gmail.com>
2015-09-13 14:42:35 -0400

    comentando

--- trabalho2/lista1/lista1.03/soma.c
+++ trabalho2/lista1/lista1.03/soma.c
@@ -5,8 +5,8 @@
  *  Description:  Esta função irá fazer a soma de três numeros
  * =====================================================================================
  */
-float soma (float num1, float num2, float num3  )
+float soma (float num1, float num2, float num3  ) //declarando as variaveis da função
 {
-	return  num1+num2+num3;
+	return  num1+num2+num3; 
 }		/* -----  end of function soma  ----- */
 
