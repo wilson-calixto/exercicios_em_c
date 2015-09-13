e0f53872cf9d67f973f8d377538c663d257b789d
pflmarinho <pflmarinho@gmail.com>
2015-09-13 14:28:45 -0400

    adicionando comentarios

--- trabalho2/lista1/lista1.01/lista1.1.c
+++ trabalho2/lista1/lista1.01/lista1.1.c
@@ -14,12 +14,12 @@
  */
 
 #include<stdio.h>
-#include"qnt_numbers.h"
+#include"qnt_numbers.h" 
 int main (int argc,const char *argv[])
 {
     int n;
     printf("digite numero: "); /*numero que vai ser o tamanho da laço*/
-    scanf("%d",&n);
+    scanf("%d",&n); //lendo a variavel
     qnt_numbers(n); /*chamando a funçao*/
   return 0;
 }
