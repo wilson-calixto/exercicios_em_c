e66807c7458a45c0c52aeb4f094f51ec827288d1
pflmarinho <pflmarinho@gmail.com>
2015-09-13 14:00:38 -0400

    atualizando

--- trabalho2/lista3/desafio4/lista3.d4.c
+++ trabalho2/lista3/desafio4/lista3.d4.c
@@ -12,29 +12,8 @@
  * *     
  * *
  * */
-
-
-
-
-
 #include <stdio.h>
-#include "util.h"
-
-int reverse(int n)
-{
-    int i, j, r, result, div;
-    for(i=10; n/i>=1; i=i*10)
-    {
-        div=i;
-    }
-    for(j=div; j>=1; j=j/10)
-    {
-        r=n%j;
-        result = result+((n/j)*(div/j));
-        n=r;
-    }
-    return n;
-}
+#include "reverse.h"
 int main (int argc, const char *argv[])
 {
     reverse(123);
