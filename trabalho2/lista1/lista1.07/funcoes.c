15cc2c8d5a7ade76e1c904867049e5603733fac5
pflmarinho <pflmarinho@gmail.com>
2015-09-13 15:01:01 -0400

    padronizando

--- trabalho2/lista1/lista1.07/funcoes.c
+++ trabalho2/lista1/lista1.07/funcoes.c
@@ -1,20 +1,31 @@
 /* Esta função recebe o valor de uma prestação e a qtde de dias de atraso. 
 Retorna o valor da prestação mais multa de 3% do valor, mas 1% por cada dia de atraso.*/
 
-float valor_pagamento(float x, float y){
+float valor_pagamento(float x, float y)
+{
+    int result=0;
     if (y==0)
+    {
         return x;
+    }
     else
-        return x+(x*0.03)+x*(0.01*y);
+    {
+        result = x+(x*0.03)+x*(0.01*y);
+        return result;
+    }
 }
 
 
 /* Esta função recebe um número e retorna  qtde de divisores deste número.*/
-int div_cont(x){
+int div_cont(int x)
+{
     int i,cont=0;
-    for (i=1;i<=x;i++){
+    for (i=1;i<=x;i++)
+    {
         if (x%i==0)
-        cont++;   
-   }
+        {
+            cont++; 
+        }
+    }
     return cont;
 }
