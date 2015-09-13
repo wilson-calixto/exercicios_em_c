1275e8ed7aa6456816d1c825fd5e5cc646ada119
pflmarinho <pflmarinho@gmail.com>
2015-09-13 15:07:19 -0400

    comentando

--- trabalho2/lista1/lista1.08/cont_digit.c
+++ trabalho2/lista1/lista1.08/cont_digit.c
@@ -7,7 +7,7 @@ int qtd_num(int num)
     int contador;
     if ( num != 0  ) //perguntando se o mumero e diferente de 0
     {
-        if ( num < 0 )
+        if ( num < 0 ) //aninhando o if perguntando se alem de diferente de 0, e menor
         {
            num = num*(-1);
         }
