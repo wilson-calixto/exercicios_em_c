59f132b2b7e66cfe0f14de3b9666a07ca4f3368d
pflmarinho <pflmarinho@gmail.com>
2015-09-14 00:27:47 -0400

    comentando

--- trabalho2/lista3/desafio4/reverse.c
+++ trabalho2/lista3/desafio4/reverse.c
@@ -6,8 +6,8 @@ int reverse(int num)
 
     while (num>0)
     {
-        rev=rev*10+num%10;
-        num=num/10;
+        rev=rev*10+num%10; ///multiplica o resto por 10 e soma com o resto sequinte
+        num=num/10; ///decrementando o numero de 10 em 10 vezes
     }
     return rev;
 }
