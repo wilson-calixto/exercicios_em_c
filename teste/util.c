3420070d834cc44fb20692c277aaa8e5075513cf
jacksonggomes <jackson.am2009@gmail.com>
2015-09-16 19:32:36 -0400

    Alteracao

--- teste/util.c
+++ teste/util.c
@@ -50,10 +50,11 @@ int teste_permutacao(int matriz[][],int n,int m)
     int  i, j, cont1=0, cont2=0, aux=1;
     for (i=0; i < n; i++)
     {
+        cont1=0;
+        cont2=0;
         for(j=0; j<m; j++)
         {
-            cont1=0;
-            cont2=0;
+            
             if(matriz[i][j]==0)
             {
                 cont1++;
@@ -63,10 +64,10 @@ int teste_permutacao(int matriz[][],int n,int m)
                 cont2++;
             }
         }
-    }
-    if (cont1!=(n-1) && (cont2!=1))
+        if (cont1!=(n-1) && (cont2!=1))
     {
         aux=0;
+    }
     }
 
     return aux;
