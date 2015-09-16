700ad8a3e1a48a5d84c5ad327e23f5c8dea11c4e
AndressaMoreira <dreka.moreira@gmail.com>
2015-09-16 16:45:41 -0400

    questao

---
+++ teste/lista2.01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+int main()
+{
+
+	int tam=0,n=0;	
+	printf("Tamanho da sequencia: ");
+	scanf("%d",&tam);
+	printf("Digite a sequencia correspondente ao tamanho anterior: ");
+	scanf("%d",&n);
+	printf("\n\n");
+	reverse = inverso(n,tam);
+
+	printf("Numero inverso: ",reverse);
+
+	printf("\n\n\n");
+
+	return 0;
+}
