b0572cd80043fed9df0a39bcffad6895defce28f
faracogabriel <gabriel.faraco@hotmail.com>
2015-09-16 17:29:24 -0400

    atualizando util.c

--- teste/util.c
+++ teste/util.c
@@ -4,20 +4,20 @@
 
 void select_sort(int *x)
 {
-	int i, j, aux;
-	for (i=0;i<5;i++)
-	{
-		for (j=0;j<5;j++)
-		{
-			if (x[i]<x[j])
-			{
-				aux=x[i];
-				x[i]=x[j];
-				x[j]=aux;				
-			}
-		}
-	}
-		
+    int i, j, aux;
+    for (i=0;i<5;i++)
+    {
+        for (j=0;j<5;j++)
+        {
+            if (x[i]<x[j])
+            {
+                aux=x[i];
+                x[i]=x[j];
+                x[j]=aux;				
+            }
+        }
+    }
+
 }
 /* funcao recebe dois vetores,o numero de loops (len do vetor) e calcula seu produto */
 void produto_escalar(int *vetor1,int *vetor2,int loops)
@@ -29,7 +29,7 @@ void produto_escalar(int *vetor1,int *vetor2,int loops)
 
         printf ( "O produto de x[%d] por y[%d] e : %d \n",i+1,i+1,produto );   
     }
-   
+
 }
 
 //-------------------------------------------------------------------------------
@@ -38,36 +38,36 @@ int inverso(int *vetor, int s)
     int i=0;
     for (i=s-1;i>=0;i--)
     {
-        
+        return vetor[i];  
     }
 
-    return vetor;
+   
 }
 
-//fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+//------------------------------------------------------------------------------
 int teste_permutacao(int matriz[][],int n,int m)
 {
-	int  i, j, cont1=0, cont2=0, aux=1;
-	for (i=0; i < n; i++)
-	{
-		for(j=0; j<m; j++)
-		{
-			cont1=0;
-			cont2=0;
-			if(matriz[i][j]==0)
-			{
-				cont1++;
-			}
-			if(matriz[i][j]==1)
-			{
-				cont2++;
-			}
-		}
-	}
-	if (cont1!=(n-1) && (cont2!=1))
-	{
-		aux=0;
-	}
-	
-        return aux;
-	}
+    int  i, j, cont1=0, cont2=0, aux=1;
+    for (i=0; i < n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            cont1=0;
+            cont2=0;
+            if(matriz[i][j]==0)
+            {
+                cont1++;
+            }
+            if(matriz[i][j]==1)
+            {
+                cont2++;
+            }
+        }
+    }
+    if (cont1!=(n-1) && (cont2!=1))
+    {
+        aux=0;
+    }
+
+    return aux;
+}
