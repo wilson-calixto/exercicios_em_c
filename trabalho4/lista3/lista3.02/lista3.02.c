e15da12ebb6dadc5583e9cb7798803cab2eeb00f
JulianyRaiol <julianyraiol@outlook.com>
2015-09-16 10:40:15 -0400

    Questão 2 da lista 3 incompleta

--- trabalho4/lista3/lista3.02/lista3.02.c
+++ trabalho4/lista3/lista3.02/lista3.02.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#define MAXI 100
+#define MAXI 100 //Constante
 
 int main(int argc, char const *argv[])
 {
 	int i,n, m;
-	int x[MAXI]
+	int x[MAXI];
 	
 	printf("Digite os valores de N e M: ");
 	scanf("%d%d",&n,&m);
+	
 	printf("Digite os valores do vetor X: ");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&x[i]);
 	}
+	
+	
 		
 	return 0;
 }
