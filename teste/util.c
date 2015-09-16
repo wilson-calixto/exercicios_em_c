6c5fb3a57991a672e1a72d576e5e4f12f34244d4
Botinelly <lucas_botinelly@hotmail.com>
2015-09-16 16:25:15 -0400

    lista1.09.c

--- teste/util.c
+++ teste/util.c
@@ -1,1 +1,19 @@
 #include <stdio>
+
+void select_sort(int *x)
+{
+	int i, j, aux;
+	for (i=0;i<5;i++)
+	{
+		for (j=0;j<5;j++)
+		{
+			if (x[i]<x[j])
+			{
+				aux=x[i];
+				x[i]=x[j];
+				x[j]=aux;				
+			}
+		}
+	}
+		
+}
