7c47b2a9b0572041d0d35efa1e3bb2c73c720122
jackkssi <jkdsgg@gmail.com>
2015-09-16 18:52:28 -0400

    Update lista3.5.c

--- teste/lista3.5.c
+++ teste/lista3.5.c
@@ -11,7 +11,13 @@
 		printf("Digite a quantidade de colunas da matriz A:\n");
 		scanf("%d", &n);
 		
-		le_matriz(A, m, n);
+		for(i=0;i<m;i++)
+		{
+			for(j = 0;j<n;j++)
+			{
+				scanf("%d",&A[i][j]);
+			}
+		}
 		if(teste_permutacao(A,m,n)==1)
 		{
 			printf("é permutação!");
@@ -20,4 +26,3 @@
                }
 	return 0;
 }
-
