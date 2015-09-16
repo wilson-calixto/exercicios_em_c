e40f4ed879f9359866f15c2b344d6ee5905dc811
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-15 23:08:12 -0400

    Update util.c

--- trabalho4/util.c/util.c
+++ trabalho4/util.c/util.c
@@ -6,5 +6,9 @@ void read_array(int n, int v[n])
 	int cont; //contador
 
 	for(cont = 0; cont < n; cont++)
+	{
+		printf("Informe o %dº valor do vetor: ",cont + 1);
 		scanf("%d", &v[cont]); //lê valores para o vetor
+	}
+		
 }
