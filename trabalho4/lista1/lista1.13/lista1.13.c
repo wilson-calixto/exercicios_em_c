6a4e2e2506774d0f843fbfb837bb7f25b0f1e4d4
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-09-15 22:10:37 -0400

    questao 13 da lista 1

---
+++ trabalho4/lista1/lista1.13/lista1.13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int x, *p, **q;
+
+	p = &x; //p = 0x0000x, *p = x;
+	q = &p; //q = 0x0000p, *q = 0x0000x, **q = 10; 
+	x = 10; //x = 10; 
+
+	printf("\n%d\n", **q); //o '&' estava pedindo para exibir o endereço de q, nao o valor de x;
+
+	return 0;
+}
