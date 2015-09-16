76e9a2771f54f31162035839c4999a67a2430a9c
faracogabriel <gabriel.faraco@hotmail.com>
2015-09-16 17:05:39 -0400

    atualizar util.h

--- teste/util.h
+++ teste/util.h
@@ -1,4 +1,4 @@
-int inverso(n,tam);
+int inverso(int *vetor, int s);
 int select_sort(int *x);
-void produto_escalar(int *vetor1,int *vetor2,int loops)
+void produto_escalar(int *vetor1,int *vetor2,int loops);
 
