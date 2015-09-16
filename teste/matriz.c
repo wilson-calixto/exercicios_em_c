37bdf1888ac877dffd84c7a0dbe1186b1ea838f3
jailson <jailsoncolares@gmail.com>
2015-09-16 17:02:15 -0400

    insert

---
+++ teste/matriz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int matriz(int n ,int m )
+{ 
+  int i,j;
+  int mat = [n][m];
+  for (i= 0; i<n;i++)
+  {
+    for(j=0;j<m;j++)
+     { 
+         scanf("%d",matriz[i][j]);
+     
+     }
+ 
+ 
+}
+
+int teste_permutacao(int matriz[][],int n,int m)
+{
+	int  i, j, cont1=0, cont2=0, aux=1;
+	for (i=0; i < n; i++)
+	{
+		for(j=0; j<m; j++)
+		{
+			cont1=0;
+			cont2=0;
+			if(matriz[i][j]==0)
+			{
+				cont1++;
+			}
+			if(matriz[i][j]==1)
+			{
+				cont2++;
+			}
+		}
+	}
+	if (cont1!=(n-1) && (cont2!=1))
+	{
+		aux=0;
+	}
+	
+        return aux;
+	}
+
