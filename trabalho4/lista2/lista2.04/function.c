d70e72034652bb29d780baba0955714133cace5c
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-09-15 22:44:31 -0400

    questao 4 lista 2

---
+++ trabalho4/lista2/lista2.04/function.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int produto_escalar(int n, int v1[n], int v2[n])
+{
+	int cont, acm = 0; //contador, acumulador;
+
+
+	for(cont = 0; cont < n; cont++)
+		acm += v1[cont] * v2[cont]; //acm acumula o produto escalar dos vetores
+
+	return acm;
+}
+
+void read_array(int n, int v[n])
+{
+	int cont; //contador
+
+	for(cont = 0; cont < n; cont++)
+		scanf("%d", &v[cont]); //lê um valor para o vetor
+}
