da7630459eac8d406a296c3edebafee9cdcdf7ac
Botinelly <lucas_botinelly@hotmail.com>
2015-09-16 16:28:39 -0400

    lista1.09.c MAIN

---
+++ teste/lista1.09.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "select.c"
+int main(int argc, const char *argv[])
+
+{
+	int pont, numeros[5], i;
+	for (i=0;i<5;i++)
+	{
+		scanf("%d", &numeros[i]);
+	}
+		
+	select_sort(numeros);
+	for (i=0;i<5; i++)
+	{
+		printf("%d ", numeros[i]);
+	}
+	printf("\n");
+	return 0;
+}
