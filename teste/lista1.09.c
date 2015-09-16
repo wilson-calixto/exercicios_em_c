cf61c5ee9ddfc0e9d4ccb8e1cc7bbe19642e00a1
Botinelly <lucas_botinelly@hotmail.com>
2015-09-16 16:51:24 -0400

    atualizando

--- teste/lista1.09.c
+++ teste/lista1.09.c
@@ -4,16 +4,18 @@ int main(int argc, const char *argv[])
 
 {
 	int pont, numeros[5], i;
-	for (i=0;i<5;i++)
+	for (i=0;i<5;i++)//Pede os 5 valores do vetor
 	{
+		printf("Digite um número do vetor: ");
 		scanf("%d", &numeros[i]);
 	}
 		
-	select_sort(numeros);
-	for (i=0;i<5; i++)
+	select_sort(numeros); //Chama a função para ordenar o vetor
+	printf("O vetor ordenado é: \n");
+	for (i=0;i<5; i++)//Imprime o vetor
 	{
-		printf("%d ", numeros[i]);
+		printf(" %d ", numeros[i]);
 	}
-	printf("\n");
+	printf("\n");//apenas vaidade
 	return 0;
 }
