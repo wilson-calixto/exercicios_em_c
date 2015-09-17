3a53109c4d70b7db1322c59c7b063edda9728e68
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-17 17:32:35 -0400

    Finalizando questão 07

--- trabalho4/lista4/lista4.07/lista4.7.c
+++ trabalho4/lista4/lista4.07/lista4.7.c
@@ -1,19 +1,37 @@
+/*
+ *  Programacao de Computadores e ALgoritmos
+ *  Trabalho 4
+ *  lista4.02
+ * 
+ *  Equipe:
+ *   Juliany Raiol
+ *   Lucas Botinelly
+ *   Luiz Brandão
+ *   Richardson Souza
+ */
+
 #include <stdio.h> 
 #include "sets_ab.h"
 
 int main ( int argc, const char *argv[] )
 {
     int n,m,*p,*q,result;
-
+    
     printf ( "Informe o numero de elementos de A: \n" );
+    // le a quatidade que A tera de n elementos
     scanf ( "%d", &n );
-    printf ( "Informe os elementos: \n" );
+    printf ( "Informe os elementos de A: \n" );
+    // chama a funcao para preencher o vetor e p aponta
     p=set_a(n);
 
     printf ( "Informe o numero de elementos de B: \n" );
+    // le a quantidade que B tera de m elementos
     scanf ( "%d", &m );
+    printf ( "Informe os elementos de B: \n" );
+    // chama a funcao para preencher o vetor e q aponta 
     q=set_b(m);
     
+    // recebe o resultado 1 ou 0
     result = a_contains_b(p,n,q,m);
 
     if (result==1)
@@ -24,8 +42,9 @@ int main ( int argc, const char *argv[] )
     {
         printf ( "Nao esta contido\n" );
     }
-
-    result= ab_equals_ba(p,n,q,m);
+    
+    // recebe o resultado 1 ou 0
+    result = ab_equals_ba(p,n,q,m);
 
     if (result==1)
     {
