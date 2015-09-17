679af17417661166a208c8f420722ecff6f0f93c
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-09-16 20:05:10 -0400

    comentando lista3.04.c e funçoes verifica e read_matriz

--- teste/lista3.04.c
+++ teste/lista3.04.c
@@ -20,14 +20,14 @@ int main(int argc, const char *argv[])
 {
     int a[100][100], m, n;
 
-    printf("Informe o valor de m: ");
+    printf("Informe o valor de m: ");	
     scanf("%d", &m);
     printf("Informe o valor de n: ");
     scanf("%d", &n);
 
-    read_matrix(m, n, a);
+    read_matrix(m, n, a); //lê a matriz 'a' com seu tamanho informado pelo usuario;
 
-    if(verifica(m, n, a))
+    if(verifica(m, n, a)) //verifica se há elementos iguais na matriz;
     {
         printf("Existe elementos repetidos.\n");
     }
