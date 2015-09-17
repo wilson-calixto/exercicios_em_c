ddb182b2712ea5616fdab7835b428ba048b2a7af
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-17 12:44:29 -0400

    trabalhando na questão 7 -lista 4

--- trabalho4/lista4/lista4.07/sets_ab.c
+++ trabalho4/lista4/lista4.07/sets_ab.c
@@ -29,9 +29,28 @@ int *set_b(int m)
     return b;
 }
 
-int a_contains_b(int *a, int *b)
+int a_contains_b(int *a,int n, int *b, int m)
 {
-    
+    int i, j, result;
+
+    result = 0;
+
+    for ( i = 0; i < n; i += 1 )
+    {
+        for ( j = 0; j < m; j += 1 )
+        {
+            if (a[i]==b[j])
+            {
+                result = 1;
+            }
+        }
+        if (result==0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 int ab_equals_ba(int *a, int *b)
