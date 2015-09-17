50e920e1a9c607acac2cbb086e8405c06844e8eb
AndressaMoreira <dreka.moreira@gmail.com>
2015-09-16 23:27:16 -0400

    Arrumando identação

--- trabalho4/lista3/lista3.08/lista3.08.c
+++ trabalho4/lista3/lista3.08/lista3.08.c
@@ -25,11 +25,13 @@ int main(int argc, char *argv[])
 	//o valor de num
 	int i=0,i2=0,num=8;
 
-	for(i = 0; i <= num; i++) {
+	for(i = 0; i <= num; i++) 
+	{
 
 		//inicio de linha
 		//tabulamos nela, é o numero total de linhas - o num da linha atual
-		for(i2 = 0; i2 <= i; i2++) {
+		for(i2 = 0; i2 <= i; i2++) 
+		{
 			//usamos 2 pois só vamos até 2 digitos
 			printf("%2d  ",comb(i,i2));
 		}
