66ad91d1fe6444a313aec81387b318b7831c3f2e
jackkssi <jkdsgg@gmail.com>
2015-09-16 20:15:15 -0400

    "fazendo"

---
+++ trabalho4/lista2/lista2.02/lista2.02.c
@@ -0,0 +1,25 @@
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * 
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos 
+ *          Jackson Kelvin
+ *          Wilson Calisto 
+ */
+
+
+#include <stdio.h> 
+#include "process.h"
+
+int main ( int argc, char *argv[] )
+{
+
+
+
+
+
+    
+    return 0;
+}
