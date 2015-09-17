2d508cabd3bfac3b69247c1cd36bb9a22e1f4963
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-17 02:09:24 -0400

    atualizando codigo

--- trabalho4/lista4/lista4.02/matrix.c
+++ trabalho4/lista4/lista4.02/matrix.c
@@ -3,31 +3,42 @@
 
 float **read_matrix(int m, int n)
 {
-    float **v;  //ponteiro para matriz
     int i, j;      //variavel auxiliar
+    float **v;
 
-    if (m<1 || n<1) //verifica parametros recebidos
-    {
-        printf ( "<<< Erro: Parametro invalidos >>>\n" );
-        return (NULL);
-    }
-    
-    //aloca linhas da matriz
-    v= (float **) malloc (m * sizeof(float *));
-    
-    //aloca colunas da matriz    
+    v = (float **)malloc(m * sizeof(float*));
     for ( i = 0; i < m; i++ )
     {
-        v[i]= (float *) malloc (n * sizeof(float));
-
+        v[i] = (float *) malloc (n * sizeof(float));
         for ( j = 0; j < n; j++ )
         {
             scanf ( "%f", &v[i][j] );
         }
-
     }
-    
-    return (v); // retorna o ponteiro para a matriz
+    return v;
 }
 
+void print_matrix(float **v, int m, int n)
+{   
+    int i,j;
 
+    for ( i = 0; i < m; i += 1 )
+    {    
+        for ( j = 0; j < n; j += 1 )
+        {
+            printf ( "%.2f \t\n", v[i][j] );
+        }
+        printf ( "\n" );
+    }
+}
+
+void clean_matrix(float **v, int n)
+{
+    int i;
+
+    for ( i = 0; i < n; i++ )
+    {
+        free(v[i]);
+    }
+    free (v);
+}
