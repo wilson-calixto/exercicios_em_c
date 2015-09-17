117a0ba51d7548a163e4ef05ed7547c8a2d09b0e
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-17 12:39:24 -0400

    Trabalhando a Questao 09 Lista3

---
+++ trabalho4/lista3/lista3.09/lista3.09.c
@@ -0,0 +1,30 @@
+
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * lista3.9.c
+ *
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos
+ *          Jackson Kelvin
+ *          Wilson Calisto
+ *          Ewerton Petillo
+ *          
+ */
+
+#include <stdio.h>
+#include "function.c"
+#define max 20
+
+int main()
+{
+    int mat[max][max], line, col, i, j, cont;
+
+    printf("Quantas linhas? \n ");
+    scanf("%d", &line);
+    printf("Quantas colunas? \n ");
+    scanf("%d", &col);
+    
+
+}
