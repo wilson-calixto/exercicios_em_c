7a3360ee8bdb7b2a64910876781d94be43279358
jacksonggomes <jackson.am2009@gmail.com>
2015-09-17 08:37:39 -0400

    aa

--- teste/matriz.c
+++ teste/matriz.c
@@ -3,16 +3,14 @@
 #define MAX 100
 void preencher(int A[MAX][MAX], int m, int n)
 { 
-  int i,j;
-  for (i= 0; i<m;i++)
-  {
-    for(j=0;j<n;j++)
-     { 
-         scanf("%d",&A[i][j]);
-     
-     }
- 
- }
+ 	int i,j;
+ 	for (i= 0; i<m;i++)
+	{
+    		for(j=0;j<n;j++)
+		{ 
+         		scanf("%d",&A[i][j]);
+     		}
+  	}
 }
 
 int teste_permutacao(int matriz[MAX][MAX],int m,int n)
@@ -20,8 +18,8 @@ int teste_permutacao(int matriz[MAX][MAX],int m,int n)
 	int  i, j, cont1=0, cont2=0, aux=1;
 	for (i=0; i < m; i++)
 	{
-        cont1=0;
-        cont2=0;
+        	cont1=0;
+        	cont2=0;
 		for(j=0; j<n; j++)
 		{
 			if(matriz[i][j]==0)
@@ -32,14 +30,14 @@ int teste_permutacao(int matriz[MAX][MAX],int m,int n)
 			{
 				cont2++;
 			}
-    	 }
+    	  	}
         
-	    if (cont1!=(n-1) || (cont2!=1))
-	    {
-		    aux=0;
-	    }
-    }
+	  	if (cont1!=(n-1) || (cont2!=1))
+	    		{
+		    		aux=0;
+	 		}
+   		}
 	
-        return aux;
-	}
+	return aux;
+}
 
