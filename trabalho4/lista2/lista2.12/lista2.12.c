7bcbf91f2c7d393225c9e0dfbb371a4fa33ba7bb
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-17 17:57:15 -0400

    Implementado lista2 q12

---
+++ trabalho4/lista2/lista2.12/lista2.12.c
@@ -0,0 +1,31 @@
+
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * lista2.12.c
+ *
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos
+ *          Jackson Kelvin
+ *          Wilson Calisto
+ *          Ewerton Petillo
+ *          
+ */
+#include<stdio.h>
+#include "function.h"
+#define MAX 10
+
+int main
+{
+    int n, vet[n], i;
+
+    printf("Digite o grau do polimonio: \n");
+    scanf("%d", &n);
+    for (i=0; i<n; i++)
+    {
+        printf("Coeficiente de A[%d]: \n", i);
+        scanf("%d", &vet[i]);
+    }
+}
+
