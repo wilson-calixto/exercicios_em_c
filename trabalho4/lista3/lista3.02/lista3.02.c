1c6014d786de042d03957640b6d05fb2fcf73aff
JulianyRaiol <julianyraiol@outlook.com>
2015-09-17 00:11:23 -0400

    Alterações na questão 2 da lista 3

--- trabalho4/lista3/lista3.02/lista3.02.c
+++ trabalho4/lista3/lista3.02/lista3.02.c
@@ -4,18 +4,34 @@
 int main(int argc, char const *argv[])
 {
 	int i,n, m;
-	int x[MAXI];
+	int x[MAXI], b[MAXI], a[MAXI][MAXI];
 	
+	//Inserindo o número de linhas e colunas
 	printf("Digite os valores de N e M: ");
 	scanf("%d%d",&n,&m);
 	
+	//Leitura do vetor X
 	printf("Digite os valores do vetor X: ");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&x[i]);
 	}
 	
+	//Leitura do vetor B
+	printf("Digite os valores do vetor B: ");
+	for(i=0;i<b;i++)
+	{
+		scanf("%d",&b[i]);
+	}
 	
-		
+	//Leitura da matriz
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			scanf("%d%d",&a[i][j]);
+		}
+	}
+			
 	return 0;
 }
