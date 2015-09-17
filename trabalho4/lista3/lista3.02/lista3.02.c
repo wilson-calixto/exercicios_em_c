ebae9b1093b8f1449cf09491e0dfcfa223e9aa48
JulianyRaiol <julianyraiol@outlook.com>
2015-09-17 00:48:39 -0400

    Completando a questão

--- trabalho4/lista3/lista3.02/lista3.02.c
+++ trabalho4/lista3/lista3.02/lista3.02.c
@@ -5,7 +5,7 @@
 int main(int argc, char const *argv[])
 {
 	int i,n, m;
-	int x[MAXI], b[MAXI], a[MAXI][MAXI];
+	float x[MAXI], b[MAXI], a[MAXI][MAXI], resultado = 0.0;
 	
 	//Inserindo o número de linhas e colunas
 	printf("Digite os valores de N e M: ");
@@ -18,12 +18,26 @@ int main(int argc, char const *argv[])
 	printf("Digite os valores do vetor B: ");
 	read_array(m, b[m]);
 	
-	//Leitura da matriz
+	//Leitura da matriz A
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<m;j++)
 		{
-			scanf("%d%d",&a[i][j]);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j = 0; j < m;j++)
+		{
+			resultado = a[i][j]*x[i];
+			if( resultado == b[j] )
+			{
+				printf("Para o valor %f de A e %f de B, %f de X satisfaz o problema.\n",a[i][j],b[j],x[i]);
+			}
+			else printf("Para o valor %f de A e %f de B, não %f de X satisfaz o problema.\n",a[i][j],b[j],x[i]);
+			
 		}
 	}
 			
