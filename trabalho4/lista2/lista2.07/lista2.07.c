16e305dabed01a48196cd7cb312713743f6228ab
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-09-17 15:25:18 -0400

    refatorando o codigo

--- trabalho4/lista2/lista2.07/lista2.07.c
+++ trabalho4/lista2/lista2.07/lista2.07.c
@@ -17,23 +17,23 @@
 #include "funcao.h"
 int main()
 {
-	float *vetor,*nvetor,procurado;
-	int i,e,indice,loops,vezes_q_apareceu,re;
+	float *vetor,*numeros_q_repetiram,procurado;
+	int i,e,indice,loops,vezes_q_apareceu,resposta;
 	
 	/* pede o numero de loops */
-	printf("digite os loops: ");    	
+/*	printf("digite os loops: ");    	*/
 	scanf("%d",&loops);
 	vetor = (float*) ( malloc( sizeof(float) * loops ) );//vetor[loops]
-	nvetor = (float*) ( malloc( sizeof(float) * loops ) );//vetor[loops]
+	numeros_q_repetiram = (float*) ( malloc( sizeof(float) * loops ) );//vetor[loops]
 	/* carrega os numeros no vetor */
 	for (i=0;i<loops;i++ )
 	{
-		printf ("Digite o numero %d: ",i+1);
+	/*	printf ("Digite o numero %d: ",i+1);*/
 		scanf ("%f", &vetor[i]);
-		nvetor[i]=388888888888888;
+		numeros_q_repetiram[i]=3888859555888;
 	}
     
-	
+    /* a letra e é o indice do vetor chamdo numeros_q_repetiram */	
    
 	e=0;
 	/* pega um numero e mostra na tela quantas vezes ele se apareceu  */
@@ -41,22 +41,22 @@ int main()
 	{
 
 		procurado=vetor[i];
-		re=qnt_ap(nvetor,loops,procurado);
+		resposta=qnt_ap(numeros_q_repetiram,loops,procurado);
 			/* se o numero não esta no novo vetor, o programa o isere lá e mostra ao usuario qnt vzs ele apareceu no vetor original*/
-		if(re==0)
+		if(resposta==0)
 		{
-			nvetor[e]=procurado;
+			numeros_q_repetiram[e]=procurado;
 			e++;				
 			/* chama a funcao que devolve quantas vezes esse numero apareceu  no vetor original */
 		    vezes_q_apareceu = qnt_ap(vetor,loops,procurado);
 			/*  mostra o numero e quantas vezes o numero apareceu no vetor original*/
 			if( vezes_q_apareceu==1)
 			{
-				printf ( "o numero %.2f ocorre : %d vez\n",vetor[i],vezes_q_apareceu );
+				printf ( "%.1f ocorre %d vez\n",vetor[i],vezes_q_apareceu );
 			}
 			else
 			{
-				printf ( "o numero %.2f ocorreu : %d vezes\n",vetor[i],vezes_q_apareceu );
+				printf ( "%.1f ocorre %d vezes\n",vetor[i],vezes_q_apareceu );
 			}
       	
          }
