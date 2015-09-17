3bad17ef10367880fca2b8e733509f2aac33e58f
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-09-17 16:13:31 -0400

    melhorando os comentarios

--- trabalho4/lista3/lista3.14.c
+++ trabalho4/lista3/lista3.14.c
@@ -24,14 +24,15 @@ int calcula_custo(int **matriz,int *itinerario,int ordem);
     int
 main ( int argc, char *argv[] )
 {
-    int **matriz,*itinerario,custo=0,ordem,i;
+    int **matriz,*itinerario,custo=0,ordem,i,tamanho_do_vetor=0;
     
     /* pede a ordem da matriz */
-   /* printf ( "Digite a ordem da matriz\n" );*/
+  
     scanf("%d",&ordem);
 
 	/* inicia o vetor com o dobro da ordem da matriz*/    
-    itinerario=(int*)(malloc((ordem*2)*sizeof(int)));    
+	tamanho_do_vetor=ordem*2;
+    itinerario=(int*)(malloc(tamanho_do_vetor*sizeof(int)));    
 
     /*inicia a matriz*/
     matriz=(int**)malloc(ordem*sizeof(int*));  
@@ -47,7 +48,7 @@ main ( int argc, char *argv[] )
     completa_vetor(itinerario,ordem);
 
 	/* calcula o custo do itinerario*/
-    custo=calcula_custo(matriz,itinerario,ordem);
+    custo=calcula_custo(matriz,itinerario,tamanho_do_vetor);
     
     /* mostra o resultado final*/
     printf ( "%d\n",custo );
@@ -60,14 +61,14 @@ main ( int argc, char *argv[] )
 /*Dada uma matriz  quadrada e sua ordem, a funcao completa essa matriz */
 void pede_dados(int **matriz,int ordem)
 {
-	/*i sao as linhas e j sao as colunas, ordem e a ordem da matriz que e quadrada*/
+	
     int i,j;
-    /*printf("vc esta na matriz\n");*/
+    
     for (i=0;i<ordem;i++ )
     {   
         for (j=0;j<ordem ;j++ )
         {
-/*                printf ( "Digite um numero da matriz\n" ); */
+
                 scanf ( "%d", &matriz[i][j] );
         }    
      }
@@ -77,25 +78,28 @@ void pede_dados(int **matriz,int ordem)
 void completa_vetor(int *itinerario,int ordem)
 {
     int i;
-        ordem*=2;
-/*        printf("vc esta no vetor\n");*/
+    	/*o vetor tem o dobro da ordem da matriz por isso ele e modificada no escopo LOCAL da funcao */
+    ordem*=2;
+
     for ( i=0;i<ordem;i++ )
         {
-           /* printf ( "Digite o numero o vetor\n" );*/
+        
            scanf ( "%d", &itinerario[i] );      
         }
 }
 
 
 /* dado uma matriz quadrada, sua ordem e um vetor, a funcao retorna o custo do itinerario */
-int calcula_custo(int **matriz,int *itinerario,int ordem)
+int calcula_custo(int **matriz,int *itinerario,int tamanho_do_vetor)
 {   
-    int i,linha=0,coluna=0,acm=0,tamanho_max=0;
-        tamanho_max=((ordem*2)-1);
-     
-    for (i=0;i<tamanho_max;i++)
+    int i,linha=0,coluna=0,acm=0;
+    
+    /*o loop vai de 0 ate o tamanho_do_vetor -2 para que o endereco nao estore o vetor*/  
+    for (i=0;i<tamanho_do_vetor-1;i++)
     {
+    /*linha recebe o elemento do vetor na posicao atual */
         linha=itinerario[i];
+    /*coluna recebe o elemento vetor na proxima posicao */
         coluna=itinerario[i+1];
         acm+=matriz[linha][coluna];
     
