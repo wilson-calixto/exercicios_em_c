48ea33bce5825b769d73f157c661af6adb3b35d0
borgesbsb <borges.bnjamin@gmail.com>
2015-09-17 21:24:37 -0400

    Lendo uma matriz de strings (Tentando..)

--- trabalho4/lista3/lista3.13/lista3.13a.c
+++ trabalho4/lista3/lista3.13/lista3.13a.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  main
- *  Description:  
- * =====================================================================================
- */
 int main ( int argc, char *argv[] )
 {
-	
+	char **matriz_resultado;
+	int npartidas=0;
+	scanf("%d", &npartidas);
+	matriz_resultado = (char **) malloc (npartidas*sizeof(char*));
+	insere_vetor(npartidas, matriz_resultado);
+
 
 	return 0;
-}				/* ----------  end of function main  ---------- */
+}
