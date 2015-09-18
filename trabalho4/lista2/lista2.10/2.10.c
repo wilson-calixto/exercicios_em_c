b3e4ddbe0fd9ab94cb2b5558d8e47312572193bf
Botinelly <lucas_botinelly@hotmail.com>
2015-09-18 14:48:29 -0400

    Atualizando

--- trabalho4/lista2/lista2.10/2.10.c
+++ trabalho4/lista2/lista2.10/2.10.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+void polinome(int n, int (*vet))
+
+{
+	//p(x)=a0+a1x+a2x2**2+...+anx**n
+	int i, result = 0;
+	for(i=0;i<n;i++)
+	{
+		result+=vet[i];	
+	}
+	printf("%d.x**%d\n", result,n);
+}
