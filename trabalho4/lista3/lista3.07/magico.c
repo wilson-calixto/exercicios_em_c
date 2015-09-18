423dc6f47a301fd24ed8ea09ecd278ad78250ff0
JulianyRaiol <julianyraiol@outlook.com>
2015-09-18 10:58:40 -0400

    Alteração na questão 7

--- trabalho4/lista3/lista3.07/magico.c
+++ trabalho4/lista3/lista3.07/magico.c
@@ -6,7 +6,6 @@ void read_m(int n, int mat[][n])
 	    {
 		for (j=0; j<n; j++)
 		{
-		    printf("Digite o elemento[%d][%d]", i, j);
 		    scanf("%d", &mat[i][j]);
 		}
 	    }
@@ -18,7 +17,7 @@ int qmagico(int n, int matriz[][n])
 	int somal = 0, somac = 0, somap = 0, somas = 0, i, j;
 	for(i = 0; i < n;i++)
 	{
-		
+		somal+=matriz[i][j];
 		for(j = 0; j < n; j++)
 		{
 			somac+=matriz[i][j];
@@ -28,13 +27,12 @@ int qmagico(int n, int matriz[][n])
 			}
 			else 
 			{
-				if( i >= j )
+				if( i == j )
 				{
 					somas+=matriz[i][j];
 				}	
 			}
 		}
-		somal+=matriz[i][j];
 	}
 	
 	if( (somal == somac) && (somap == somas) && (somap == somal) ) return 1;
