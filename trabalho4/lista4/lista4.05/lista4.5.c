9ae82355694879e14f6f2065e4e36ac5d8420838
jacksonggomes <jackson.am2009@gmail.com>
2015-09-18 16:32:38 -0400

    insercao

---
+++ trabalho4/lista4/lista4.05/lista4.5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include"matriz.c"
+#define Max 100
+
+int main(int argc,const char *argv[])
+	{
+		int maior,n,i;
+		int A[MAX][MAX];
+		 int linha, coluna;
+
+		printf("Digite a quantidade de linhas da matriz A:\n");
+		scanf("%d",&n);
+		
+	preencher(A,n);
+
+        for (i = 0; i < (n*n); i++) {
+    		maior = devolveMaior(A, n, &linha, &coluna);
+    		printf ("%d em (%d, %d)\n", maior, linha, coluna);
+    		A[linha][coluna] = -1;
+ 	 }
+        return 0;
+}
