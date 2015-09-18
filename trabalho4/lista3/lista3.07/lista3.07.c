bdf7b14c514aa1c67c725274f4163f4a566d621f
JulianyRaiol <julianyraiol@outlook.com>
2015-09-18 11:23:39 -0400

    Alterações na questão 7 da lista 3

--- trabalho4/lista3/lista3.07/lista3.07.c
+++ trabalho4/lista3/lista3.07/lista3.07.c
@@ -19,11 +19,15 @@ int main()
 	int i, j, n;
 	int matriz[MAX][MAX];
 	
+	//Recebe a ordem da matriz
 	printf("Digite o tamanho da matriz: ");
 	scanf("%d",&n);
 	
+	//Lê os elementos para a matriz
+	printf("Digite os elementos da matriz\n");
 	read_m(n,matriz);
 	
+	//Verifica se é uma quadrado mágico
 	if(qmagico(n, matriz))
 	{
 		printf("A matriz é um quadrado mágico\n");
