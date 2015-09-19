f662e94f23b5354016228255d1d3186440591bdd
jacksonggomes <jackson.am2009@gmail.com>
2015-09-19 13:44:16 -0400

    alteracao

--- trabalho4/lista4/lista4.05/lista4.5.c
+++ trabalho4/lista4/lista4.05/lista4.5.c
@@ -1,3 +1,17 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * lista4.5
+ *
+ * Equipe:
+ *
+ *          Jackson Gomes
+ *          Paulo Marinho
+ *          Hermann
+ *          Elizabeth
+ *          Jailson
+ */
+
 #include <stdio.h>
 #include"util.h"
 #define Max 100
@@ -8,11 +22,10 @@ int main(int argc,const char *argv[])
 		int A[MAX][MAX];
 		 int linha, coluna;
 
-		printf("Digite a quantidade de linhas da matriz A:\n");
+		//printf("Digite a quantidade de linhas da matriz A:\n");
 		scanf("%d",&n);
 		
 	preencher(A,n);
-
         for (i = 0; i < (n*n); i++) {
     		maior = devolveMaior(A, n, &linha, &coluna);
     		printf ("%d em (%d, %d)\n", maior, linha, coluna);
