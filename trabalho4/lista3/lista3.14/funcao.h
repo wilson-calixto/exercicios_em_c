f09d2bff5385b243280cd7aa826e333ab7514263
raisantos <raisoledade@outlook.com>
2015-09-18 22:21:26 -0400

    funcao

---
+++ trabalho4/lista3/lista3.14/funcao.h
@@ -0,0 +1,5 @@
+void pede_dados(int **matriz,int ordem);
+void completa_vetor(int *itinerario,int ordem);
+int calcula_custo(int **matriz,int *itinerario,int ordem);
+void imprime_vetor(int *itinerario,int tamanho_do_vetor);
+
