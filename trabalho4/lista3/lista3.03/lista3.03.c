1d0bc1515371191bc5739a4a9077b7c469011c91
faracogabriel <gabriel.faraco@hotmail.com>
2015-09-18 20:24:14 -0400

    sending lista3.03

---
+++ trabalho4/lista3/lista3.03/lista3.03.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAX 100
+
+int main(int argc, const char *argv[])
+{
+	int a, b, c, d, i, j, l;
+	float X[MAX][MAX], Y[MAX][MAX], Z[MAX][MAX];	//matrizes
+
+	printf("digite o tamanho da primeira matriz:\n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+
+	printf("digite os numeros da primeira matriz:\n");
+	for (i=0; i<a; i++)
+	{
+		for (j=0;j<b;j++)
+		{
+			scanf("%f",&X[i][j]);	//leitura dos numeros da primeira matriz
+		}
+	}
+
+	printf("digite o tamanho da segunda matriz:\n");
+	scanf("%d",&c);
+	scanf("%d",&d);
+
+	if (b!=c)
+	{
+		printf("o produto não pode ser feito!\n");
+		
+	}else{
+
+		printf("digite os numeros da segunda matriz:\n");
+		for (i=0; i<c; i++)
+		{
+			for (j=0;j<d;j++)
+			{
+				scanf("%f",&Y[i][j]);	//leitura dos numeros da segunda matriz
+			}
+
+		}
+		
+		//imprimir funcao multiplica matriz
+
+
+	}
+	return 0;
+}
