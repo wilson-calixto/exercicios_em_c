ba7f062fd3efda1cb7cc88bf5290ceadec660bf5
Delrick-Oliveira <delricknunes@gmail.com>
2015-09-20 17:52:08 -0400

    ajeitando detalhes

--- trabalho4/lista2/lista2.04/lista2.04.c
+++ trabalho4/lista2/lista2.04/lista2.04.c
@@ -20,13 +20,14 @@ int main(int argc, const char *argv[])
 {
 	int *v1, *v2, n, cont; //vetor 1, vetor 2, numero de elementos, contador;
 
-	puts("Informe o numero de elementos:");
+	puts("Informe o tamanho do vetor:\n");
 	scanf("%d", &n);	
 
 	v1 = (malloc(sizeof(int) * n)); //alocar n endereços de memoria para o vetor 1;
 	v2 = (malloc(sizeof(int) * n)); //alocar n endereços de memoria para o vetor 2;
-
+    printf ( "Lendo o primeiro vetor...\n" );
 	read_array(n, v1); //vetor 1 recebe os valores;
+    printf ( "Lendo o segundo vetor...\n" );
 	read_array(n, v2); //vetor 2 recebe os valores;
 
 	printf("O produto escalar é: %d\n", produto_escalar(n, v1, v2));
