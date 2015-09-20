968e6a829b6d97e52be5b393d5398fcce573318f
eivandru <evandro-souza10@hotmail.com>
2015-09-20 14:29:00 -0400

    versao 1.0.0

---
+++ trabalho4/lista3/lista3.11/function.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  function.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  20/09/2015 14:20:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+
+void addcliente();
+void mostrar();
+void addopr(char);
+void mostraroprs();
