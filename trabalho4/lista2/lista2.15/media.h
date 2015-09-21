0bb4165bb3d145494c0436949c26304366bf2b70
Botinelly <lucas_botinelly@hotmail.com>
2015-09-21 17:49:16 -0400

    Pasta da questao 2.15

---
+++ trabalho4/lista2/lista2.15/media.h
@@ -0,0 +1,4 @@
+void read_notas(float *nota, float*peso, int aluno, int nota, int prova);
+void soma_peso(float*peso, int aluno, int nota, int prova);
+float soma_prova(float *nota, int prova);
+float media_ponderada(float *nota, float *peso, int aluno, int prova);
