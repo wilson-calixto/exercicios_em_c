7a2d5b2016f3fb854908a960fc616db5ad9f398e
jackkssi <jkdsgg@gmail.com>
2015-09-21 18:24:39 -0400

    "100%"

--- trabalho4/lista4/lista4.04/ff.c
+++ trabalho4/lista4/lista4.04/ff.c
@@ -22,6 +22,16 @@ void maker_matriz(int m,int n,int matriz[100][100])
     }
 }
 /*-------------------------------------------------------------*/
+void troca(int i,int j,int m,int n,int matriz[100][100])
+{
+	int b;
+    for(b = 0;b < n; b++)
+       {
+                
+	  change(&matriz[i][b],&matriz[j][b]);           
+	}
+}
+/*-------------------------------------------------------------*/
 void print(int m,int n,int matriz[100][100])
 {
 	int l,c;
