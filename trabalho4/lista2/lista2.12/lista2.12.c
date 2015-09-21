a73b7a4286733dddc5ce088a62e17c293cd0ecde
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-21 08:58:18 -0400

    Atualizando e Comentando o Codigo

--- trabalho4/lista2/lista2.12/lista2.12.c
+++ trabalho4/lista2/lista2.12/lista2.12.c
@@ -21,24 +21,23 @@ int main()
     int n, vet[MAX], i,j, x, p[MAX], ind, cont;
 
     printf("Digite o grau do polimonio: \n");
-    scanf("%d", &n);
+    scanf("%d", &n); //Verifica o grau do polinomio
     for (i=n; i>=0; i--)
     {
-        printf("Coeficiente de A[%d]: \n", i);
-        scanf("%d", &vet[i]);
+        printf("Coeficiente de A[%d]: \n", i); //Le os coeficientes do polinomio de tras pra frente
+        scanf("%d", &vet[i]); 
     }
-    printf("Digite um valor para X: \n" );
+    printf("Digite um valor para a,tal que (x-a = 0): \n" ); //Verifica m valores para "a" para efetuar a divisao dos polinomios 
     scanf("%d", &x);
-    ind = n;
+    ind = n; //variavel que vai pegar o indice inicial do polinomio(vetor)
     for(j=n-1;j>=0;j--)
     {
-        p[j] = polynomial(vet,p,j,x,ind);
+        p[j] = polynomial(vet,p,j,x,ind); //O novo vetor(resultado) recebe a funcao que usa o metodo de Briot Ruffini para dividir polinomio
     }
-    for (i=ind-1 ; i>=0; i--)
+    for (i=ind-1 ; i>=0; i--) // Impressao do polinomio(vetor) resultante da divisao
     {
         printf("%dx^%d + ", p[i], i);
     }
-    printf("\n");
 }
  
 
