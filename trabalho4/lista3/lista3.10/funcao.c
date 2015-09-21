5ac1d3dc4fc13b0e5965fb28ea5d2b5d3451374d
jailsonpj <jailsoncolares@gmail.com>
2015-09-21 10:26:06 -0400

    configurando cabeçalho

--- trabalho4/lista3/lista3.10/funcao.c
+++ trabalho4/lista3/lista3.10/funcao.c
@@ -5,12 +5,12 @@
  *
  * Equipe:
  *
- *          Ewerton
+ *          Elizabeth
  *          Hermann
- *          Lucas
+ *          Jackson
  *          Jailson
- *          Wilson
- *          Richardson 
+ *          Paulo
+ *           
  */
 #include <stdio.h>
 #include <stdlib.h>
