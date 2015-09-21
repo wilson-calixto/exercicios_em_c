986f5ed0725e95ad81f7150bb8bbe5f96f0b9013
jacksonggomes <jackson.am2009@gmail.com>
2015-09-21 15:41:21 -0400

    a

--- trabalho4/lista1/lista1.12/lista1.12.c
+++ trabalho4/lista1/lista1.12/lista1.12.c
@@ -13,22 +13,21 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-	int matrx [10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int *p;
-	p=&matrx[0];//Apontando para matrx[0]
+	char matrx[] = "120A120B";
+	char *p;
+	p=matrx;//Apontando para matrx[0]
 	
-	printf ("O elemento do vetor e: %p\n",p);//Endereço de memória de matrx[0]
-	printf ("O elemento do vetor e: %d\n",*p);//valor para onde o ponteiro p aponta
-	printf ("O elemento do vetor e: %p\n",&p); //Endereço de memória do próprio ponteiro p
-	printf ("O elemento do vetor e: %d\n",*(p+1));//Incrementando o matrx[0]+1
-	printf ("O elemento do vetor e: %p\n",(p+5));//Endereço de memória do quinto elemento
-	//printf ("O elemento do vetor e: %p\n",(p+A));
-	printf ("O elemento do vetor e: %d\n",*(p+2));//Incrementando o matrx[0]+2
-	
-
-   
+	printf ("O elemento do vetor é: %p\n",p);//Endereço de memória de matrx[0]
+	printf ("O elemento do vetor é: %c\n",*p);//valor para onde o ponteiro p aponta
+	printf ("O elemento do vetor é: %p\n",&p); //Endereço de memória do próprio ponteiro p
+	printf ("O elemento do vetor é: %c\n",*(p+1));//Incrementando o matrx[0]+1
+	printf ("O elemento do vetor é: %p\n",(p+5));//Endereço de memória do quinto elemento
+	//printf ("O elemento do vetor é: %c\n",(p+A));(Isso não rola, pois A não foi declarado.
+	printf ("O elemento do vetor é: %c\n",*(p+2));//Incrementando o matrx[0]+2
+	   
 	return(0);
 }
 
