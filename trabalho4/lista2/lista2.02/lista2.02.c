8b1c7cc533e0be2f844825f13063666977f70b18
jackkssi <jkdsgg@gmail.com>
2015-09-21 16:07:47 -0400

    "versao final"

--- trabalho4/lista2/lista2.02/lista2.02.c
+++ trabalho4/lista2/lista2.02/lista2.02.c
@@ -16,15 +16,15 @@
 int main ( int argc, char *argv[] )
 {
 	int n, i, aluno, pontos,lol;
-	char gabarito[10];/*gabarito da prova*/
-	char resp[10];/*respostas do aluno corrente*/
+	char gabarito[30];/*gabarito da prova*/
+	char resp[30];/*respostas do aluno corrente*/
   
 	printf("Digite o numero de alunos:\n");
 	scanf("%d", &n);
  	
 	printf("Obs.:o gabarito deverá ser constituido das letras: a,b,c,d,e\n");
 	printf("Digite a letra das questões do gabarito da prova:\n");
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 30; i++) 
    	scanf(" %c", &gabarito[i]);  
   	/* O espaco em branco antes do %c e' importante.
      	Ele evita que o return digitado depois do n seja 
@@ -35,13 +35,13 @@ int main ( int argc, char *argv[] )
 
     		printf("Obs.:o gabarito deverá ser constituido das letras: a,b,c,d,e\n");
     		printf("Digite as letras correspondentes as respostas do aluno %d:\n ", aluno);
- 		for (i = 0; i < 10; i++) 
+ 		for (i = 0; i < 30; i++) 
 		{
       			scanf(" %c", &resp[i]);  /* atencao para o espaco em branco */
     		}
 		lol=process(resp,gabarito);
 		printf("gabarito da prova:\tgabarito do aluno:\n");
-		for (i = 0; i < 10;++i)
+		for (i = 0; i < 30;++i)
 		{
 			printf("\t\t%c\n\t\t\t%c\n", gabarito[i],resp[i]);
 		}
