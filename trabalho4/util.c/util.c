7a99d0359885c47421bbcecdc85bcdd0abea714e
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-23 13:53:17 -0400

    Update util.c

--- trabalho4/util.c/util.c
+++ trabalho4/util.c/util.c
@@ -120,3 +120,24 @@ int produto_escalar(int n, int v1[n], int v2[n])
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+//Funçao para calcular o produto vetorial(externo)
+
+void make_result(int *R, int *V, int **A, int n, int m) //funçao para calcular o vetor resultado.
+{
+
+	int i, j, aux = 0; //i - contador, j - contador, aux - acumulador
+
+	for(i = 0; i < n; i++)
+	{
+		aux = 0; //aux recebe o valor 0 depois de cada iteração do 'i'
+
+		for(j = 0; j < m; j++)
+		{
+			aux += V[j] * A[j][i]; //aux acumula o produto da matriz pelo vetor. 
+		}
+		R[i] = aux; //vetor resultado recebe o valor do acumulador.
+	}
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
