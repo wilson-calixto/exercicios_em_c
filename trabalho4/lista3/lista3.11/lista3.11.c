82d50372024ed8744871b29f3751d78f423c95fc
Evandro Fernandes <evandro-souza10@hotmail.com>
2015-09-23 15:31:20 -0400

    Update lista3.11.c

--- trabalho4/lista3/lista3.11/lista3.11.c
+++ trabalho4/lista3/lista3.11/lista3.11.c
@@ -18,43 +18,43 @@ Grupo:
 
 main()
 {
-	char aux[6]; // String de tamanho 5 
+	char aux[6]; // String de tamanho 6
 
-	int op = 0; //declara e inicia a variavel 'op'(opçao).
+	int op = 0; //declara e inicia a variavel 'op'(opção).
 	
 	printf("Escolha a operacao\n(1) - Adicionar\n(2) - Mostrar todos clientes\n(3) - Adicionar operacao\n(4) - Mostrar operacoes\n(5) - Sair\n\n-->");
 	setbuf(stdin, NULL); //limpar o buffer do teclado
-	scanf("%d",&op); //recebe a opçao do usuario.
+	scanf("%d",&op); //recebe a opçao do usuario entre os números escolhidos.
 
 
-	switch(op)
+	switch(op) //escolha da opção
 	{
 		case 1:
-			addcliente(); //adicniona cliente
-			break;
+			addcliente(); //chamar função adicniona cliente
+			break; // parar
 
 		case 2:
-			mostrar(); //mostra clientes ja cadastrados
-			break;
+			mostrar(); //chamar função mostra clientes ja cadastrados
+			break; // parar
 
 		case 3:
 			printf("digite o numero de sua conta: ");  
 			setbuf(stdin, NULL); //limpa buffer do teclado
-			scanf("%s", aux); //recebe o numero da conta
-			setbuf(stdin, NULL); //limpa buffer do teclado
-			addopr(aux); // para a dada conta adiciona o nº de operaçoes informados.
-			break;
+			scanf("%s", aux); //recebe o numero da conta em string
+			setbuf(stdin, NULL); //limpa buffer do teclado (dados recebidos no teclado)
+			addopr(aux); // para a conta dada adiciona o nº de operaçoes informados. Chamar função add operação
+			break; // parar
 
 		case 4:
-			mostraroprs(); //mostra operações realizadas.
-			break;
+			mostraroprs(); //chamar função mostra operações realizadas.
+			break; // parar
 
 		case 5:
-			exit(0); //finaliza o loop, fechando o programa.
-			break;
+			exit(0); //finaliza o loop de opções, fechando o programa.
+			break; // parar
 
 		default:
-			printf("Operacao inválida !\n");
-			break;
+			printf("Operacao inválida !\n"); // caso nenhuma opção seja valida
+			break; // parar
 	}
 }
