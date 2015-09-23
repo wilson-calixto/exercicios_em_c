5c186fee04baa9445b8a3e9c71635968a7a66165
Evandro Fernandes <evandro-souza10@hotmail.com>
2015-09-23 15:17:27 -0400

    Update function.c

--- trabalho4/lista2/lista2.14/function.c
+++ trabalho4/lista2/lista2.14/function.c
@@ -18,11 +18,12 @@ void farey(int n)
 {
 	typedef struct { int d, n; } fraci; //declara estrutura 'fraci'
 	fraci f1 = {0, 1}, f2 = {1, n}, t; //declara variaveis do tipo 'fraci' e inicia 'f1' e 'f2'.
-	int k; 
+	int k; //declara variavel k
  
-	printf(" %d/%d\n %d/%d\n", 0, 1, 1, n);
-	while (f2.n > 1) {
-		k = (n + f1.n) / f2.n;
+	printf(" %d/%d\n %d/%d\n", 0, 1, 1, n);//printar primeiros números da sequencia de farey
+	while (f2.n > 1) 
+	{
+		k = (n + f1.n) / f2.n; // variavel k recebe a equação
 		t = f1; //'t' recebe os valores de f1
 		f1 = f2; //'f1' recebe os valores de 'f2'
 		f2 = (fraci) { f2.d * k - t.d, f2.n * k - t.n }; // 'f2' recebe valores.
