8ab84d3b99f263153d7ada96eab714c8c3a3dd26
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-23 10:53:34 -0400

    Update lista3.11.c

--- trabalho4/lista3/lista3.11/lista3.11.c
+++ trabalho4/lista3/lista3.11/lista3.11.c
@@ -18,42 +18,43 @@ Grupo:
 
 main()
 {
-	char aux[6];
+	char aux[6]; // String de tamanho 5 
 
-	int op = 0;
+	int op = 0; //declara e inicia a variavel 'op'(opçao).
+	
 	printf("Escolha a operacao\n(1) - Adicionar\n(2) - Mostrar todos clientes\n(3) - Adicionar operacao\n(4) - Mostrar operacoes\n(5) - Sair\n\n-->");
-	setbuf(stdin, NULL);
-	scanf("%d",&op);
+	setbuf(stdin, NULL); //limpar o buffer do teclado
+	scanf("%d",&op); //recebe a opçao do usuario.
 
 
 	switch(op)
 	{
 		case 1:
-			addcliente();
+			addcliente(); //adicniona cliente
 			break;
 
 		case 2:
-			mostrar();
+			mostrar(); //mostra clientes ja cadastrados
 			break;
 
 		case 3:
-			printf("digite o numero de sua conta: ");
-			setbuf(stdin, NULL);
-			scanf("%s", aux);
-			setbuf(stdin, NULL);
-			addopr(aux);
+			printf("digite o numero de sua conta: ");  
+			setbuf(stdin, NULL); //limpa buffer do teclado
+			scanf("%s", aux); //recebe o numero da conta
+			setbuf(stdin, NULL); //limpa buffer do teclado
+			addopr(aux); // para a dada conta adiciona o nº de operaçoes informados.
 			break;
 
 		case 4:
-			mostraroprs();
+			mostraroprs(); //mostra operações realizadas.
 			break;
 
 		case 5:
-			exit(0);
+			exit(0); //finaliza o loop, fechando o programa.
 			break;
 
 		default:
-			printf("Operacao inválida !\n"); 
+			printf("Operacao inválida !\n");
 			break;
 	}
 }
