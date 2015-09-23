3fe92a827a668048b191a4b68ce79ee50d9a09fc
pflmarinho <pflmarinho@gmail.com>
2015-09-23 17:21:20 -0400

    a fazer

---
+++ trabalho4/lista2/lista2.13/lista2.13.c
@@ -0,0 +1,41 @@
+/*
+ * Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * lista4.03
+ *
+ * Equipe:
+ *
+ *          Jackson Gomes
+ *          Paulo Marinho
+ *          Hermann
+ *          Elizabeth
+ *          Jailson
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, const char *argv[])
+{
+    int  n, l, i;
+    int *result; 
+    
+    printf("informe quantas jogadas: \n");
+    scanf("%d", &n);
+
+    result = (int*) (malloc(sizeof(int) * n));
+    
+    printf("informe os valores de cada lancamento: \n");
+    
+    for (i=0; i<n; i++)
+    {
+        scanf("%d", &l);
+        result[i] = l;
+    }
+    
+    printf("%d", *result);
+    free(result);
+    result = NULL;
+
+    return 0;
+}
+
