31aac297eaf6d00ac4943ccce6c6124760a39ba0
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-23 11:05:54 -0400

    Update function.c

--- trabalho4/lista3/lista3.11/function.c
+++ trabalho4/lista3/lista3.11/function.c
@@ -14,69 +14,69 @@ Grupo:
 #include <stdlib.h>
 #include <string.h>
 
-int i = 0, j = 0, saldo[3], opr[3], valor[3];
-char conta[6],conta2[6];
-char nome[20], tipo[1];
+int i = 0, j = 0, saldo[3], opr[3], valor[3]; // declara variaveis globais
+char conta[6],conta2[6];		      // ---------*------*--------
+char nome[20], tipo[1];			      // ---------*------*--------
 
 
-void addopr(char x[6])
+void addopr(char x[6]) //adiciona operações
 {
      
-     if(opr[j] >= 0)
+     if(opr[j] >= 0) 
         opr[j]++;
      else
-	     j++;
+     	j++;
 
-     strcpy (conta, x);
+     strcpy (conta, x); //copia o conteudo de 'x' para a variavel 'conta'
      printf("(c) - Credito\t(d) - Debito: \n");
-     setbuf(stdin, NULL);
-     scanf("%s", tipo);
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     scanf("%s", tipo); //recebe do usuario o tipo de operação
      printf("Valor da operacao: ");
-     setbuf(stdin, NULL);
-     scanf("%d",&valor[j]);
-     setbuf(stdin, NULL);
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     scanf("%d",&valor[j]); //recebe o valor da operação
+     setbuf(stdin, NULL); //limpa buffer do teclado
 
-     main();
+     main(); //invoca funçao principal
 }
 
-void addcliente()
+void addcliente() //funçao para adicionar clientes
 {
-     printf("digite seu nome: \n");
-     setbuf(stdin, NULL);
-     scanf("%s", nome);
-     setbuf(stdin, NULL);
-     printf("digite o numero de sua conta: \n");
-     setbuf(stdin, NULL);
-     scanf("%s", conta);
+     printf("digite seu nome: \n"); 
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     scanf("%s", nome); //recebe o nome do cliente
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     printf("digite o numero de sua conta: \n"); 
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     scanf("%s", conta); //recebe o numero da conta
      printf("digite seu saldo: \n");
-     setbuf(stdin, NULL);
-     scanf("%d",&saldo[i]);
-     setbuf(stdin, NULL);
-     i++;
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     scanf("%d",&saldo[i]); //recebe o saldo da conta
+     setbuf(stdin, NULL); //limpa buffer do teclado
+     i++; //incrementa o numero de clientes
      
-     addopr(conta);
+     addopr(conta); //adiciona operação
      
-     main();
+     main(); //invoca função principal
 }
 
-void mostrar()
+void mostrar() //mostra contas cadastradas
 {
-     for(i=0;i<3;i++)
+     for(i=0;i<3;i++) 
       {
-         if(saldo[i] != 0)
+         if(saldo[i] != 0) //verifica se o saldo é '0'
             printf("nome: %s\tconta: %s\tsaldo: %d\tOperacoes Realizadas: %d\n", nome, conta, saldo[i], opr[i]);
-      }
+      }		//^_ mostra a conta do usuario
      
-     main();  
+     main();  //invoca funçao principal
 }
 
-void mostraroprs()
+void mostraroprs() //mostra operações realizadas
 {
 	for(j=0;j<3;j++)
 	{
-		if(valor[j] != 0)
+		if(valor[j] != 0) // verifica se o valor é '0'
 			printf("conta: %s\tOperacoes: %d\ttipo: %s\tvalor: %d\n",conta, opr[i], tipo, valor[i]);
-	}
+	}		//^_ mostra as operações realizadas com o tipo e valor.
 
-	main();
+	main(); //invoca funçao principal
 }
