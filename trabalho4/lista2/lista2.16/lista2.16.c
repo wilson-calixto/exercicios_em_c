9af08cf86b6f1b0e82573380d7b79215ff14da40
netoolii <netow@myself.com>
2015-09-23 11:22:11 -0400

    update 2.16

---
+++ trabalho4/lista2/lista2.16/lista2.16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "segmento.h"
+
+#define MAX 100
+#define FALSE 0
+#define TRUE  1
+
+int main() 
+{
+
+    int n,i,seq[MAX];
+
+
+    printf("Digite o comprimento da sequencia: ");
+    scanf("%d", &n);
+    printf("Digite uma sequencia de %d inteiros: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &seq[i]);
+    }
+    
+    segmento(n,seq);
+
+    return 0;
+}
---
+++ trabalho4/lista2/lista2.16/segmento.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define FALSE 0
+#define TRUE  1
+
+void segmento(int n, int *seq) 
+{
+
+    int i, j,
+        m,         /* comprimento do segmento repetido */  
+        achou,zero;     
+
+    achou = FALSE;
+
+    for (m = 1; !achou && m <= n/2; m++) 
+    {
+        /* procura dois segmentos iguais de comprimento m consecutivos */
+        for (i = 0; !achou && i <= n-2*m; i++)
+        {
+            /* i indica o inicio do primeiro segmento        */
+            /* testa se os dois segmentos sao de fato iguais */
+            achou = TRUE;
+            for (j = 0; achou && j < m; j++)
+            { 
+                if (seq[i+j] != seq[i+m+j])
+                {
+                    achou = FALSE;
+
+                }		
+            }
+        }
+    }
+
+    if (achou) 
+    {
+        printf("Sim, existem dois segmentos iguais consecutivos.\n");
+        printf("Tome, por exemplo, i = %d e m = %d.\n", i-1, m-1);
+    }
+    else
+    {
+        printf("Não existem segmentos iguais");
+    }
+}
