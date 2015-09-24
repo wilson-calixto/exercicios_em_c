bd1680fc1658a566422a80f6bd25118214a09179
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-23 20:30:01 -0400

    Update lista3.6.c

--- trabalho4/lista3/lista3.06/lista3.6.c
+++ trabalho4/lista3/lista3.06/lista3.6.c
@@ -18,21 +18,21 @@ Grupo:
 
 int main(int argc, const char *argv[])
 {
-	int **a, i, j, M, N;
-
+	int **a, i, j, M, N; //matriz A, i - contador que ira iterar menos, j - contador que ira iterar
+			     //M - numero de linhas da matriz A, N - numero de colunas da matriz A
 	printf("Quatidade de linha\n");
 	scanf("%d", &M);
 	printf("Quantidade de Coluna\n");
 	scanf("%d", &N);
 
-	a=(int **)malloc (M * sizeof(int **));
+	a=(int **)malloc (M * sizeof(int **)); //aloca 'M' endereços de memoria para a matriz A
 	for (i=0; i<M; i++)
 	{
-		a[i]=(int *)malloc (N * sizeof(int *));
+		a[i]=(int *)malloc (N * sizeof(int *)); // aloca 'N' endereços de memoria para cada vetor A[i]
 		for (j=0; j<N; j++) 
 		{
 			printf("Informe a[%d][%d]: ", i, j);
-			scanf("%d", &a[i][j]);
+			scanf("%d", &a[i][j]); //recebe do usuario um valor para a matriz A na posiçao [i][j].
 		}
 	}
 	
