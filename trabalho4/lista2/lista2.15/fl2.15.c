2fd133fe7fc91051c6615b0fb07346dc0260ae67
Botinelly <lucas_botinelly@hotmail.com>
2015-09-25 15:17:06 -0400

    Atualizaçao

--- trabalho4/lista2/lista2.15/fl2.15.c
+++ trabalho4/lista2/lista2.15/fl2.15.c
@@ -20,3 +20,30 @@ void media_ponderada(float *notas, int *pesos,int alunos, float somapeso)
     media=acm/somapeso;      //divide a soma do produto entre as notas e os pesos
     printf("%.2f\n", media);
 }
+
+float read_peso(float *pesos)
+{
+	int k;
+	for (k=0;k<prov;k++)
+	{
+        printf("Insira o peso da %dº prova: ", k+1);    
+        scanf("%d", &pesos[k]);                   //Recebe o valor do peso de cada prova
+    	acm+=pesos[k];
+    	}
+	return acm;
+}
+void read_notas(float *notas, float *media_aritmetica, float *notas, float *pesos, int alu, float acm)
+{	
+	int i, j;	
+	for (i=0;i<alu;i++)
+    	{
+        	for (j=0; j<prov;j++)
+        	{
+        	    printf("Insira a nota da %d prova do %d aluno: ", j+1, i+1);
+        	    scanf("%f", &notas[j]);   //Recebe a nota do aluno "i" da prova "j"
+        	    media_aritmetica[j]+=notas[j];    //Acumula o valor da nota "j" no vetor media_aritmetica
+        	}
+        	media_ponderada(notas, pesos, alu, acm); //Chama a função pra calcular a média ponderada do aluno "i"
+    		
+    	}
+}
--- trabalho4/lista2/lista2.15/lista2.15.c
+++ trabalho4/lista2/lista2.15/lista2.15.c
@@ -25,23 +25,8 @@ int main(int argc, const char *argv[])
     {
         media_aritmetica[j]=0; //Zera o vetor, para não imprimir endereço de memória
     }
-    for (k=0;k<prov;k++)
-    {
-        printf("Insira o peso da %dº prova: ", k+1);    
-        scanf("%d", &pesos[k]);                   //Recebe o valor do peso de cada prova
-    	acm+=pesos[k];
-    }
-    for (i=0;i<alu;i++)
-    {
-        for (j=0; j<prov;j++)
-        {
-            printf("Insira a nota da %d prova do %d aluno: ", j+1, i+1);
-            scanf("%f", &notas[j]);   //Recebe a nota do aluno "i" da prova "j"
-            media_aritmetica[j]+=notas[j];    //Acumula o valor da nota "j" no vetor media_aritmetica
-        }
-        media_ponderada(notas, pesos, alu, acm); //Chama a função pra calcular a média ponderada do aluno "i"
-    	
-    }
+    acm=read_pesos(pesos)
+    read_notas(notas, media_aritmetica, notas, pesos, alu, acm)	
     for (j=0;j<prov;j++)
     {
         media_aritmetica[j]=(media_aritmetica[j]/alu);    //Divide o somatorio das notas de cada prova pela quantidade de alunos
