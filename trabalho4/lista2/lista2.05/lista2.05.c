dd929fe59ed7c39f12470ed297d4a54a9cd5c525
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-25 12:26:59 -0400

    Update lista2.05.c

--- trabalho4/lista2/lista2.05/lista2.05.c
+++ trabalho4/lista2/lista2.05/lista2.05.c
@@ -1,6 +1,6 @@
 /*
  * Programacao de Computadores e ALgoritmos
- * Trabalho 1
+ * Trabalho 4
  * lista2.05.c
  *
  * Equipe:
@@ -18,30 +18,32 @@
 
 int main(int argc, const char *argv[])
 {
-    int n, i;
-    float x, y, *A, *B, *d;
-
+    int n, i; //n - numero de pontos, i - contador
+    float x, y, *A, *B, *d; //x - abscissa central, y - ordenada central, A - vetor de abscissas, B - vetor de ordenadas
+                            //d - vetor de distancias
+                            
     printf("Informe o valor de x:\n");
-    scanf("%f", &x);
+    scanf("%f", &x); //lê valor para 'x'
     printf("Informe o valor de y:\n");
-    scanf("%f", &y);
+    scanf("%f", &y); //lê valor para 'y'
 
     printf("Informe o valor de n:\n");
-    scanf("%d", &n);
+    scanf("%d", &n); //lê valor para 'n'
 
-    verificaN(&n);
-
+    verificaN(&n); //verifica se o valor de 'n' está entre 1 e 100, se nao estiver entra em um loop até o usuario digitar
+                   // um numero dentro deste intervalo
+    
     A = malloc(sizeof(float) * n);
     B = malloc(sizeof(float) * n);
     d = malloc(sizeof(float) * n);
 
-    ler_vetores(A, B, n);
+    ler_vetores(A, B, n); //lê valores para os vetores 'A' e 'B' de tamanho 'n'
 
-    geraD(d, A, B, x, y, n);   
+    geraD(d, A, B, x, y, n); //gera o vetor de distancias
 
-    elimina(d, n);
+    elimina(d, n); //ordena o vetor 'd' e elimina os valores repetidos. 
 
-    printD(d, n);
+    printD(d, n); //mostra o vetor de distancias
     
     return 0;
 }
