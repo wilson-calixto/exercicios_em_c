523eae1f02fa923b3145de661280c63741a9c482
jacksonggomes <jackson.am2009@gmail.com>
2015-09-25 16:44:40 -0400

    correção

--- trabalho4/lista2/lista2.13/lista2.13.c
+++ trabalho4/lista2/lista2.13/lista2.13.c
@@ -1,61 +1,34 @@
-/*
- * Programacao de Computadores e ALgoritmos
- * Trabalho 4
- * lista4.03
- *
- * Equipe:
- *
- *          Jackson Gomes
- *          Paulo Marinho
- *          Hermann
- *          Elizabeth
- *          Jailson
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, const char *argv[])
-{
-    int  n, l, i;
-    int p[6]; 
-    
-    printf("informe quantas jogadas: \n");
-    scanf("%d", &n);
 
-    printf("informe os valores de cada lancamento: \n");
+int main (int argc, const char *argv){ 
+	int i,j,n, *aux, *v,*w;
+	printf("Digite o grau dos polinômios: \n");
+	scanf("%d",&n);
+	v=(int *)malloc(n*sizeof(int));
+	w=(int *)malloc(n*sizeof(int));	
+	aux=(int *)malloc(n*sizeof(int));	
+	for (i=n;i>0;i--){
+		printf("Digite o valor do %d elemento do polinomio 1: \n",i);
+		scanf("%d",&v[i]);	
+		printf("Digite o valor do %d elemento do polinomio 2: \n",i);
+		scanf("%d",&w[i]);
+		
+	}
+	for (i=n;i>0;i--){ 
+		aux[i]=0;	
+	}	
 
-    for (i=0;i<6;i++){ 
-	p[i]=0;	
+	for (i=n;i>0;i--){ 
+		for (j=n;j>0;j--){
+			aux[i]+=v[i] * w[j];
+		
+		}
 	}
 
-    for (i=0; i<n; i++)
-    {
-        scanf("%d", &l);
-		if (l==1){ 
-			p[0]++;
-		}	
-		if (l==2){ 
-			p[1]++;
-		}
-		if (l==3){ 
-			p[2]++;	
-		}
-		if (l==4){ 
-			p[3]++;	
-		}
-		if (l==5){ 
-			p[4]++;	
-		}
-		if (l==6){ 
-			p[5]++;	
-		}
-
+	for (i=n;i>0;i--){
+		printf("%d ",aux[i]);	
 	}
-	for (i=0;i<6;i++){ 
-		printf("%d ",p[i]);
+	return 0;
 	
-	}
-    
-    return 0;
 }
-
