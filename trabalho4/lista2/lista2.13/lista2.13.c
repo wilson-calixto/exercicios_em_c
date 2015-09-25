c63ab863e5a04078f5388845b68a1f9a73c2218c
jacksonggomes <jackson.am2009@gmail.com>
2015-09-25 10:26:31 -0400

    a

--- trabalho4/lista2/lista2.13/lista2.13.c
+++ trabalho4/lista2/lista2.13/lista2.13.c
@@ -17,24 +17,54 @@
 int main(int argc, const char *argv[])
 {
     int  n, l, i;
-    int *result; 
+    //int cont1,cont2,cont3,cont4,cont5,cont6;
+    int p[6]; 
     
     printf("informe quantas jogadas: \n");
     scanf("%d", &n);
 
-    result = (int*) (malloc(sizeof(int) * n));
+    /*result = (int*) (malloc(sizeof(int) * n));
+    
+     p= (int*) (malloc(sizeof(int) * 6));*/
     
     printf("informe os valores de cada lancamento: \n");
-    
+
+    for (i=0;i<6;i++){ 
+	p[i]=0;	
+	}
+
     for (i=0; i<n; i++)
     {
         scanf("%d", &l);
-        result[i] = l;
-    }
+//        result[i] = l;
+		if (l==1){ 
+			p[0]++;
+		}	
+		if (l==2){ 
+			p[1]++;
+		}
+		if (l==3){ 
+			p[2]++;	
+		}
+		if (l==4){ 
+			p[3]++;	
+		}
+		if (l==5){ 
+			p[4]++;	
+		}
+		if (l==6){ 
+			p[5]++;	
+		}
+
+	}
+	for (i=0;i<6;i++){ 
+		printf("%d ",p[i]);
+	
+	}
     
-    printf("%d", *result);
+   /* printf("%d %d %d %d %d %d",cont1, cont2, cont3, cont4,cont5,cont6);
     free(result);
-    result = NULL;
+    result = NULL;*/
 
     return 0;
 }
