bbdf6510db77d5da8d977de1f0f8d9a8b8d7b397
Botinelly <lucas_botinelly@hotmail.com>
2015-09-28 16:48:08 -0400

    Função

--- trabalho5/lista4/lista4.07/ex4q7f.c
+++ trabalho5/lista4/lista4.07/ex4q7f.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-void strings(char *s, int i, int j)
-{
-    int m,k=0;
-    for(m=i;m<j;m++)
-    { 
-        s[k]=s[m];
-        k++;
-    }
-    s[k]='\0';
-}
+
