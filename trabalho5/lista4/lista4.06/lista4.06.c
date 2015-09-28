01da98a93810418ae5847926d0b4bf2fd40d522f
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-28 18:31:20 -0400

    trabalhando na questão

--- trabalho5/lista4/lista4.06/lista4.06.c
+++ trabalho5/lista4/lista4.06/lista4.06.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#include "sf_extend.h"
 
 int main ( int argc, const char *argv[] )
 {
     char s[100], t[100];
-    int i=0, len, lens, lent, result;
+    int result;
 
-    result=1;
+    scanf ( "%s %s",s,t);
+
+    result=sf_extend(s,t);
     
-    fflush(stdin);
-
-    scanf ( "%s %s", s,t );
-    //scanf ( "%s", t );
-    
-    lens = strlen(s);
-    lent = strlen(t);
-    len = (lent-lens);
-
-    //printf ( "%d\n", len );
-
-    while( s[i] != '\0')
+    if (result == 1)
     {
-        if (s[i] != t[len])
-        {
-            printf ( "%d:%c = %d:%c\n",i,s[i],len,t[len] );
-            result = 0;
-            break;
-        }
-        i=i+1;
-        len=len+1;   
+        printf ( "S é seguimento de T\n" );
     }
-
-    printf ( "%d\n", result );
+    else
+    {
+        printf ( "S nao é segmento de T\n" );
+    }
 
     return 0;
 }
