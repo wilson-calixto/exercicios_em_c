6ba0f44f2d33516452177384c074df884914991e
luizfernandobrandao <lf_brandao@yahoo.com>
2015-09-28 17:05:13 -0400

    Questão finalizada

--- trabalho5/lista2/lista2.05/lista2.05.c
+++ trabalho5/lista2/lista2.05/lista2.05.c
@@ -1,14 +1,31 @@
+/*
+ * Programação de Computadores e Algoritmos
+ *
+ * Trabalho 5
+ *
+ * Questao 05 lista 2
+ *
+ * Equipe:
+ *
+ *       Wilson Calisto   
+ *       Jackson Gomes 
+ *       Richardson Souza
+ *	 Benjamin Borges
+ *	 Luiz Fernando Brandão
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include "palindromo.h"
 
 int main( int argc, const char *argv[] ) {
 
-	char string[ 50 ];
+	char string[ 50 ]; /*Declara vetor de caracteres */
 
 	printf( "Informe uma string: " );
-	scanf( "%s", string );
+	scanf( "%s", string ); /* Ler string do usuário */
 
+	/* Imprime se é palíndromo ou não conforme o retorno da função palíndromo utilizando o operador condicional ?: */
 	printf( "%s\n", palindromo( string, strlen( string ) ) ? "Eh palindromo" : "Nao eh palindromo" );
 
 	return 0;
--- trabalho5/lista2/lista2.05/palindromo.h
+++ trabalho5/lista2/lista2.05/palindromo.h
@@ -1,1 +1,1 @@
-int palindromo( char *, int );
+int palindromo( char *, int ); /* Função palindromo recebe um vetor de caracteres e seu tamanho, e tem tipo de retorno inteiro */
