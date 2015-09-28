d16294077ec98f549ac9a43382b5d3b7315d7e87
richardson-souza <richardson.allan.souza@gmail.com>
2015-09-28 17:30:59 -0400

    trabalhando na questão

--- trabalho5/lista4/lista4.06/lista4.06.c
+++ trabalho5/lista4/lista4.06/lista4.06.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-
-#include <stdio.h> 
+#include <string.h> 
 
 int main ( int argc, const char *argv[] )
 {
+    char s[100], t[100];
+    int i=0, len, lens, lent, result;
+
+    result=1;
     
+    fflush(stdin);
+
+    scanf ( "%s %s", s,t );
+    //scanf ( "%s", t );
+    
+    lens = strlen(s);
+    lent = strlen(t);
+    len = (lent-lens);
+
+    //printf ( "%d\n", len );
+
+    while( s[i] != '\0')
+    {
+        if (s[i] != t[len])
+        {
+            printf ( "%d:%c = %d:%c\n",i,s[i],len,t[len] );
+            result = 0;
+            break;
+        }
+        i=i+1;
+        len=len+1;   
+    }
+
+    printf ( "%d\n", result );
+
     return 0;
 }
