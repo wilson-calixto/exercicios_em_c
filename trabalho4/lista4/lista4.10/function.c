77ea136d6fadda1c368b86114626c047fbb0b262
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-28 13:05:28 -0400

    Update function.c

--- trabalho4/lista4/lista4.10/function.c
+++ trabalho4/lista4/lista4.10/function.c
@@ -1,25 +1,35 @@
-  float power(float x, int y)
-  {
-  	if(y == 1)
+float power(float x, int y) //funçao para calcular um real 'x' elevado a um inteiro 'y'
+{
+	if(y == 1)
 		return x;
 	else
 		return x * power(x , y-1);
-  }
+	
+}
 
-    void derivada (float a[],float b[],int n){
-  	int i;
-  	for (i=1;i<=n;i++){
+void derivada (float a[],float b[],int n)//funçao para calcular a derivada
+{ 
+	int i; // i - contador
+  	
+  	for (i=1;i<=n;i++)
+  	{
 		b[i]=i*a[i+1];
   	}
-  }
+	
+}
 
-  float valor_polinomio(float a[],float x,int n){
-  	float valor;
-  	int i;  
-  	valor=a[1];
+float valor_polinomio(float a[],float x,int n) //funçao para calcular o valor do polinomio
+{	
+	float valor;
+  	int i; // i - contador  
+  	
+  	valor=a[1]; // valor recebe a1 * x elevado a 0, ou seja, 1
 
- 	for (i=2;i<=n+1;i++){
-		valor+= a[i] * (power(x,(i-1)));
+ 	for (i=2;i<=n+1;i++)
+ 	{
+		valor+= a[i] * (power(x,(i-1))); //valor acumula a soma dos 'a's multiplicados por x
 	}
+	
 	return valor;
-  }
+	
+}
