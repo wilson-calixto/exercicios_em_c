cefbf0998a429545a3b13a78e02c970f63c845a6
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-29 15:30:44 -0400

    Update util.c

--- trabalho5/util.c/util.c
+++ trabalho5/util.c/util.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int find(char *str, char ch)
+{
+    int indice = 0;
+    
+    while(indice < strlen(str)) 
+    {
+        if(str[indice] == ch)  
+          return indice;
+        indice++;
+    }
+    
+    return -1;
+}
