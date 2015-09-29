a208fb7ab7e7943f0a6a05ae1d9ff06989a6b541
raisantos <raisoledade@outlook.com>
2015-09-29 00:17:26 -0400

    ajustando

--- trabalho5/lista5/lista5.01/lista5.01.c
+++ trabalho5/lista5/lista5.01/lista5.01.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, const char *argv[])
 {
     char string1[50];
-    int i, n;
+    int n;
 
     printf("Insira o numero decimal: ");
     scanf("%d", &n);
 
     sprintf(string1, "%d\n", n); //converte número e armazena tudo em string1.
-    printf("%s", string1); //mostra na tela o conteúdo da string1;
+    printf("%s", string1); //mostra na tela o conteúdo da string1.
     
-    for(i = 0; i < 4; i++)
-    {
-        printf("%c\n", string1[i]);
-    }
-    
-     
     return 0; 
 }
  
