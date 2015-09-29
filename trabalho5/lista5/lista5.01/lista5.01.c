9c94c551439cfebf2d3cd8b56d1bec7651a23c9d
raisantos <raisoledade@outlook.com>
2015-09-29 00:27:22 -0400

    ajustando

--- trabalho5/lista5/lista5.01/lista5.01.c
+++ trabalho5/lista5/lista5.01/lista5.01.c
@@ -9,8 +9,8 @@ int main(int argc, const char *argv[])
     printf("Insira o numero decimal: ");
     scanf("%d", &n);
 
-    sprintf(string1, "%d\n", n); //converte número e armazena tudo em string1.
-    printf("%s", string1); //mostra na tela o conteúdo da string1.
+    sprintf(string1, "%d", n); //converte número e armazena tudo em string1.
+    printf("\"%s\"", string1); //mostra na tela o conteúdo da string1.
     
     return 0; 
 }
