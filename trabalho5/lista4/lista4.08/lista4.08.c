28af8036b34e2b1687500c2682247d3730030a64
faracogabriel <gabriel.faraco@hotmail.com>
2015-09-29 10:19:19 -0400

    create lista4.08.c

---
+++ trabalho5/lista4/lista4.08/lista4.08.c
@@ -0,0 +1,15 @@
+/*
+ * Programação de Computadores e Algoritmos
+Trabalho 5
+Questão 08 da Lista 4
+Grupo:
+	Davi Tavares;
+	Hermann Hernani;
+	Evandro Fernandes;
+	Lucas Frota;
+	Gabriel Faraco;
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include ""
