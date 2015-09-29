7a161d5a5f0ac4944096941d1be198d17f735882
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-29 15:33:55 -0400

    Update util.c

--- trabalho5/util.c/util.c
+++ trabalho5/util.c/util.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//--------------------------------------------------------------------------------------------------------------------
+//Função para achar um char dentro de uma string
 
 int find(char *str, char ch)
 {
-    int indice = 0;
+    int indice = 0; //inicia indice
     
-    while(indice < strlen(str)) 
+    while(indice < strlen(str))
     {
         if(str[indice] == ch)  
-          return indice;
-        indice++;
+          return indice; //se achar 'ch' na string 'str' retorna indice
+        indice++; //incrementa indice
     }
     
-    return -1;
+    return -1; //retorna '-1' pois '0' é um índice possível 
 }
+
+//--------------------------------------------------------------------------------------------------------------------
