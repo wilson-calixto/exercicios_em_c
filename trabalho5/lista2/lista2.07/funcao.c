932b83b719a073b78578a5d883dc1e504232e016
HermannHernani <hermannhernani@outlook.com>
2015-09-30 12:14:38 -0400

    lista 2 questao 07

--- trabalho5/lista2/lista2.07/funcao.c
+++ trabalho5/lista2/lista2.07/funcao.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int cont_str(char *a)
+#include <string.h>
+void cont_str()
 {
-	while (
+	char stri[100], c = ' ';
+	int cont = 0, maior = 0, i, a;
+	printf("digite sua string com ate 100 caracteres: ");
+	scanf("%s", stri);
+	stri[strlen(stri) + 1] = 'c';
+
+	for (i = 0; i < (strlen(stri) + 1); i++)
+	{
+		if (stri[i] == c)
+		{
+			cont++;
+			printf("%d", cont);
+		}	
+		else
+		{
+			if (cont > maior)
+			{
+				maior = cont;
+				cont = 0;
+			}
+		}
+	}
+	printf("%d\n", maior);
 }
---
+++ trabalho5/lista2/lista2.07/funcao.h
@@ -0,0 +1,1 @@
+void cont_str();
