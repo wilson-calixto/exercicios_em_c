9f9b9b4a6920df43dadc9a133fd031f29f3e7314
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-30 14:38:27 -0400

    Update lista4.05.c

--- trabalho5/lista4/lista4.05/lista4.05.c
+++ trabalho5/lista4/lista4.05/lista4.05.c
@@ -5,8 +5,8 @@
  *
  * Equipe:
  *      
- *      Manoel
- *      Paulo M.
+ *      Manoel Victor
+ *      Paulo Marinho
  *      Wilson Neto
  */
 #include <stdio.h>
@@ -14,17 +14,15 @@
 
 int main (int argc, const char *argv[])
 {
-	char a[100], b[100];
+	char a[100], b[100]; //declara vetores
 	
 	printf("informe uma sequencia de caracters para o primeiro vet: \n");
-    	scanf("%s", a);
+    	scanf("%s", a); //recebe uma string
 
     	printf("informe uma sequencia de caracters para o segundo vet: \n");
-    	scanf("%s", b);
+    	scanf("%s", b);//recebe um trecho da string
     
-	printf("%d\n", position(a,b));
+	printf("%d\n", position(a,b)); //mostra a posição do trecho da string na string
 
 	return 0;
 }
-
-
