b79ffda1771ac5071ebdad18b398a637981572f9
faracogabriel <gabriel.faraco@hotmail.com>
2015-09-29 23:50:13 -0400

    update lista4.08.c and funcao.c

--- trabalho5/lista4/lista4.08/funcao.c
+++ trabalho5/lista4/lista4.08/funcao.c
@@ -13,4 +13,13 @@ Grupo:
 #include <stdio.h>
 #include <string.h>
 
-
+char *string_copy (char *a, char *b)
+{
+	int i;
+	for (i = 0; b[i] != '\0'; i++) 
+	{   
+		a[i] = b[i];
+	}
+	a[i] = '\0';
+	return a;
+}
--- trabalho5/lista4/lista4.08/lista4.08.c
+++ trabalho5/lista4/lista4.08/lista4.08.c
@@ -13,3 +13,15 @@ Grupo:
 #include <stdio.h>
 #include <stdlib.h>
 #include "funcao.h"
+
+int main (int argc, const char *argv[])
+{
+	char s[50],cs[50];
+	printf("escreva a string:\n");
+	scanf("%[^\n]s",s);
+	printf("esta é a sua cópia:\n");	
+	printf("%s\n",string_copy(cs,s));
+	
+
+	return 0;
+}
