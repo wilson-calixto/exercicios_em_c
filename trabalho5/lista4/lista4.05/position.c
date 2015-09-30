c6f558ac65982489614e37366b8ad65a7a3cb410
Manoel Victor <manoelvictor1602@hotmail.com>
2015-09-30 14:40:38 -0400

    Update position.c

--- trabalho5/lista4/lista4.05/position.c
+++ trabalho5/lista4/lista4.05/position.c
@@ -3,16 +3,16 @@
 
 int position (char a[], char b[])
 {
-    int *aux, ind=-1, i=0, j=0;
+    int *aux, ind=-1, i=0, j=0; //auxiliar, indice, i - contador, j - indice do trecho
     
     while (i < strlen(a))
     {
-        if((a[i]==b[j]) && (b[j] != '\0'))
+        if((a[i]==b[j]) && (b[j] != '\0')) //!= '\0' por ser o char delimitador
         {
-            ind = i;
-            j++;
+            ind = i; //indice recebe indice atual, caso os chars sejam iguais
+            j++; //indice do trecho incrementa para verificar se é igual
         }
-        i++;
+        i++;//contador é incrementado
     }
       
     return ind;
