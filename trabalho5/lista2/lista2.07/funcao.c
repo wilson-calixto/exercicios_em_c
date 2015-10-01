3fb9f3526a449157296d5b45a1d48a8b206a19e0
lucasfrota <frota_lv@hotmail.com>
2015-09-30 22:50:31 -0400

    questao agora com cabeçalho e melhor comentada

--- trabalho5/lista2/lista2.07/funcao.c
+++ trabalho5/lista2/lista2.07/funcao.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 void cont_str()
 {
-	char stri[100], c = ' ';
+	char stri[100];
 	int cont = 0, maior = 0, i, a;
 	printf("digite sua string com ate 100 caracteres: ");
 	gets(stri); //aqui nao se usou o scanf, se usou o gets que se adequa melhor a situacao.
 	stri[strlen(stri) + 1] = 'c';
 
-	for (i = 0; i < (strlen(stri) + 1); i++)
+	for (i = 0; i < (strlen(stri) + 1); i++)// a funcao "strlen" da biblioteca string eh usada para se obter o numero de elementos de um vetor
 	{
-		if (stri[i] == c)
+		if (stri[i] == ' ')
 		{
 			cont++;
 		}	
--- trabalho5/lista2/lista2.07/lista2.07.c
+++ trabalho5/lista2/lista2.07/lista2.07.c
@@ -1,6 +1,19 @@
+/*
+Programaçao de Computadores e Algoritmos
+Trabalho 5
+Questão 03 da Lista 4
+Grupo:
+	Davi Tavares;
+	Hermann Hernani;
+	Evandro Fernandes;
+	Lucas Frota;
+	Gabriel Faraco;
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "funcao.h"
 
 int main(int argc, const char *argv[])
 {
