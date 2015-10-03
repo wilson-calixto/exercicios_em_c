7abcf5fb5d19dee579e8d1b1f6a77b030e3f61ae
Neto OLiveira <netow@myself.com>
2015-10-02 21:38:24 -0400

    Update conjunto.c

--- trabalho5/parte2/questao04/conjunto.c
+++ trabalho5/parte2/questao04/conjunto.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ehzero(int nconjelem, int )
+int main()
 {
     
+    int nconjelem, nsubconj,contador, *nelem;
+    printf("Quantos elementos o conjunto possui: ");
+    scanf("%d", &nconjelem);
+    printf("Quantos subconjuntos o conjunto possui: ");
+    scanf("%d", &nsubconj);
+    if(nsubconj>1 && nsubconj<=10)
+    {
+        contador=nsubconj;
+        printf("Quantos elemento o subconjunto possui: ");
+        scanf("%d", &nsubconjelem);
+        for(i=0;i==nconjelem;i++)
+        {
+            printf("Qual elemento: ");
+            scanf("%d", *nelem);
 
+        }
+
+
+    }
+
+    if(nsubconj<=0 || nconjelem<=0)
+    {
+        printf("===NÃO POSSIVEL FAZER O CONJUNTO===\n====OPERAÇÃO FINALIZADA====\n");
+
+    }
+
+
+    return 0;
 }
-
 
 
