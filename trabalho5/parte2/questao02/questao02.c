10f510ea6e1f5fde0e6dcf45aeb25c7d8447ce1f
manoel-victor1602 <manoelvictor1602@hotmail.com>
2015-10-05 12:04:34 -0400

    Questao 02 Parte 2 Separada

--- trabalho5/parte2/questao02/questao02.c
+++ trabalho5/parte2/questao02/questao02.c
@@ -12,67 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-void ListP(int n[], int num);
-void Permuta(int n[], int num, int k);
-void Troca(int n[], int num, int p1, int p2);
-void Mostra(int n[], int num);
-void LerVetor(int n[], int num);
-
-void ListP(int n[], int num)
-{
-    Permuta(n, num, 0);
-}
-
-void Mostra(int n[], int num)
-{
-    int i;
-
-    for(i = 0; i < num; i++)
-    {
-        printf("%d ", n[i]);
-    }
-    putchar('\n');
-}
-
-void Permuta(int n[], int num, int k)
-{
-    int i, len;
-
-    len = num;
-
-    if(k == len)
-        Mostra(n, num);
-    else
-    {
-        for(i = k; i < len; i++)
-        {
-            Troca(n, num, k, i);
-            Permuta(n, num, k+1);
-            Troca(n, num, i, k);
-        }
-    }
-}
-
-void Troca(int n[], int num, int p1, int p2)
-{
-    int aux;
-
-    aux = n[p1];
-    n[p1] = n[p2];
-    n[p2] = aux;
-}
-
-void LerVetor(int n[], int num)
-{
-    int i;
-
-    for(i = 0; i < num; i++)
-    {
-        scanf("%d", &n[i]);
-    }
-
-}
+#include "Permutacao.h"
 
 int main()
 {
