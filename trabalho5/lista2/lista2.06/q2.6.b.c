9f3161651d41266efd606da81143273c0b7eb621
Delrick-Oliveira <delricknunes@gmail.com>
2015-10-05 14:45:55 -0400

    adicionando comentários

--- trabalho5/lista2/lista2.06/q2.6.b.c
+++ trabalho5/lista2/lista2.06/q2.6.b.c
@@ -20,18 +20,18 @@
 
 int main(int argc, const char argv[])
 {
-    char s[100], t[100];
-    int times;
+    char s[100], t[100]; // vetores para a leitura das duas strings
+    int times; // variável que conta o número de vezes
 
     printf ( "digite o segmento a ser verificado na palavra\n" );
-    scanf ( "%s", s );
+    scanf ( "%s", s ); // lendo a string segmento
 
-    fflush(stdin);
+    fflush(stdin); // limpeza do buffer 
 
     printf ( "digite a palavra \n" );
-    scanf ( "%s", t );
+    scanf ( "%s", t ); // leitura da string palavra a ser verificada 
 
-    times = count_segments(s,t);
-    printf ( " há %d segmentos\n", times );
+    times = count_segments(s,t); // a variável times armazena a quantidade de vezes que o segmento aparece na palavra 
+    printf ( " há %d segmentos\n", times ); // print da variável times 
     return 0;
 } 
