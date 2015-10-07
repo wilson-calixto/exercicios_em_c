037e06871c1e633c0efeec1ebb865301ed25e9d5
jackkssi <jkdsgg@gmail.com>
2015-10-07 12:08:23 -0400

    subindo pasta

---
+++ trabalho5/parte2/questao08/q08.c
@@ -0,0 +1,23 @@
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 5 
+ * 
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos 
+ *          Jackson Kelvin
+ *          Wilson Calisto
+ *
+ */
+
+ //AUTOR: Jackson Kelvin de Souza
+
+
+#include <stdio.h> 
+
+int main ( int argc, char *argv[] )
+{
+
+    
+    return 0;
+}
