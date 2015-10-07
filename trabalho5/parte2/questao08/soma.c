a3d0661fec50f7664956d7d7058a4515b132f12a
Manoel Victor <manoelvictor1602@hotmail.com>
2015-10-07 17:25:14 -0400

    Update soma.c

--- trabalho5/parte2/questao08/soma.c
+++ trabalho5/parte2/questao08/soma.c
@@ -1,12 +1,12 @@
 int inc(int x, int y)
 {
-    int soma=0;
+    int soma=0; //soma - varaivel para receber o resultado da função
     
     if(y==0)
-        return x;
+        return x; // 'x' + 0 = x
     else
     {
-        soma = inc(x,y-1);
-        return(++soma);
+        soma = inc(x,y-1); //como não é possivel incrementar o valor de funçoes, uma variavel deve receber o valor antes realizar os incrementos
+        return(++soma); //retorna os incrementos sucessivos da soma
     }
 }
