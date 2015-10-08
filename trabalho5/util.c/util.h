36b9dd0d47b966d62b4f3686593f2b77d778ceaf
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-07 21:30:40 -0400

    Update util.h
    corrigindo um erro

--- trabalho5/util.c/util.h
+++ trabalho5/util.c/util.h
@@ -1,5 +1,5 @@
 void copy(char *a, char *b);
 int find(char *str, char ch);
 int select_sort(int num[], int tam);
-void LerVetor(int n[], int num)
+void LerVetor(int n[], int num);
 void Quick(int vetor[], int inicio, int fim);
