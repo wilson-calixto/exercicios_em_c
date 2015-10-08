eb572bba890f7ffcf54511dbde77407f32b780c0
Manoel Victor <manoelvictor1602@hotmail.com>
2015-10-08 10:35:56 -0400

    Update expo.c

--- trabalho5/parte2/questao07/expo.c
+++ trabalho5/parte2/questao07/expo.c
@@ -1,24 +1,24 @@
-int contador = 0;
+int contador = 0; //quantidade de multiplicaçoes realizadas
 
 int expo(int k, int n)
 {
     if (n == 0)
     {
-        contador++;
-        return 1;
+        contador++; //contador é incrementado
+        return 1; //todo numero diferente de 0 elevado a 0 é igual a 1. 
     }
     else if (n == 1)
     {
-        return k;
+        return k; //retorna 'k'
     }
     else
     {
-        contador++;
-        return (k*expo(k,n-1));
+        contador++; //contador é incrementado
+        return (k*expo(k,n-1)); //retorna k * k elevando a n-1
     }
 }
 
 int cont()
 {
-    return contador;
+    return contador; //retorna a quantidade de multiplicações realizadas
 }
