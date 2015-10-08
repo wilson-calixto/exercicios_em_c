91b6fa6d0ec204a9e2e35ae23339c2451fc7dff9
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-07 21:28:52 -0400

    Update util.c
    adicionando Função Quick

--- trabalho5/util.c/util.c
+++ trabalho5/util.c/util.c
@@ -68,3 +68,39 @@ void LerVetor(int n[], int num)
     }
 }
 //--------------------------------------------------------------------------------------------------------------------
+//Função Quick Sort : Ordenação de numeros
+
+//Bibliotecas utilizadas
+#include <stdio.h>
+#include <stdlib.h>
+
+void Quick(int vetor[], int inicio, int fim){
+   
+   int pivo, aux, i, j, meio;
+   
+   i = inicio;
+   j = fim;
+   
+   meio = (int) ((i + j) / 2);
+   pivo = vetor[meio];
+   
+   do{
+      while (vetor[i] < pivo) i = i + 1;
+      while (vetor[j] > pivo) j = j - 1;
+      
+      if(i <= j){
+         aux = vetor[i];
+         vetor[i] = vetor[j];
+         vetor[j] = aux;
+         i = i + 1;
+         j = j - 1;
+      }
+   }while(j > i);
+   
+   if(inicio < j) Quick(vetor, inicio, j);
+   if(i < fim) Quick(vetor, i, fim);
+}
+
+}
+//--------------------------------------------------------------------------------------------------------------------
+
