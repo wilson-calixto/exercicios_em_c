5d3dd507da221181bd8c170e3026eff0b5de5b90
Manoel Victor <manoelvictor1602@hotmail.com>
2015-10-08 10:33:12 -0400

    Update q07.c

--- trabalho5/parte2/questao07/q07.c
+++ trabalho5/parte2/questao07/q07.c
@@ -4,11 +4,11 @@
 int main(int argc, const char *argv[])
 {
     int k, n;
-    printf("Informe numeros positivos nao nulos (Z*).\n");    
-    scanf("%d %d", &k,&n);    
+    printf("Informe numeros positivos nao nulos.\n");     
+    scanf("%d %d", &k,&n); //recebe valores de 'k' e 'n'
     
-    printf("%d\n", expo(k,n));
-    printf("%d\n", cont());
+    printf("%d\n", expo(k,n)); //mostra o resultado da multiplicações sucessivas
+    printf("%d\n", cont()); //mostra a quantidade de multiplicações realizadas
    
     return 0;
 }
