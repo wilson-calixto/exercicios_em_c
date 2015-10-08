f1b4bb1814f651c616713caa6199ffaecdbe5b42
richardson-souza <richardson.allan.souza@gmail.com>
2015-10-08 02:34:35 -0400

    atualizando codigo

--- trabalho5/parte2/questao06/order/main.c
+++ trabalho5/parte2/questao06/order/main.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include "order.h"
-#include <sys/time.h>
+//#include <sys/time.h>
 
 int main ( int argc, const char *argv[] )
 {
     int *b,*q,n;
-    int i,j;
-    struct timeval inicio, final;
-    int tmili;
+    clock_t t;
+    //int i,j;
+    //struct timeval inicio, final;
+    //int tmili;
 
     scanf ( "%d", &n );
     // cria um vetor desordenado
     b=create_vec_rand(n);
+    
+    t = clock();
+    bubblesort(b,n);
+    t = clock()-1;
+    printf ( "tempo - bubblesort - vetor desordenado: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)) );
+
+    /*
     //print_vec(n,b);
     gettimeofday(&inicio, NULL);
     bubblesort (b,n); // funcao para organizar vetor desordenado
@@ -23,7 +31,14 @@ int main ( int argc, const char *argv[] )
     tmili = (int) (1000 * (final.tv_sec - inicio.tv_sec) + (final.tv_usec - inicio.tv_usec) / 1000);
     // imprime o tempo decorrido
     printf ( "tempo - bubblesort - vetor desordenado: %d\n", tmili );
+    */
 
+    t = clock();
+    bubblesort(b,n);
+    t = clock()-1;
+    printf ( "tempo - bubblesort - vetor ordenado: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)) );    
+
+    /*
     //print_vec(n,b);
     gettimeofday(&inicio,NULL);
     bubblesort(b,n); // chama novamente a funcao para um vetor ja ordenado
@@ -34,10 +49,18 @@ int main ( int argc, const char *argv[] )
     // imprime o tempo decorrido
     printf ( "tempo - bubblesort - vetor ordenado: %d\n", tmili );
     //print_vec(n,b);
+    */
     free(b);
     
     // cria um vetor desordenado
     q=create_vec_rand(n);
+
+    t = clock();
+    quick(q,0,n-1);
+    t = clock()-1;
+    printf ( "tempo - quicksort - vetor desordenado: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)) ); 
+
+    /*
     //print_vec(n,q);
     gettimeofday(&inicio,NULL);
     quick(q,0,n-1); // chama a funcao quick para organizar o vetor desordenado
@@ -48,7 +71,14 @@ int main ( int argc, const char *argv[] )
     // imprime o tempo decorrido
     printf ( "tempo - quicksort - vetor desordenado: %d\n", tmili );
     //print_vec(n,q);
-    
+    */
+
+    t = clock();
+    quick(q,0,n-1);
+    t = clock()-1;
+    printf ( "tempo - quicksort - vetor ordenado: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)) );
+
+    /*
     gettimeofday(&inicio, NULL);
     quick(q,0,n-1); // chama novamente a funcao para um vetor ja ordenado
     for ( j = 0; j < 10; j++ )
@@ -57,6 +87,7 @@ int main ( int argc, const char *argv[] )
     tmili = (int) (1000 * (final.tv_sec - inicio.tv_sec) + (final.tv_usec - inicio.tv_usec) / 1000);
     printf ( "tempo - quicksort - vetor ordenado: %d\n", tmili );
     //print_vec(n,q);
+    */
     free(q);
  
     return 0;
