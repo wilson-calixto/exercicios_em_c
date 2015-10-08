43b0c5ec9ec6faa1320c0877f63f06d16a211ef3
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-10-07 21:21:38 -0400

    subindo nova versao

---
+++ trabalho5/parte2/questao071/mostra_matriz.c
@@ -0,0 +1,39 @@
+
+
+/*  questao 7 */
+
+/* Função que imprime uma matriz de mxn */
+
+void mostra_matriz(float **mat)
+{   
+    int i,j,linha=10,coluna=10;
+
+    for ( i = 0; i < linha; i ++ )
+    {    
+        for ( j = 0; j < coluna; j ++ )
+        {
+            printf ( "%d", mat[i][j] );
+        }
+        printf ( "\n" );
+    }
+}
+void mostra_caminho(float **mat)
+{   
+    int i,j,linha=10,coluna=10;
+
+    for ( i = 0; i < linha; i ++ )
+    {    
+        for ( j = 0; j < coluna; j ++ )
+        {
+        	if(mat[i][j]==2)
+        	{
+            	printf ( "0");
+            }else{
+            
+            	printf ( "1");
+            }
+            
+        }
+        printf ( "\n" );
+    }
+}
