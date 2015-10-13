d63b1c7abd8094f33d281efcf07eb719b9d7da8a
eivandru <evandro-souza10@hotmail.com>
2015-10-12 22:56:40 -0400

    versão 1.0.0

---
+++ trabalho5/parte2/questao05/function.c
@@ -0,0 +1,13 @@
+#include <time.h>
+
+int main () 
+{
+   clock_t tempoInicial, tempoFinal; //definindo parametros
+   double tempoGasto; // definindo parametros
+   tempoInicial = clock(); //pegando o tempo Inicial
+   tempoFinal = clock(); // pegando o tempo final
+
+   tempoGasto = (tempoFinal-tempoInicial)/CLOCKS_PER_SEC); // calculando o tempo gasto
+   printf("Tempo em segundos: %f", tempoGasto);
+   return 0;
+}
