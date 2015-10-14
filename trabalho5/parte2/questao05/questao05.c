df76323066dbf373ad3027dce8ba28523e6bb75d
faracogabriel <gabriel.faraco@hotmail.com>
2015-10-14 15:13:50 -0400

    create questao05.c

---
+++ trabalho5/parte2/questao05/questao05.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <funcoes.h>
+#include <time.h>
+
+
+int main(int argc, const char *argv[])
+{
+    int v[5000], x, n;
+    clock_t ti1, tf1, ti2, tf2;
+    printf("digite o tamanho do vetor:\n");
+    scanf("%d",&n)
+    array_creator(n, v);
+ 
+    printf("digite o termo a ser procurado:\n");
+    scanf("%d",&x)
+    ti1 = clock();
+    busca_binaria(x, n, v);
+    tf1 = clock();
+    tempo(ti1, tf1);
+    
+    ti2 = clock();
+    buscaSequencial(n, v, x);
+    tf2 = clock();
+    tempo(ti2,tf2);
+
+
+
+    return 0;
+}
+
+
