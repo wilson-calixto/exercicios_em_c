ca1c0175b9077b6513e574bba5628cfbae0e4693
Botinelly <lucas_botinelly@hotmail.com>
2015-10-14 16:30:48 -0400

    #final

--- trabalho5/parte2/questao01/q1.c
+++ trabalho5/parte2/questao01/q1.c
@@ -4,7 +4,7 @@ int seq[100000];
 
 void comb(int beg, int n, int k)
 {
-        int i,j;
+        int i;
         if(k == 1)
         {
                 seq[k-1] = beg;
@@ -26,11 +26,13 @@ void comb(int beg, int n, int k)
 
 int main()
 {
-        int i,n,k, tam;
+        int i,n,k;
         scanf("%d%d",&n,&k);
 
-        for(i = 1;i <= n-k+1;i++) comb(i,n,k);
-        
+        for(i = 1;i <= n-k+1;i++) 
+        {
+                comb(i,n,k);
+        }
         
         return 0;
 }
