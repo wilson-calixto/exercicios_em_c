81a8880aa8450a4752b3e896c9b0c60cc29da3b7
HermannHernani <hermannhernani@outlook.com>
2015-10-14 11:07:33 -0400

    questao 03 trabalho 5 parte 2

---
+++ trabalho5/parte2/questao03/questao03.c
@@ -0,0 +1,30 @@
+/*
+Programaçao de Computadores e Algoritmos
+Trabalho 5
+Questão 03 da Lista 4
+Grupo:
+	Hermann Hernani;
+	Evandro Fernandes;
+	Lucas Frota;
+	Gabriel Faraco;
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "desarranjo.h"
+
+int main()
+{
+	int *n, num, i, cont;
+
+	puts("Informe n: ");
+	scanf("%d", &num);
+	n = malloc(sizeof(int) * num);
+	for(cont = 0; cont < num; cont++)
+		{
+			n[cont] = cont + 1;
+		}
+
+	ListP(n, num);
+	return 0;
+}
