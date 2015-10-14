7789355233ac6e41aab92eee7c083df7ad28ad49
Gabriel Faraco <gabriel.faraco@hotmail.com>
2015-10-14 15:06:29 -0400

    Update funcoes.h

--- trabalho5/parte2/questao05/funcoes.h
+++ trabalho5/parte2/questao05/funcoes.h
@@ -1,4 +1,4 @@
-void array_creator();
+void array_creator(int n);
 int busca_binaria (int x, int n, int v[]);
 int buscaSequencial(int tamanho, int vetor[],int p);
 void tempo(clock_t tempoInicial, clock_t tempoFinal);
