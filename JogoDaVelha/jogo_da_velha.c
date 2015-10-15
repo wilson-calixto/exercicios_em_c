4192bca05cd8da2f636bf2818b4aea9734694f83
HermannHernani <hermannhernani@outlook.com>
2015-10-15 15:38:12 -0400

    jogo_da_velha.c e jogo_da_velha.h

---
+++ JogoDaVelha/jogo_da_velha.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Funcao que marca com X ou O*/
+
+// Programador: wilson calisto 
+
+void joga(int *vet, char **matriz) //recebe o vetor da  ultima jogada e a matriz e modifica a matriz
+{
+    int i,cont,len;
+    len=strlen(vet);
+    coluna=vet[len-1];
+    linha=vet[len-2];
+    if(i_global%2==0)
+    {
+            matriz[linha][coluna]='0';
+            cont_global++;
+    }
+    else
+    {
+        cont_global++;
+         matriz[linha][coluna]='X';
+    }
+}
+
+/* Funcao que verifica se uma jogada é valida ou nao percorrendo o vetor de jogas ja realizadas*/
+
+// Programador: Hermann J. Hernani.
+
+int valida_jogada(char vet[9], int jogada) //recebe o vetor de jogadas realizadas e a jogada que se deseja fazer
+{
+   int i;
+   for(i = 0; i < 9; i++) // percorre o vetor
+   {
+       if (jogada == vet[i]) // se a jogada existir no vetor, ou seja, ja foi realizada retorna 0, valor para false.
+       {
+           return 0;
+       }
+ 
+   }
+    return 1; //  se a jogada não existir ele retorn 1, valor para true
+}
+
+
+/* Funcao que verifica se uma o jogo acabou*/
+
+// Programador: wilson calisto
+
+int velhou(char *vet) //recebe o vetor de jogadas e retorna: 1 pra velhou e 0 para falso
+{
+   int len;
+   len=strlen(vet);
+   if(len=9)
+   {
+        return 1;
+   }else{
+   
+        return 0;
+    }
+   
+}
+
+
---
+++ JogoDaVelha/jogo_da_velha.h
@@ -0,0 +1,3 @@
+void joga(int *vet, char **matriz);
+int valida_jogada(char vet[9], int jogada);
+int velhou(char *vet);
