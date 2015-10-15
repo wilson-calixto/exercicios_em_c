10a1d52085c6a176b2d468a6c3df6950250ad22c
HermannHernani <hermannhernani@outlook.com>
2015-10-15 15:41:47 -0400

    atualização na biblioteca

--- JogoDaVelha/jogo_da_velha.c
+++ JogoDaVelha/jogo_da_velha.c
@@ -61,4 +61,18 @@ int velhou(char *vet) //recebe o vetor de jogadas e retorna: 1 pra velhou e 0 pa
    
 }
 
+/* Funcao que verifica se uma jogada é valida ou nao percorrendo o vetor de jogas ja realizadas*/
 
+// Programador: wilson calisto 
+
+int tem_jogada(int cont_global) //recebe o vetor de jogadas realizadas e a jogada que se deseja fazer
+{
+    if(cont==9) /*se os jogadsores ja fizeram 9 jogadas ele retorna 0 False*/
+    {
+        return 0;
+    }
+    else
+    {
+        return 1; /* se ainda tiver jogada ele retorna 1 true*/
+    }
+}
--- JogoDaVelha/jogo_da_velha.h
+++ JogoDaVelha/jogo_da_velha.h
@@ -1,3 +1,4 @@
 void joga(int *vet, char **matriz);
 int valida_jogada(char vet[9], int jogada);
 int velhou(char *vet);
+int tem_jogada(int cont_global);
