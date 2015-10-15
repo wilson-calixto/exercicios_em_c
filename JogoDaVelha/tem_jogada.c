3015b44154b6c208de88d87892b1fae2a69ecdb0
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-15 15:42:32 -0400

    Update tem_jogada.c
    arrumando

--- JogoDaVelha/tem_jogada.c
+++ JogoDaVelha/tem_jogada.c
@@ -6,6 +6,9 @@
 
 int tem_jogada(int cont_global) //recebe o vetor de jogadas realizadas e a jogada que se deseja fazer
 {
+   int len;
+   len=strlen(vet);
+   if(len=9)
     if(cont==9) /*se os jogadsores ja fizeram 9 jogadas ele retorna 0 False*/
     {
         return 0;
