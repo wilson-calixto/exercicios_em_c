deb52ea3e8f753d15f10c3960b9d3fe459dba3de
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-15 16:31:24 -0400

    Update rai.c
    melhorando o codigo

--- JogoDaVelha/rai.c
+++ JogoDaVelha/rai.c
@@ -11,3 +11,51 @@ printf("   |   |\n %c | %c | %c\n___|___|___\n   |   |\n %c | %c | %c\n___|___|_
 	
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+int vetor_de_endereco[17];
+int vetor_posicao[17];
+int main()
+{
+    int i,jogada;
+	char matriz[3][3] ={{'1','2','3'},
+					   {'4','5','6'},
+					   {'7','8','9',}};
+
+    for(i=1;i<=9:i++){
+    
+	    mostra_matriz(matriz);
+	
+	    if(i%2==0) /*se par o jogador é o 1==O se impar é 2==X */
+	    {
+	
+	    }
+	    else
+	    {
+	        
+	        /*retira as coordenadads invalidas*/
+	        do{            
+	            printf("Digite a jogada jogador 2");
+	            scanf("%d",&jogada);
+	        }while(jogada>9 || jogada<1);
+	        
+	        /*converte um numero em um endereco*/
+	        pede_jogada(jogada);
+	        
+	        /* se ainda tiver jogada */       
+          
+	                valida_jogada();
+	                    
+	                joga();
+	                ganhou();
+	                velhou();
+	                
+	       
+	            
+	       
+	    }
+	
+	}
+    return 0;
+}
