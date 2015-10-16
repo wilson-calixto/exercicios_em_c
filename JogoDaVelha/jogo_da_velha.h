7999c875b1da083f80191d175ff758b3c63d8b6a
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-16 09:25:18 -0400

    Update jogo_da_velha.h
    trocando nome

--- JogoDaVelha/jogo_da_velha.h
+++ JogoDaVelha/jogo_da_velha.h
@@ -1,4 +1,4 @@
 void joga(int *vet, char **matriz);
-int valida_jogada(char vet[9], int jogada);
+int jogada_valida(char vet[9], int jogada);
 int velhou(char *vet);
 int tem_jogada(int cont_global);
