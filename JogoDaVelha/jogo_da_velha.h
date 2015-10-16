9052db547364863dee53662697d368d94f7fa2dd
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-16 11:14:31 -0400

    Update jogo_da_velha.h
    retirando uma funcao inutil

--- JogoDaVelha/jogo_da_velha.h
+++ JogoDaVelha/jogo_da_velha.h
@@ -3,5 +3,4 @@ int jogada_valida(char *vetor_de_posicao, int jogada);
 void converte_jodada(int posicao);
 void joga(int *vetor_de_endereco,char **matriz,int i);
 int velhou(char *vetor_de_jogadas);
-int tem_jogada(int i);
 int ganhou(char **mat);
