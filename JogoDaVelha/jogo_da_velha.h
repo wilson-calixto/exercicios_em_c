e5e76c470348f22e42630fd9cf2842d85d96f649
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-16 09:59:30 -0400

    Update jogo_da_velha.h
    comnetando

--- JogoDaVelha/jogo_da_velha.h
+++ JogoDaVelha/jogo_da_velha.h
@@ -1,6 +1,6 @@
 void mostra_matriz(char **matriz);
 void converte_jodada(int posicao);
 int jogada_valida(char *vetor_de_posicao, int jogada)
-void joga(int *vet, char **matriz);
-int velhou(char *vet);
+void joga(int *vetor_de_endereco,char **matriz,int i);
+int velhou(char *vetor_de_jogadas);
 int tem_jogada(int cont_global);
