d9dedb5698b87945f1e3a03456ce20978675a201
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-16 10:14:46 -0400

    Update jogo_da_velha.h
    adicionando funcao

--- JogoDaVelha/jogo_da_velha.h
+++ JogoDaVelha/jogo_da_velha.h
@@ -4,3 +4,4 @@ int jogada_valida(char *vetor_de_posicao, int jogada)
 void joga(int *vetor_de_endereco,char **matriz,int i);
 int velhou(char *vetor_de_jogadas);
 int tem_jogada(int i);
+int ganhou(char **mat);
