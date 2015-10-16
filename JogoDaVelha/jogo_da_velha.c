c197cd265d6e50868fbb8ecb77bd438918f3476b
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-16 11:13:08 -0400

    Update jogo_da_velha.c
    comentando

--- JogoDaVelha/jogo_da_velha.c
+++ JogoDaVelha/jogo_da_velha.c
@@ -154,6 +154,7 @@ int tem_jogada(int i) //recebe o vetor de jogadas realizadas e a jogada que se d
 }
 
 
+// Programador: Delrick 
 int ganhou(char **mat)
 {
     if((mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2]) || (mat[1][0] == mat[1][1] && mat[1][1] == mat[1][2]) || (mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2]) || (mat[0][0] == mat[1][0] && mat[1][0] == mat[2][0]) || (mat[0][1] == mat[1][1] && mat[1][1] == mat[2][1]) || (mat[0][2] == mat[1][2] && mat[1][2] == mat[2][2]) ||  (mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2]) || (mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0]))
