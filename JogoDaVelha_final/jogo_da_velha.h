2a8e22bcbbcbc5b79874a42c38452a5aeb2222f0
Botinelly <lucas_botinelly@hotmail.com>
2015-10-26 19:25:36 -0400

    Update jogo_da_velha.h
    atualizando

--- JogoDaVelha_final/jogo_da_velha.h
+++ JogoDaVelha_final/jogo_da_velha.h
@@ -4,7 +4,7 @@ int jogada_correta(int vetor_posicao[9],int i);
 int jogada_valida(int *vetor_de_posicao, int jogada);
 void marca_jodada(int posicao,int i,int *vetor_posicao);
 void joga(int jogada,int matriz[3][3],int i);
-int ganhou(int mat[3][3],int i);
+int ganhou(int mat[3][3]);
 void play(int matriz[3][3],int vetor_posicao[9],int i,int modo_de_jogo,int jogada);
 int vencedor(int matriz[3][3]);
 
