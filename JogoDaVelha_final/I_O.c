d5e2b0a4e65068cafca841b53faf591835c364b8
Neto Oliveira <netow@myself.com>
2015-10-26 17:06:07 -0400

    Update I_O.c

--- JogoDaVelha_final/I_O.c
+++ JogoDaVelha_final/I_O.c
@@ -38,7 +38,7 @@ void saida(int saida, char matriz[3][3])
 
 //A função pega a entrada de dados, serão processados em uma outra função.
 // Programador: Wilson Oliveira Neto.
-void entrada(int entrada)
+int entrada(int entrada)
 {
     int jogs=0;
     switch(entrada)
@@ -46,5 +46,5 @@ void entrada(int entrada)
         case 1: scanf("%d", &jogs);
                 break;
     }
-
+    return jogs;
 }
