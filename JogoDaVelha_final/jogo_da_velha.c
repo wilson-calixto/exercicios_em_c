c06cfc5b5c3d8d122c63b280a752e42506fd2f2c
Botinelly <lucas_botinelly@hotmail.com>
2015-10-26 19:21:37 -0400

    Update jogo_da_velha.c
    atualizando

--- JogoDaVelha_final/jogo_da_velha.c
+++ JogoDaVelha_final/jogo_da_velha.c
@@ -318,29 +318,4 @@ int check_winner()
 	return 0;
 }
  
-#define for_ij for (i = 0; i < 3; i++) for (j = 0; j < 3; j++)
-int best_i, best_j;
-int test_move(int val, int depth)
-{
-	int i, j, score;
-	int best = -1, changed = 0;
- 
-	if ((score = check_winner())) return (score == val) ? 1 : -1;
- 
-	for_ij {
-		if (b2[i][j]) continue;
- 
-		changed = b2[i][j] = val;
-		score = -test_move(-val, depth + 1);
-		b2[i][j] = 0;
- 
-		if (score <= best) continue;
-		if (!depth) {
-			best_i = i;
-			best_j = j;
-		}
-		best = score;
-	}
- 
-	return changed ? best : 0;
-}
+
