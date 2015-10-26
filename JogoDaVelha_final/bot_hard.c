cc1de85084a0e74890fa2fb2d7c6880e0c546c10
JulianyRaiol <julianyraiol@outlook.com>
2015-10-26 17:59:43 -0400

    Atualizado

--- JogoDaVelha_final/bot_hard.c
+++ JogoDaVelha_final/bot_hard.c
@@ -1,33 +1,54 @@
+/* Implementado por Juliany, Evandro */
+
 #include <stdio.h>
 #define max 3
 
-int best_i, best_j;
+int best_i, best_j; /* Melhor posição */
 
-int test_move(int val, int depth)
+/* Implementação do MiniMax */
+
+int minimax(int posicao[9], int player) 
 {
-	int i, j, score;
-	int best = -1, changed = 0;
- 
-	if ((score = check_winner())) return (score == val) ? 1 : -1;
- 
-	for(i=0;i<max;i++)
-	{
-	        for(j=0;j<max;j++)
-	        {
-		        if (b2[i][j]) continue;
-         
-		        changed = b2[i][j] = val;
-		        score = -test_move(-val, depth + 1);
-		        b2[i][j] = 0;
-         
-		        if (score <= best) continue;
-		        if (!depth) {
-			        best_i = i;
-			        best_j = j;
-		        }
-		        best = score;
-	        }
-	  }
- 
-	return changed ? best : 0;
+    //Descobre a melhor posição para jogar
+    int winner = win(posicao);
+    if(winner != 0) return winner*player;
+
+    move = -1;
+    int score = -2;//Perde os movimentos anteriores
+    int i;
+    for(i = 1; i <= 9; ++i) {//Pesquisa todos os movimentos,
+        if(posicao[i] == 0) {//Se posição estiver vazia, ou seja, não tiver nenhuma jogada
+            posicao[i] = player;//Testa o movimento
+            int thisScore = -minimax(posicao, player*-1);
+            if(thisScore > score) {
+                score = thisScore;
+                move = i;
+            }//Escolhe o que é pior para o adversário, ou seja, aquela jogada que o bot tem mais chances de ganhar
+            posicao[i] = 0;//Se ele não achou, tenta novamente
+        }
+    }
+    if(move == -1) return 0;
+    return score;
 }
+
+void movimento_bot(int posicao[9]) {
+    int move = -1;
+    int score = -2;
+    int i;
+    for(i = 0; i < 9; ++i) {
+        if(posicao[i] == 0) {
+            posicao[i] = 1;
+            int tempScore = -minimax(posicao, -1);
+            posicao[i] = 0;
+            if(tempScore > score) {
+                score = tempScore;
+                move = i;
+            }
+        }
+    }
+    //É o melhor movimento.
+    posicao[move] = 1;
+}
+
+
+
