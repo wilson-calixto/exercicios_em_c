4e3e887ab2d009b96e444d7333441253d37b0ade
netoolii <netow@myself.com>
2015-10-26 16:49:48 -0400

    update

---
+++ JogoDaVelha_final/I_O.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+//a entrada vai ser o parâmetro para a saida, cada entrada corresponde a uma saida que é um printf
+
+void io (int entrada, char matriz[3][3])
+{
+    switch(entrada)
+    {
+        case 1: printf("******* JOGO DA VELHA *****\n\n1. 1 jogador\n2. 2 jogadores\n3. Sair*");
+                break;
+        case 2: printf("\nDificuldade: \n1. Facil\n2. Dificil\n");
+                break;
+        //case 3 é a saida da imprime matriz
+        case 3:  printf("   |   |\n %c | %c | %c\n___|___|___\n   |   |\n %c | %c | %c\n___|___|___\n   |   |\n %c | %c | %c\n   |   |\n",matriz[0][0],matriz[0][1],matriz[0][2],matriz[1][0],matriz[1][1],matriz[1][2],matriz[2][0],matriz[2][1],matriz[2][2]);
+                break;
+        case 4: printf("Digite a jogada jogador 2\n");
+                break;
+        case 5: printf("Digite a jogada jogador 1\n");
+                break;
+        case 6: printf("Posicao invalida! \n");
+                break;
+        case 7: printf("jogador 2 o 'O' ganhou\n");
+                break;
+        case 8: printf("jogador 1 o 'X' ganhou\n");
+                break;
+        case 9: printf("\nVelhou\n");
+                break;
+        case 10: printf("O");
+                break;
+        case 11: printf("X");
+                break;
+
+    }
+
+}
