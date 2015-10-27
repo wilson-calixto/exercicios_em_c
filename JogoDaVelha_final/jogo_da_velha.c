fc93dd0baf31dff7d101500dcf2ba39b2eae768c
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-27 08:43:20 -0400

    Update jogo_da_velha.c
    refatorando

--- JogoDaVelha_final/jogo_da_velha.c
+++ JogoDaVelha_final/jogo_da_velha.c
@@ -32,20 +32,6 @@ int jogada_valida(int *vetor_posicao, int jogada) //recebe o vetor de jogadas re
     return 1; //  se a jogada não existir ele retorna 1, valor para true
 }
 
-/* Função com jogadas randomicas de 1 a 9 */
-int play_easy_bot(int vetor_posicao[9])
-{
-    int jogada;
-    srand( (unsigned)time(NULL) );
-    
-    do{
-        	jogada=1 + ( rand() % 8 );
-	    
-    }while((jogada>9 || jogada<1)||!jogada_valida(vetor_posicao,jogada));
- 
-    return jogada;
- }
-
 /* Funcao que marca as jogadas que ja sairam.
  */
 // Programador: Raí Santos
