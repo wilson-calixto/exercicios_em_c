615ca9654b921e11aac738d1c2f4d6af6597581e
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-27 08:45:23 -0400

    Update jogo_da_velha.h
    refatorando

--- JogoDaVelha_final/jogo_da_velha.h
+++ JogoDaVelha_final/jogo_da_velha.h
@@ -1,11 +1,5 @@
-
-
-
 int jogada_valida(int *vetor_de_posicao, int jogada);
 void marca_jodada(int posicao,int i,int *vetor_posicao);
 void joga(int jogada,int matriz[3][3],int i);
 int ganhou(int mat[3][3]);
 void play(int matriz[3][3],int vetor_posicao[9],int i,int modo_de_jogo,int jogada);
-int vencedor(int matriz[3][3]);
-int minimax(int val, int profundidade, int matriz[3][3], int best_i, int best_j);
-
