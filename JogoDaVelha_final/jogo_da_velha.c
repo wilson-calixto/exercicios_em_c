d2dc24c776ef305aeead3431c3ab469217768d8b
wilsonccj <wilsoncalisto2012@Bol.com.br>
2015-10-27 08:40:04 -0400

    Update jogo_da_velha.c
    nova versao

--- JogoDaVelha_final/jogo_da_velha.c
+++ JogoDaVelha_final/jogo_da_velha.c
@@ -32,27 +32,6 @@ int jogada_valida(int *vetor_posicao, int jogada) //recebe o vetor de jogadas re
     return 1; //  se a jogada não existir ele retorna 1, valor para true
 }
 
-/*retira as coordenadas invalidas e as que ja sairam e retorna uma jogada valida*/   
-// programador : WISON CALIXTO
- int jogada_correta(int vetor_posicao[9],int i)
- {
- int jogada;
-        do{            
-	            if(i%2==0)// se i é par é o '0'
-	            {
-	            	saida(4);
-	            }
-	            else// se i é impar é o 'X'
-	            {
-	            	saida(5);
-	            }
-	            
-	            scanf("%d",&jogada);
-	      }while((jogada>9 || jogada<1)||!jogada_valida(vetor_posicao,jogada));
- return jogada;
- 
- }
-
 /* Função com jogadas randomicas de 1 a 9 */
 int play_easy_bot(int vetor_posicao[9])
 {
