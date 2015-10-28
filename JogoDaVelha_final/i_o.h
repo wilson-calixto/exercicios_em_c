799fd2602817d956293a0200ccdbb4391311ccee
Raí Santos da Soledade <raisoledade@outlook.com>
2015-10-28 17:28:48 -0400

    Update i_o.h

--- JogoDaVelha_final/i_o.h
+++ JogoDaVelha_final/i_o.h
@@ -1,4 +1,4 @@
 void saida(int controle,int saida);
 int entrada();
-void mostra_matriz(int matriz[3][3], char mprint[3][3]);
+void mostra_matriz(int matriz[LINHA][COLUNA], char mprint[LINHA][COLUNA]);
 int show_menu();
