53138c847488741874ad2a19263677c48d2e8c1d
Raí Santos da Soledade <raisoledade@outlook.com>
2015-10-28 21:10:16 -0400

    Update jogo_da_velha.h

--- JogoDaVelha_final/jogo_da_velha.h
+++ JogoDaVelha_final/jogo_da_velha.h
@@ -1,3 +1,6 @@
+#define LINHA 3
+#define COLUNA 3
+
 int jogada_valida(int vetor_posicao[9], int jogada);
 void marca_jodada(int posicao,int i,int vetor_posicao[9]);
 void joga(int jogada,int matriz[LINHA][COLUNA],int i);
