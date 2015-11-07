2876a8363715df09c7ca949804d4cc21e47d57d3
borgesbsb <borges.bnjamin@gmail.com>
2015-11-07 11:20:34 -0400

    Reformulando maze

--- pacman/teste_pink/maze.c
+++ pacman/teste_pink/maze.c
@@ -10,9 +10,39 @@ void print_maze (char maze[L][C])
   {
     for ( j = 0; j < C; j++ )
     {
-      printf ( "%c", maze[i][j] );
+      switch (maze[i][j])
+      {
+	      case '0':
+		      printf("[X]");
+		      break;
+	      case '7':
+		      printf("   ");
+      		      break;
+	      case '6':
+		      printf("   ");
+		      break;
+	      case 'G':
+		      printf(" G "); 
+		      break;
+	      case ' ':
+		      printf("   ");
+		      break;      
+      }
+
     }
     printf ( "\n" );
   }
+
+//    printf ( "\n" );
+//    printf ( "\n" );
+//for ( i = 0; i < L; i++ )
+//  {
+//    for ( j = 0; j < C; j++ )
+//    {
+//     printf("%c", maze[i][j] ); 
+//    }
+//    printf ( "\n" );
+//  }
+
 
 }
