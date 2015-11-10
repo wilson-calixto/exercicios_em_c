b514891ab46ad7c9fd558247fafccd3a770006ab
luizfernandobrandao <lf_brandao@yahoo.com>
2015-11-10 18:00:17 -0400

    Updating functions

--- pacman/teste_blinky_luiz/io_blinky.c
+++ pacman/teste_blinky_luiz/io_blinky.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#define R 19
+#define C 23
 
 int i, j;
 
-void input( char maze[19][23], int rows, int cols ) {
+void input( char maze[R][C], int rows, int cols ) {
 
 	for( i = 0; i < rows; i++ ) {
 		for( j = 0; j < cols; j++ ) {
@@ -15,6 +17,7 @@ int input_x() {
 
 	int x;
 
+	printf( "Digite a coordenada x: " );
 	scanf( "%d", &x );
 
 	return x;
@@ -24,11 +27,17 @@ int input_y() {
 
 	int y;
 
+	printf( "Digite a coordenada y: " );
 	scanf( "%d", &y );
 
 	return y;
 }
 
+void fill( char maze[R][C], char character, int x, int y ) {
+	
+	maze[x][y] = character;
+}
+			
 void print( char maze[19][23], int rows, int cols ) {
 
 	for( i = 0; i < rows; i++ ) {
--- pacman/teste_blinky_luiz/io_blinky.h
+++ pacman/teste_blinky_luiz/io_blinky.h
@@ -1,4 +1,5 @@
 void input( char maze[19][23], int, int );
 int input_x( void );
 int input_y( void );
+void fill( char maze[19][23], char, int, int );
 void print( char maze[19][23], int, int );
