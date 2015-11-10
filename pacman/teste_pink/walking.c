387f643cd93b626f33634fed2aba87be1885ee15
borgesbsb <borgesbsb@gmail.com>
2015-11-10 09:41:08 -0400

    caminhando até encontrar caminhos diferentes

--- pacman/teste_pink/walking.c
+++ pacman/teste_pink/walking.c
@@ -1,6 +1,12 @@
+//Em construção!!!
+
 void walking(char maze[23][30], int ghost[3], int target[2])
 {
 	int x,y; 
+	y =  ghost[0];
+	x =  ghost[1]; 
+
+
 	while (maze[x][y] != '7')
 	{
 		switch (ghost[2])
@@ -20,12 +26,18 @@ void walking(char maze[23][30], int ghost[3], int target[2])
 		}
 		if (validate_position(maze, int x , int y))
 		{
+			ghost[0] = y;      
+			ghost[1] = x;
 			maze[x][y]  = 'G'; 
 		}else
 		{
 			x = ghost[1];
 			y = ghost[0];
 			ghost[2] += 1;
+			if (ghost[2] > 4)
+			{
+				ghost[2] = 1;
+			}
 		}
 	}
 
