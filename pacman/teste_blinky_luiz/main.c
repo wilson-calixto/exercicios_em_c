561cc89ca207af317bce492e1e3a0285750607db
luizfernandobrandao <lf_brandao@yahoo.com>
2015-11-10 19:35:16 -0400

    correcting errors

--- pacman/teste_blinky_luiz/main.c
+++ pacman/teste_blinky_luiz/main.c
@@ -8,8 +8,8 @@ int main( void ) {
 
 	char maze[R][C];
 
-	//input( maze, R, C );
-	//print( maze, R, C );
+	input( maze, 19, 23 );
+	print( maze, 19, 23 );
 	fill( maze, 'P', 9, 9 );
 	printf( "(%d,%d)\n", search_x( maze, 'P' ), search_y( maze, 'P' ) );
 	
