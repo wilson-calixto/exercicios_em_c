0ce4e8913ec5b636fb58b6b3b49aa8d3aec35180
borgesbsb <borges.bnjamin@gmail.com>
2015-11-10 06:39:02 -0400

    walking.c

--- pacman/teste_pink/walking.c
+++ pacman/teste_pink/walking.c
@@ -1,37 +1,8 @@
 void walking(char maze[23][30], int ghost[3], int target[2])
 {
 	int x,y; 
-	x = ghost[1];
-	y = ghost[0];
-
 	while (maze[x][y] != '7')
 	{
-		//Percorre o caminho em linha até que encontre um obstáculo, ou seja, retorne negativo da função
-		//validate position
-		while (!validate_position(maze, x, y))
-		{
-			switch (ghost[2])
-			{
-				case 1:
-					x = x+1;
-					break;
-				case 2:
-					y = y+1;
-					break;
-				case 3:
-					x = x-1;
-					break;
-				case 4:
-					y = y-1;
-					break;	
-			}
-			ghost[2] +=1;
-			if (ghost[2] > 4) 
-			{
-				ghost[2] = 1;
-			}
-		}
-		
 		switch (ghost[2])
 		{
 			case 1:
@@ -46,6 +17,15 @@ void walking(char maze[23][30], int ghost[3], int target[2])
 			case 4:
 				y = y-1;
 				break;	
+		}
+		if (validate_position(maze, int x , int y))
+		{
+			maze[x][y]  = 'G'; 
+		}else
+		{
+			x = ghost[1];
+			y = ghost[0];
+			ghost[2] += 1;
 		}
 	}
 
