1e9c436ddfecf5b4deba38a4bccddc4956a37b3a
luizfernandobrandao <lf_brandao@yahoo.com>
2015-11-10 18:02:58 -0400

    Updating functions

--- pacman/teste_blinky_luiz/target.c
+++ pacman/teste_blinky_luiz/target.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#define R 19
+#define C 23
 
 int distance( int pac_x, int pac_y, int blinky_x, int blinky_y ) {
 
@@ -12,3 +14,31 @@ int distance( int pac_x, int pac_y, int blinky_x, int blinky_y ) {
 
 	return d;
 }
+
+int search_x( char maze[][23], char character ) {
+
+	int i, j;
+
+	for( i = 0; i < R; i++ ) {
+		for( j = 0; j < C; j++ ) {
+			if( maze[i][j] == character )
+				return i;
+		}
+	}
+}
+
+int search_y( char maze[][23], char character ) {
+
+	int i, j;
+
+	for( i = 0; i < R; i++ ) {
+		for( j = 0; j < C; j++ ) {
+			if( maze[i][j] == character )
+				return j;
+		}
+	}
+}
+
+/*void better_way_x( char maze[][23], int pac_x, int pac_y, int blink_x, int blink_y ) {
+
+	distance( search_x( maze, 'o' ), search_y( maze, 'o' ), search_x( maze, 'B' ), search_y( maze, 'B' ) ); */
--- pacman/teste_blinky_luiz/target.h
+++ pacman/teste_blinky_luiz/target.h
@@ -1,1 +1,3 @@
 int distance( int, int, int, int );
+int search_x( char maze[19][23], char );
+int search_y( char maze[19][23], char );
