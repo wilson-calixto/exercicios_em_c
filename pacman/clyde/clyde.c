d5404a6874418a470c9affc13ca31f582bee64aa
lucasfrota <frota_lv@hotmail.com>
2015-11-11 15:21:47 -0400

    atualizacao de clyde.c

--- pacman/clyde/clyde.c
+++ pacman/clyde/clyde.c
@@ -29,4 +29,21 @@ int distancia (int ghost[3], int pac[3])
     return a;
 }
 
+int clayde(int ghost[3],int pac[3])
+{
+	int t[2], d;
+	d= distancia(ghost,pca);
+	if (d > 8)
+	{
+		t[0]= pac[0];
+		t[1]= pac[1];
+	}
+	else
+	{
+		t[0]= 22;
+		t[1]= 0;
+	}
+	return t;
+}
+
 
