4c79bb6eb8e82781f050eafbe1e0c12ab2c731f7
netoolii <netow@myself.com>
2015-11-11 15:22:58 -0400

    pink

--- pacman/teste_pink/def_target_pink.c
+++ pacman/teste_pink/def_target_pink.c
@@ -1,67 +1,73 @@
 #include "validate_position.h"
-#include <stdio.h>
+#include <stdlib.h>
 #define UP 1
 #define RIGHT 2
 #define DOWN 3
 #define LEFT 4
 #define C 30
-#define L 23
+#define L 23// wilson calixto: trocar os i j e k por referencias ao vetor ghost
 #define TARGET 4
+#define LINHA 1
+#define COLUNA 0
+#define TRUE 1
+
 /* Defina o alvo para o fantasma pink
  * de acordo com a regra são 4 "casas"
  * a frente do pacman */
-void def_target_pink(char maze[L][C],int ghost[3],int pacman[3],int target[2])
+void *def_target_pink(char maze[L][C],int ghost[3],int pacman[3], int target[])
 {                          
-  int result,copia[3],i=ghost[0],j=ghost[1],d=ghost[2];
-
-  copia[0]=ghost[0];
-  copia[1]=ghost[1];
-  copia[2]=ghost[2];
+  int result,i=ghost[0],j=ghost[1],d=ghost[2],*p;
+  p = target;
+ // ghost[0] linha
+ // ghost[1] coluna
+ // ghost[2] direçao
   if (d == UP) // se a direção to pacman for para cima valida 4 "casas" para cima
   {
-    copia[0]-=TARGET;
-    result = validate_position (maze,copia);// falta terminar de adaptar de 2 variaveis para um vetor
-    if (result == 1)
+
+    ghost[COLUNA]-=TARGET;
+    result = validate_position (maze,ghost);// falta terminar de adaptar de 2 variaveis para um vetor
+    if (result == TRUE)
     {
-      target[0]=i-TARGET;
-      target[1]=j;
-     // maze[i-TARGET][j] = 'T';
-    
+      *p = i-TARGET;
+       p++;
+      *p = j;
+      maze[i-TARGET][j] = 'T';
     }
   }
   else if (d == RIGHT)
   {
-    copia[1]+=TARGET;
-    result = validate_position (maze,copia);
-    if (result == 1)
+    ghost[LINHA]+=TARGET;
+    result = validate_position (maze,ghost);
+    if (result == TRUE)
     {
-      target[0]=i;
-      target[1]=j+TARGET;
-    //  maze[i][j+TARGET] = 'T';
-      
+      *p =i;
+       p++;
+      *p =j+TARGET;
+      maze[i][j+TARGET] = 'T';
     }
   }
   else if (d == DOWN)
   {
-    copia[0]+=TARGET;
-    result = validate_position (maze,copia);
-    if (result == 1)
+    ghost[COLUNA]+=TARGET;
+    result = validate_position (maze,ghost);
+    if (result == TRUE)
     {
-      target[0]= i+TARGET;
-      target[1]= j;
-    //  maze[i+TARGET][j] = 'T';
-     
+      *p= i+TARGET;
+       p++;
+      *p =j;
+      maze[i+TARGET][j] = 'T';
     }
   }
   else if (d == LEFT)
   {
-    copia[1]-=4;
-    result = validate_position (maze,copia);
-    if (result == 1)
+    ghost[LINHA]-=4;
+    result = validate_position (maze,ghost);
+    if (result == TRUE)
     {
-      target[0]=i;
-      target[1]=j-TARGET;
-     // maze[i][j-TARGET];
-	}
+      *p=i;
+       p++;
+      *p =j-TARGET;
+      maze[i][j-TARGET];
+    }
   }
 }
