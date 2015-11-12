f515d93f038c2876f8c0dfb7061ec4b22692c336
Botinelly <lucas_botinelly@hotmail.com>
2015-11-12 16:04:36 -0400

    Atualizações Inky

--- pacman/teste_blue/Inky.c
+++ pacman/teste_blue/Inky.c
@@ -37,32 +37,29 @@ int* dist (int pacman[3])
 	if (posicao_pacman == DOWN) operacao_dist(pacman, dir, -2, 0);
 		
 	if (posicao_pacman == LEFT) operacao_dist(pacman, dir,   2, 0);
-			
+        			
 	return 	dir;
 }
 
-void verifica_target(int *target, char *maze)
+void verifica_target(int target[2], char maze[23][30])
 {
-        int boo=1;
-        while(boo==1)
+        if (target[LINHA]>22)
         {
-                if (maze[target[LINHA]][target[COLUNA]]=='0')
-                {
-                        target[COLUNA]--;                               
-                }
-                else
-                {
-                        boo=0;
-                }
+               target[LINHA]=22;
         }
+        if (target[COLUNA]>29)
+        {
+               target[COLUNA]=29;
+        }
+        
 }        
 
-void print_maze(char maze[L][C]) 
+void print_maze(char maze[23][30]) 
 {   	
 	int i, j;
-	for (i=0; i<L; i++)
+	for (i=0; i<23; i++)
         {
-              	for (j=0; j<C; j++)
+              	for (j=0; j<30; j++)
             	{
                 	printf("%c", maze[i][j]);	
 	        }
@@ -71,17 +68,17 @@ void print_maze(char maze[L][C])
     printf("\n");
 }
 
-void marca_target(char *maze, int *target)
+void marca_target(char maze[23][30], int target[2])
 {
         maze[target[LINHA]][target[COLUNA]]='T';
 }
 
-void marca_ghost(char *maze, int *ghost)
+void marca_ghost(char maze[23][30], int *ghost)
 {
         maze[ghost[LINHA]][ghost[COLUNA]]='B';
 }
 
-void zera_vetor(int *vet)
+void zera_vetor(int vet[4])
 {
         int i;
         for (i=0;i<4;i++)
@@ -90,36 +87,40 @@ void zera_vetor(int *vet)
         }
 }
 
-void anda(int *ghost, char *maze, int *target)
+void anda(int ghost[2], char maze[23][30], int target[2])
 {
-        int dist[4], i=0, j=0, menor=0;
-        char vet[8];
-        while(maze[ghost[LINHA]][ghost[COLUNA]+1]=='T' || maze[ghost[LINHA]][ghost[COLUNA]-1]=='T' || maze[ghost[LINHA]+1][ghost[COLUNA]]=='T' || maze[ghost[LINHA]-1][ghost[COLUNA]]=='T')
+        int dist[4], i=0, j=0, menor=0, vet[8];
+        while(maze[ghost[LINHA]][ghost[COLUNA]+1]!='T' || maze[ghost[LINHA]][ghost[COLUNA]-1]!='T' || maze[ghost[LINHA]+1][ghost[COLUNA]]!='T' || maze[ghost[LINHA]-1][ghost[COLUNA]]!='T')
         {
-                while (maze[ghost[LINHA]][ghost[COLUNA]+1]!='7' || (maze[ghost[LINHA]][ghost[COLUNA]-1]!='7' || (maze[ghost[LINHA]+1][ghost[COLUNA]]!='7' || (maze[ghost[LINHA]-1][ghost[COLUNA]]!='7')
+                while (maze[ghost[LINHA]][ghost[COLUNA]+1]!='7' || maze[ghost[LINHA]][ghost[COLUNA]-1]!='7' || maze[ghost[LINHA]+1][ghost[COLUNA]]!='7' || maze[ghost[LINHA]-1][ghost[COLUNA]]!='7')
         {
-                        if (maze[ghost[LINHA]][ghost[COLUNA]+1]!='0')
+                        if (maze[ghost[LINHA]-1][ghost[COLUNA]]!='0')
                         {
                                 ghost[COLUNA]++;
                                 marca_ghost(maze, ghost);
+                                print_maze(maze);         
+                        
                         }
                         else if (maze[ghost[LINHA]][ghost[COLUNA]-1]!='0')
                         {
                                 ghost[COLUNA]--;
                                 marca_ghost(maze, ghost);
+                                print_maze(maze);
                         }
                         else if (maze[ghost[LINHA]+1][ghost[COLUNA]]!='0')
                         {
                                 ghost[LINHA]++;        
                                 marca_ghost(maze, ghost);
+                                print_maze(maze);
                         }
-                        else if (maze[ghost[LINHA]-1][ghost[COLUNA]]!='0')
+                        else if (maze[ghost[LINHA]][ghost[COLUNA]-1]!='0')
                         {
                                 ghost[COLUNA]--;
                                 marca_ghost(maze, ghost);
+                                print_maze(maze);
                         }
                 }
-                if (maze[ghost[LINHA]][ghost[COLUNA]+1]=='7' || (maze[ghost[LINHA]][ghost[COLUNA]-1]=='7' || (maze[ghost[LINHA]+1][ghost[COLUNA]]=='7' || (maze[ghost[LINHA]-1][ghost[COLUNA]]=='7')       
+                if (maze[ghost[LINHA]][ghost[COLUNA]+1]=='7' || (maze[ghost[LINHA]][ghost[COLUNA]-1]=='7') || (maze[ghost[LINHA]+1][ghost[COLUNA]]=='7') || (maze[ghost[LINHA]-1][ghost[COLUNA]]=='7'))       
                 {
                         vet[0]=ghost[LINHA];
                         vet[1]=ghost[COLUNA]+2;
@@ -138,19 +139,19 @@ void anda(int *ghost, char *maze, int *target)
                 zera_vetor(dist);
                 if (maze[vet[0]][vet[1]]!='0')
                 {
-                        dist[0]=sqrt((vet[LINHA]-target[LINHA])**2+(vet[COLUNA]-target[COLUNA]**2);
+                        dist[0]=sqrt(pow((vet[LINHA]-target[LINHA]), 2)+pow((vet[COLUNA]-target[COLUNA]), 2));
                 }
                 if (maze[vet[2]][vet[3]]!='0')
                 {
-                        dist[1]=sqrt((vet[2]-target[LINHA])**2+(vet[3]-target[COLUNA]**2);
+                        dist[1]=sqrt(pow((vet[2]-target[LINHA]),2)+pow((vet[3]-target[COLUNA]),2));
                 }        
                 if (maze[vet[4]][vet[5]]!='0')
                 {
-                        dist[2]=sqrt((vet[4]-target[LINHA])**2+(vet[5]-target[COLUNA]**2);
+                        dist[2]=sqrt(pow((vet[4]-target[LINHA]),2) + pow((vet[5]-target[COLUNA]),2));
                 }
                 if (maze[vet[6]][vet[7]]!='0')
                 {
-                        dist[3]=sqrt((vet[6]-target[LINHA])**2+(vet[7]-target[COLUNA]**2);
+                        dist[3]=sqrt(pow((vet[6]-target[LINHA]),2) + pow((vet[7]-target[COLUNA]),2));
                 }
                 
                 for(i=0;i<4;i++)
@@ -170,30 +171,77 @@ void anda(int *ghost, char *maze, int *target)
                 if (menor==dist[0])
                 {
                         maze[ghost[LINHA]][ghost[COLUNA]+2]='B';
+                        print_maze(maze);
                 }
                 else if (menor==dist[1])
                 {
                         maze[ghost[LINHA]][ghost[COLUNA]-2]='B';      
+                        print_maze(maze);
                 }
                 else if (menor==dist[2])
                 {
                         maze[ghost[LINHA]+2][ghost[COLUNA]]='B';     
+                        print_maze(maze);
                 }
                 else if (menor==dist[3])
                 {
                         maze[ghost[LINHA]-2][ghost[COLUNA]]='B';
+                        print_maze(maze);
                 }
                 
         }
 }
-void inky(int *dir, int *red, char *maze)
+void inky(int dir[2], int red[3], char maze[23][30], int ghost[3])
 {
 
         int target[2], boo=1;
-        target[LINHA]=(dir[LINHA]-red[LINHA])+dir[LINHA];
-        target[COLUNA]=(dir[COLUNA]-red[COLUNA])+dir[COLUNA];
+        target[LINHA]=(red[LINHA]-dir[LINHA])+dir[LINHA];
+        target[COLUNA]=(red[COLUNA]-dir[COLUNA])+dir[COLUNA];
+        printf("%d", target[0]);
+        printf("%d", target[1]);
         verifica_target(target, maze);
         marca_target(maze, target);
-        anda(ghost, maze, target);                     
-        
+        print_maze(maze);
+        anda(ghost, maze, target);            
+}
+
+int main (int argc, const char *argv[])
+{
+        char maze[L][C]={{'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'},
+                    {'0',' ',' ',' ',' ',' ','7',' ',' ',' ',' ',' ',' ',' ','0',' ',' ',' ',' ',' ',' ',' ','7',' ',' ',' ',' ',' ',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0','0',' ','0','0','0','0','0',' ','0'},
+                    {'0','7',' ',' ',' ',' ','7',' ',' ',' ',' ',' ',' ','7',' ','7',' ',' ',' ',' ',' ',' ','7',' ',' ',' ',' ',' ','7','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ',' ',' ',' ',' ','7','0',' ',' ',' ',' ',' ',' ','0',' ',' ',' ',' ',' ',' ','0','7',' ',' ',' ',' ',' ',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ',' ',' ',' ',' ','6',' ','6',' ',' ',' ',' ',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ',' ','7','0','0','0','0','0','0','0','0','0','0','0','7',' ',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0','7',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','7','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0',' ','0'},
+                    {'0',' ',' ',' ',' ',' ','7',' ','7',' ',' ',' ',' ',' ','0',' ',' ',' ',' ',' ','7',' ','7',' ',' ',' ',' ',' ',' ','0'},
+                    {'0',' ','0','0','0','0',' ','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0','0',' ','0','0','0','0','0',' ','0'},
+                    {'0','7',' ',' ',' ',' ','7',' ','7',' ',' ',' ',' ','6',' ','6',' ',' ',' ',' ','7',' ','7','0',' ',' ',' ',' ','7','0'},
+                    {'0',' ','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0',' ','0','0','0',' ','0'},
+                    {'0','7',' ',' ',' ',' ',' ','0',' ',' ',' ',' ',' ',' ','0',' ',' ',' ',' ',' ',' ','0',' ',' ','7',' ',' ',' ','7','0'},
+                    {'0',' ','0','0','0','0','0','0','0','0','0','0','0',' ','0',' ','0','0','0','0','0','0','0','0','0','0','0','0',' ','0'},
+                    {'0',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','7',' ','7',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','0'},
+                    {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'}};
+        int red[3], pacman[3], *dis, ghost[3];
+        dis=malloc(sizeof(int) * 2);
+        red[0]=4;
+        red[1]=2;
+        red[2]=RIGHT;
+        pacman[0]=4;  
+        pacman[1]=7;
+        pacman[2]=RIGHT;
+        ghost[0]=1;
+        ghost[1]=1;
+        ghost[2]=RIGHT;
+        dis=dist(pacman);
+        inky(dis, red, maze, ghost);
+        return 0;
 }
