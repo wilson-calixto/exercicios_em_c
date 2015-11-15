2dfa698c298124b991e53dd5df3dab223e593bc7
JulianyRaiol <julianyraiol@outlook.com>
2015-11-15 16:38:43 -0400

    Atualização

---
+++ pacman/teste_blinky/verifica_target.c
@@ -0,0 +1,41 @@
+void verifica_target(char maze[23][30], target[2])
+{
+	if (target[0]>29)
+	{
+		target[0]=28;
+		if (maze[target[0]][target[1]]=='0')
+		{
+			if (maze[target[0]][target[1]+1]=='0' && maze[target[0]][target[1]-1]='0' && maze[target[0]+1][target[1]]='0' && maze[target[0]-1][target[1]]='0' )
+		        {
+		                if (target[0]>14)
+		                {
+		                        target[0]--;
+		                        verifica_target(maze, target);
+		                }
+		                else 
+		                {
+		                        if (target[0]<14)
+		                        {
+		                                target[0]++;
+		                                verifica_target(maze, target);
+		                        }
+		                }            
+		                
+		        }
+		}
+		else
+		{
+		       maze[target[0]][target[1]]='T';
+		}
+				
+	        else
+	        {
+	                maze[target[0]][target[1]]='T';
+	        }
+	}
+	if (target[1]>22)
+	{
+		target[1]=21;
+	}
+
+}
