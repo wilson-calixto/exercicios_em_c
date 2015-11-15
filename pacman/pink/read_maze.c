eced9fc66f5a55dfec10f4158ddf98be8edc8a0b
wilsonccj <wilsoncalisto2012@bol.com.br>
2015-11-15 14:03:26 -0400

    nova funcao

---
+++ pacman/pink/read_maze.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+#define C 30
+#define L 23
+
+void read_maze (char maze[L][C])
+{
+	int aux,i,j;
+	
+
+	for( i = 0; i < L; i++ ) 
+	{
+		for( j = 0; j < C; j++ ) 
+		{
+			scanf("%d",&aux );
+			printf("aux: %d",aux);
+			if(aux==1)
+			{
+				maze[i][j]=' ';
+			}
+		    if(aux==0)
+			{
+				maze[i][j]='0';
+			}
+			if(aux==7)
+			{
+				maze[i][j]='7';
+			}
+			if(aux==6)
+			{
+				maze[i][j]='6';
+			}			
+						
+		}
+	}
+
+}
+
---
+++ pacman/pink/read_maze.h
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+#define C 30
+#define L 23
+
+void read_maze (char maze[L][C]);
+
