c18684454d22e3236cc9cf439b339066c79945d4
borgesbsb <borges.bnjamin@gmail.com>
2015-11-16 16:03:11 -0400

    main function

--- pacman/main.c
+++ pacman/main.c
@@ -1,87 +1,72 @@
 #include <stdio.h>
-#include "pink/maze.h"
-#include "pink/read_maze.h"
-#include "pink/move.h"
-#include "calcula_target.h"
-#include "pink/validate_position.h"
-#include "pink/def_target_pink.h"
+#include "maze.h"
+#include "move.h"
+#include "math.h"
 
-#include "pink/partvariavel.h"
-#include "pink/partghost.h"
-#include "pink/partpackman.h"
-#include "util.c/i_o.h"
-
-#define MENU 12
 #define BLINK 1
 #define PINK 2
 #define INKY 3
 #define CLYDE 4
 #define C 30
 #define L 23
-#define UP 1
-#define RIGHT 2
-#define DOWN 3
-#define LEFT 4
-/* WILSON CALIXTO : FALTA TREANFORMARA MATRIZ EM TXT E CHAMAR A FUNCAO WALKING 
-OBS: PREFERI FAZER O VETOR TARGET COMO GLOBAL PARA FACILITAR A FORMA DE DEBUGAR */
-int target[2];
-int main ( int argc, const char *argv[] )
+
+
+// Este MAIN foi criado para testar a função walking
+
+		/* ----------  end of function main  ---------- */
+
+int main (int argc, char *argv[])
 {
- int ghost[3],pacman[3],ghost_number,aux,i,j;
-  int caminho[30][2];
-  char maze[L][C];     
-
-	read_maze(maze);	
-
-  out(MENU,0);//funcao esta funcionando ps trocar as entradas e saidas para o_i menu
-  
-  print_maze (maze);   
-  
-  ghost_number=partvariavel(ghost_number);
-
-
-  partghost(ghost,maze);
-  partpackman (pacman,maze);//sendo testada	
-
-
- printf("numero:%d\n", ghost_number);
- printf("pacman[0]==%d\n", pacman[0]);
- printf("pacman[1]==%d\n", pacman[1]);
- printf("pacman[2]==%d\n", pacman[2]);
- printf("ghost[0]==%d\n", ghost[0]);
- printf("ghost[1]==%d\n", ghost[1]);
- printf("ghost[2]==%d\n", ghost[2]);  
- position (maze,ghost,'G');
- position (maze,pacman,'P' );//mudar os parametros
- // clear_screen();
-
- if (ghost_number == BLINK)
+  int ghost[3],pacman[3],target[2], number_ghost=0;
+  char maze[L][C];
+  char ch,path[20] = "maze.txt";
+  int i=0,j=0;
+  while (number_ghost != 9)
   {
-	printf("consegui, voce vai rodar o fantasma 1 \n\n");
-  }
-  else if (ghost_number == PINK)
-  {
-	printf("consegui, voce vai rodar o fantasma 2 \n\n");
-	//out(4,0);//funcao esta funcionando ps trocar as entradas e saidas para o_i 
-  	def_target_pink(maze,ghost,pacman,target);//deve retornar um ponteiro
-       
-  }
-  else if (ghost_number == INKY)
-  {
-	printf("consegui, voce vai rodar o fantasma 3\n\n");
-  }
-  else if (ghost_number == CLYDE)
-  {
-	printf("consegui, voce vai rodar o fantasma 4\n\n");
-  }
-      
-  printf ("target i: %d \n",target[0]);
-  printf ("target j: %d \n",target[1]);
+  	//clear_screen();
+	load_maze(maze,path);
+  	//print_maze(maze);
+  	printf ( "\t>>> GHOSTS <<<\n" );
+  	printf ( "1.BLINK  2.PINK  3.INKY  4.CLYDE  9.SAIR   \n" );
+  	do
+  	{  	  
+  	  printf ( "Qual o numero do Ghost? = " );
+          scanf ("%d", &number_ghost);
+  	}while(number_ghost > 4 || number_ghost < 1 || number_ghost == 9  );  
   
-  print_maze (maze);
-  walking(maze,ghost,pacman);
-  
+  //Lendo cordenadas para o ghost 
+  	if (number_ghost != 9)
+  	{
+		
+  		print_maze(maze);
+  		read_player(maze,ghost,'3'); 
+  		read_player(maze,pacman,'5');
+	        switch (number_ghost)
+		{
+			case 1:
+				//void blink(maze,ghost,pacman,target);
+				position(maze,pacman,'8');
+				target[0] = pacman[0];
+				target[1] = pacman[1];
+				break;
+			case 2:
+			//	void pink(maze,ghost,pacman,target);
+			//	position(maze,target,'T');
+				break;
+			case 3:
+				ink(red,pacman,target);
+				position(maze,target,'8');
+				break;
+			case 4:
+			//	void clyde(maze,ghost,pacman,target);
+			//	position(maze,target,'8');
+		        	break;
 
- 
+		}	
+ 	 	
+		walking(maze,ghost,target);//mudar os parametros
+  		print_maze (maze);
+  	}
+  }
   return 0;
 }
