11bcd7f155b6f8f4472f2f3c34af8152737e4163
luizfernandobrandao <lf_brandao@yahoo.com>
2015-11-16 11:46:44 -0400

    Atualizando behavior.h

--- pacman/teste_blinky_luiz/behavior.h
+++ pacman/teste_blinky_luiz/behavior.h
@@ -2,6 +2,4 @@
 #define C 19
 
 int distance( int, int, int, int );
-int search_x( int maze[R][C], char );
-int search_y( int maze[R][C], char );
-void move_blinky( int maze[R][C], int, int );
+void move_blinky( int maze[R][C], int, int, int, int );
