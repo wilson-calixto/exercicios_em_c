b574ebd0ab810f606ec198c0bb32901168df77af
Botinelly <lucas_botinelly@hotmail.com>
2015-11-16 17:10:50 -0400

    aiuewq

--- pacman2/inky.c
+++ pacman2/inky.c
@@ -1,11 +1,14 @@
 #include "calcula_target.h"
 void inky(int pacman[3], int target[2])
 {
-       int red[2];
+       int red[3];
        printf("Insira a linha do Ghost Red: ");
        scanf("%d", &red[0]);
        printf("Insira a coluna do Ghost Red: ");
        scanf("%d", &red[1]);
+       printf("Insira a direção do Ghost Red: ");
+       scanf("%d", &red[2]);
+
        dist(pacman);
        calcula_target(red, pacman, target);                         
 }
