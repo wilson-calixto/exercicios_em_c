43d887618948aa58ffbe9154e0e0a5b5617ef3f3
Raí Santos da Soledade <raisoledade@outlook.com>
2015-11-16 16:21:20 -0400

    Update inky.c

--- pacman/inky.c
+++ pacman/inky.c
@@ -1,8 +1,14 @@
 #include "calcula_target.h"
-void inky(int pacman[3], int red[3], int target[2])
+#include "dist.h"
+void inky(int pacman[3], int target[2])
 {
+	int red[3], a, b;
+	
+	printf("insira a linha do red: ");
+	scanf("%d", &red[0]);
+	printf("insira a coluna do red: ");
+	scanf("%d", &red[1]);
 	dist(pacman);
        calcula_target(red, pacman, target);                         
 }
-
 
