c3c2e7e616cd5a7ed9af07fa5723107ada690908
netoolii <netow@myself.com>
2015-11-16 13:37:13 -0400

    adicionando i_o

--- pacman/pink/partghost.c
+++ pacman/pink/partghost.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include "validate_position.h"
 #include "maze.h"
+#include "../util.c/i_o.h"
 #define C 30
 #define L 23
+#define POSIÇÃO_GHOST 3
+#define SEM_PARAMETRO 0
+#define X 4
+#define Y 5
 
 void partghost (int *ghost,char maze[L][C])
 {
   	do
 	{  	  
-  	        printf ( "Direção do ghost:" );
-  	        scanf ("%d",&ghost[2]);
+		out(POSIÇÃO_GHOST,SEM_PARAMETRO);
+		in(ghost[2]);
   	}while(ghost[2]>4 ||ghost[2]<1);
   	//ghost_number = 2;
   	do
   	{
-  		printf ( "Qual a posição do GHOST? (x,y)\n" );
-  		printf ( "x: " );
-  		scanf ( "%d", &ghost[0] ); // referente a linha - ghost
-  		printf ( "y: " );
-  		scanf ( "%d", &ghost[1] ); // referente a coluna - ghost
+  		out(POSIÇÃO_GHOST,SEM_PARAMETRO);
+  		out(X,SEM_PARAMETRO); 
+		in(ghost[0]); // referente a linha - ghost
+  		out(Y,SEM_PARAMETRO);
+		in(ghost[1]); // referente a coluna - ghost
   	}while (!validate_position(maze,ghost));
-                                           //mudar os parametros
+                                        
 }
