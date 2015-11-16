f7ec93f4bd6460de502a2d10ac8832314911e700
borgesbsb <borges.bnjamin@gmail.com>
2015-11-15 22:35:09 -0400

    Inserindo parametro 8 na validate position

--- pacman/teste_blue/inkyteste.c
+++ pacman/teste_blue/inkyteste.c
@@ -22,7 +22,7 @@ void clear_screen()
 
 int validate_position(char maze[L][C], int linha, int coluna)
 {
-  if (maze[linha][coluna]== ' '  || maze[linha][coluna] == '5' || maze[linha][coluna] == '7' || maze[linha][coluna] == '6' )
+  if (maze[linha][coluna]== ' '  || maze[linha][coluna] == '5' || maze[linha][coluna] == '7' || maze[linha][coluna] == '6' || maze[linha][coluna] == '8'  )
   {
     return 1;
   }
