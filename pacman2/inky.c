24bed06f626013503b82500aed34ac512a6a5b82
raisantos <raisoledade@outlook.com>
2015-11-18 14:09:29 -0400

    dg

--- pacman2/inky.c
+++ pacman2/inky.c
@@ -1,21 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "calcula_target.h"
 #include "dist2.h"
-#include <stdio.h>
+#include "move.h"
 
 void inky(int pacman[3], int target[2], char maze[23][30])
 {
        int red[3];
-       printf("Insira a linha do Ghost Red: \n");
-       scanf("%d", &red[0]);
-       printf("Insira a coluna do Ghost Red: \n");
-       scanf("%d", &red[1]);
-       printf("Insira a direção do Ghost Red: \n");
-       scanf("%d", &red[2]);
-       position(maze, red, '4');
+       do
+  	{
+		printf ( "Qual a posição do RED? (x,y)\n" );
+  		printf ( "x: " );
+  		scanf ( "%d", &red[1] ); // referente a coluna - ghost
+  		printf ( "y: " );
+  		scanf ( "%d", &red[0] ); // referente a linha - ghost
+		clear_screen();
+	}while (!validate_position(maze,red[0],red[1])); 
+      
+      position(maze, red, '4');
+      print_maze(maze);
+      do
+      {
+      		printf ( "Direção do red = 1-Down , 2-Right, 3-Up, 4-Left = " );
+      		scanf ("%d", &red[2]);
+      }while (red[2] > 4 ||red[2] < 1);
+      
+       
        dist2(pacman);
        calcula_target(red, pacman, target);                         
        verifica_target(maze, target);
-       
 }
 
 
