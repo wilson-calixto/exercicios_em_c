4b19fb7c562e1700ab8c5befce6490d39171e78c
Manoel Victor <manoelvictor1602@hotmail.com>
2015-12-01 17:30:27 -0400

    Update util.h

--- Agenda(Falta modularizar)/util.h
+++ Agenda(Falta modularizar)/util.h
@@ -1,6 +1,6 @@
 void apaga(char arquivo[]);
 void copia(char dest[], char origem[]);
-void cria_agenda_arquivo(char arquivo[]);
+void cria_agenda_arquivo(const char arquivo[]);
 void edita(char arquivo[]);
 void limpa_arquivo(char arquivo[]);
 void lista(char arquivo[]);
