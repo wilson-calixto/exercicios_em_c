cba9abb443fe623d0a277c43814d7b7957e0b85d
wilsonccj <wilsoncalisto2012@Bol.com.br>
2016-01-20 14:06:11 -0400

    Update main.c
    aqui esta eduardo

--- pacman/main.c
+++ pacman/main.c
@@ -1,3 +1,5 @@
+/*
+
 #include <stdio.h>
 #include "maze.h"
 #include "move.h"
@@ -12,8 +14,7 @@
 
 // Este MAIN foi criado para testar a função walking
 
-		/* ----------  end of function main  ---------- */
-
+		/* ----------  end of function main  ---------- 
 int main (int argc, char *argv[])
 {
   int ghost[3],pacman[3],target[2], number_ghost=0;
@@ -66,4 +67,100 @@ int main (int argc, char *argv[])
   	}
   }
   return 0;
+}
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int jogada_valida(int vetor_posicao[50], int jogada) //recebe o vetor de jogadas realizadas e a jogada que se deseja fazer
+{
+   int i;
+   for(i = 0; i < 50; i++) // percorre o vetor
+   {
+       if (jogada == vetor_posicao[i]) // se a jogada existir no vetor, ou seja, ja foi realizada retorna 0, valor para false.
+       {
+           return 0;
+       }
+    }
+    return 1; //  se a jogada não existir ele retorna 1, valor para true
+}
+
+
+void cria(const char argv[])
+{
+	FILE *fin;
+	fin=fopen(argv,"r");
+	
+	if(fin==NULL)
+	{
+		printf("error");
+	}
+	else
+	{	
+		int i,j,indices[50],posicao,palavra_rand;
+		char **matriz;
+		/*inicia a matriz*/
+	    matriz=(char**)malloc(25*sizeof(char*));  
+	    for ( i=0;i<25;i++)
+	    {
+	        matriz[i]=(char*)malloc(50*sizeof(char));
+	    }
+		           
+		   //recebe os dados do arquivo 
+			 for(i=0;i<25;i++)
+	        {
+	            fgets(**matriz[i],50,fin);
+	            // fgets((*p).cod,TAM,fin);
+	        }
+		
+			
+		//mostra as strings 
+				for(i=0;i<25;i++)
+	        {
+	            printf("%s",matriz[i][50]);
+	        }
+		
+		//mostra 24 strings aleatorias
+			for(j=0;j<24;j++)
+	        {
+		
+				palavra_rand=1 + ( rand() % 23 );
+				//tirar palavras repetida com novo vetor
+				for(i=0;i<50;i++)
+		        {
+		        	posicao=1 + ( rand() % 48 );
+		            //mostra a string de forma embaralhada
+		            //guaradar o indice q ja saiou em um vetor e perguntar se esse indice ja saiu se nao print else gerre outro numeor
+					if(jogada_valida(indices,posicao) )
+					{
+						printf("%c",matriz[palavra_rand][posicao]);					
+						indices[i]=posicao;	
+					}
+					else
+					{
+							i--;
+					}
+							            
+		        }
+				printf("\n");
+			}
+	}
+
+
+
+}
+
+
+
+int main(int argc, const char *argv[] )
+{
+		char file[50];	
+		puts("Informe o nome do arquivo:");
+        scanf("%s", file);
+    	cria(file);
+	
+	return 0;
 }
